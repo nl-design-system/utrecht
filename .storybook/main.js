05e0396bfcb39ab51565c8a2c6a19e63fc8405d4
Robbert Broersma <mail@robbertbroersma.nl>
2021-05-03 18:33:42 +0200

    config: support stories.mdx
    The Storybook 6.2 default glob has been updated, this change follows the latest default.
    
    https://github.com/storybookjs/storybook/pull/13996

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../__stories__/**/*.stories.@(js|mdx)', '../src/**/*.stories.@(js|mdx)'],
+  stories: ['../__stories__/**/*stories.@(js|mdx)', '../src/**/*stories.@(js|mdx)'],
   addons: [
     '@storybook/addon-notes/register',
     '@storybook/preset-scss',
