12d3331958839a0eb4c4ee20e7adeffe4c3581c4
Robbert Broersma <mail@robbertbroersma.nl>
2021-07-03 12:57:36 +0200

    feat: counter badge

---
+++ components/badge-counter/stencil.tsx
@@ -0,0 +1,26 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+import { Component, Prop, h } from "@stencil/core";
+
+@Component({
+  tag: "utrecht-badge-counter",
+  styleUrl: "bem.scss",
+  shadow: true,
+})
+export class BadgeCounter {
+  @Prop() value: number;
+  @Prop() max: number;
+  @Prop() locale: string;
+
+  render() {
+    const { locale, max, value } = this;
+
+    const limited = typeof max === "number" && typeof value === "number" && Number(value) > Number(max);
+    const formatNumber = (n: number) => (locale ? Intl.NumberFormat(locale).format(n) : String(n));
+    const textContent = limited ? `${formatNumber(max)}+` : typeof value === "number" ? formatNumber(value) : "";
+
+    return <div class="utrecht-badge-counter">{textContent ? textContent : <slot></slot>}</div>;
+  }
+}
