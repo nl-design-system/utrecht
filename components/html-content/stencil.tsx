cb3fccdddc4493f26cd0b5ae3c0d78b47e5884f7
Robbert Broersma <mail@robbertbroersma.nl>
2021-07-08 14:00:15 +0200

    feat: HTML Content web component

---
+++ components/html-content/stencil.tsx
@@ -0,0 +1,20 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+import { Component, h } from "@stencil/core";
+
+@Component({
+  tag: "utrecht-html-content",
+  styleUrl: "html.scss",
+  shadow: false,
+})
+export class HTMLContent {
+  render() {
+    return (
+      <div class="utrecht-html">
+        <slot></slot>
+      </div>
+    );
+  }
+}
