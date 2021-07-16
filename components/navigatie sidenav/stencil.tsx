d89e6272132fd600b8e81d4c09b610cf9cd24708
Robbert <mail@robbertbroersma.nl>
2021-07-16 16:14:53 +0200

    Metro menu (#154)
    * Navigatie menu toegevoegd
    
    * Toevoegen navigatie.scss
    
    * :pencil: documentatie sidenav toegevoegd.
    
    * update styling
    
    * Update index.css
    
    * SideNav styling update
    
    * Update index.css
    
    * sidenav update
    
    * Update index.css
    
    * :pencil: documentatie voor sidenav
    
    * Update current
    
    * update
    
    * Add some lines
    
    * Refactor from CSS variables to Style Dictionary
    
    * Fix lint issues
    
    * Update lines
    
    * Update bem.css
    
    * Update bem.css
    
    * start 2 sorts of nav
    
    * move top nav to own component
    
    * updates
    
    * Work work
    
    * Update bem.stories.mdx
    
    * Siblings
    
    * Update bem.stories.mdx
    
    * Sibling fix
    
    * Update siblings -s
    
    * update
    
    * Update bem.scss
    
    * feat: link has underline by default
    
    * nav focus update
    
    * New example side nav
    
    * Update bem.scss
    
    * update
    
    * Update bem.scss
    
    * Update bem.stories.mdx
    
    * optional `<ul>`
    
    * Update bem.stories.mdx
    
    * styling update menu
    
    * Navigatie menu toegevoegd
    
    * Toevoegen navigatie.scss
    
    * :pencil: documentatie sidenav toegevoegd.
    
    * update styling
    
    * Update index.css
    
    * SideNav styling update
    
    * Update index.css
    
    * sidenav update
    
    * Update index.css
    
    * :pencil: documentatie voor sidenav
    
    * Update current
    
    * update
    
    * Add some lines
    
    * Refactor from CSS variables to Style Dictionary
    
    * Fix lint issues
    
    * Update lines
    
    * Update bem.css
    
    * Update bem.css
    
    * start 2 sorts of nav
    
    * move top nav to own component
    
    * updates
    
    * Work work
    
    * Update bem.stories.mdx
    
    * Siblings
    
    * Update bem.stories.mdx
    
    * Sibling fix
    
    * Update siblings -s
    
    * update
    
    * Update bem.scss
    
    * nav focus update
    
    * New example side nav
    
    * Update bem.scss
    
    * update
    
    * Update bem.scss
    
    * Update bem.stories.mdx
    
    * optional `<ul>`
    
    * Update bem.stories.mdx
    
    * styling update menu
    
    * example web component
    
    * update dots en lines
    
    * 🐛  kleine fixes
    
    Co-authored-by: Jeroen du Chatinier <49227194+JeroenduC@users.noreply.github.com>
    Co-authored-by: Olling <48805670+Ollie-nl@users.noreply.github.com>

---
+++ components/navigatie sidenav/stencil.tsx
@@ -0,0 +1,66 @@
+import { Component, Prop, h } from "@stencil/core";
+import clsx from "clsx";
+
+@Component({
+  tag: "utrecht-sidenav",
+  styleUrl: "bem.scss",
+  shadow: true,
+})
+export class Sidenav {
+  @Prop() json: string;
+
+  render() {
+    const items = JSON.parse(this.json);
+
+    return (
+      <nav class="utrecht-sidenav">
+        <ul class="utrecht-sidenav__list">
+          {items.map(({ href, title, current, focus, children, sibling, haschildren }) => (
+            <li
+              key={href}
+              class={clsx(
+                "utrecht-sidenav__item",
+                sibling && "utrecht-sidenav__item--sibling",
+                haschildren && "utrecht-sidenav__item--has-children"
+              )}
+            >
+              <a
+                class={clsx(
+                  "utrecht-sidenav__link",
+                  current && "utrecht-sidenav__link--current",
+                  focus && "utrecht-sidenav__link--focus",
+                  sibling && "utrecht-sidenav__link--sibling",
+                  haschildren && "utrecht-sidenav__link--has-children"
+                )}
+                href={href}
+              >
+                {title}
+              </a>
+              <span></span>
+              {children ? (
+                <ul class="utrecht-sidenav__list utrecht-sidenav__list--child">
+                  {children.map(({ href, title, current, focus }) => (
+                    <li key={href} class="utrecht-sidenav__item utrecht-sidenav__item--child">
+                      <a
+                        class={clsx(
+                          "utrecht-sidenav__link utrecht-sidenav__link--child",
+                          current && "utrecht-sidenav__link--current utrecht-sidenav__link--child--current",
+                          focus && "utrecht-sidenav__link--focus"
+                        )}
+                        href={href}
+                      >
+                        {title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                ""
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  }
+}
