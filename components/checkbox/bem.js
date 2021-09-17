486e4ec1772ebabd39f82c1ca42b5d8b0725e1da
Robbert Broersma <mail@robbertbroersma.nl>
2021-09-17 22:10:13 +0200

    feat: checkbox template

---
+++ components/checkbox/bem.js
@@ -0,0 +1,18 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+import clsx from 'clsx';
+
+export const defaultArgs = {
+  checked: false,
+  disabled: false,
+};
+
+export const Checkbox = ({ checked = false, disabled = false }) =>
+  `<input type="checkbox"${checked ? ' checked' : ''} class="${clsx(
+    'utrecht-checkbox',
+    checked && 'utrecht-checkbox--checked',
+    disabled && 'utrecht-checkbox--disabled',
+  )}">`;
