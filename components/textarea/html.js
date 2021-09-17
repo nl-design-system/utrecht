dbc76a6b5023acb0dacdc711e60a2b5842c19748
Robbert Broersma <git@robbertbroersma.nl>
2021-09-17 17:16:42 +0200

    feat: templates for textarea component

---
+++ components/textarea/html.js
@@ -0,0 +1,22 @@
+export const defaultArgs = {
+  disabled: false,
+  invalid: false,
+  label: '',
+  placeholder: '',
+  readOnly: false,
+  required: false,
+  textContent: '',
+};
+
+export const TextArea = ({
+  disabled = false,
+  invalid = false,
+  label = '',
+  placeholder = '',
+  readOnly = false,
+  required = false,
+  textContent = '',
+}) =>
+  `<textarea class="utrecht-textarea"${disabled ? ' disabled' : ''}${invalid ? ' aria-invalid="true"' : ''}${
+    placeholder ? ` placeholder="${placeholder}"` : ''
+  }${readOnly ? ' readonly' : ''}${required ? ' required' : ''}>${textContent}</textarea>`;
