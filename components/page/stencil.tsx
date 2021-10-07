69ff26418bc4b0add46b07615080a435d98660e3
Robbert Broersma <mail@robbertbroersma.nl>
2021-10-07 13:50:33 +0200

    feat: page component

---
+++ components/page/stencil.tsx
@@ -0,0 +1,23 @@
+import { Component, h } from "@stencil/core";
+
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Gemeente Utrecht
+ */
+
+@Component({
+  tag: "utrecht-page",
+  styleUrl: "stencil.scss",
+  shadow: true,
+})
+export class Page {
+  render() {
+    return (
+      <div class="utrecht-page">
+        <div class="utrecht-page__content">
+          <slot></slot>
+        </div>
+      </div>
+    );
+  }
+}
