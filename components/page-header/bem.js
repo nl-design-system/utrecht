c9a833c2675741225444d81dadc47c56dbb25e88
Robbert Broersma <mail@robbertbroersma.nl>
2021-10-07 13:52:05 +0200

    feat: page header component

---
+++ components/page-header/bem.js
@@ -0,0 +1,6 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+export const PageHeader = ({ innerHTML = '' }) => `<header class="utrecht-page-header">${innerHTML}</header>`;
---
+++ components/page-header/stencil.tsx
@@ -0,0 +1,21 @@
+import { Component, h } from "@stencil/core";
+
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Gemeente Utrecht
+ */
+
+@Component({
+  tag: "utrecht-page-header",
+  styleUrl: "stencil.scss",
+  shadow: true,
+})
+export class PageHeader {
+  render() {
+    return (
+      <header class="utrecht-page-header">
+        <slot></slot>
+      </header>
+    );
+  }
+}
