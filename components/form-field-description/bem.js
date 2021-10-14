1a3a678ff73827cff54d39c9d6d112d2d0a0376f
Robbert Broersma <mail@robbertbroersma.nl>
2021-10-14 16:53:31 +0200

    feat: warning variant of form-field-description

--- components/form-field-description/bem.js
+++ components/form-field-description/bem.js
@@ -9,6 +9,7 @@ export const FormFieldDescription = ({ id = null, innerHTML = '', status = null 
   `<div${id ? ` id="${id}"` : ''} class="${clsx(
     'utrecht-form-field-description',
     'utrecht-form-field-description--distanced',
-    status === 'valid' && 'utrecht-form-field-description--valid',
     status === 'invalid' && 'utrecht-form-field-description--invalid',
+    status === 'valid' && 'utrecht-form-field-description--valid',
+    status === 'warning' && 'utrecht-form-field-description--warning',
   )}">${innerHTML}</div>`;
