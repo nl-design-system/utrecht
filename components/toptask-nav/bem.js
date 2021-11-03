2f210a24a080989e0b67a979fe7d14c432ffdb50
Olling <48805670+Ollie-nl@users.noreply.github.com>
2021-11-03 22:05:28 +0100

    Icons en start Toptask Card

---
+++ components/toptask-nav/bem.js
@@ -0,0 +1,19 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Gemeente Utrecht
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+import clsx from 'clsx';
+import { ToptaskLink } from '../toptask-link/bem';
+
+export const defaultArgs = {
+  links: [],
+  grid: false,
+};
+
+export const ToptaskNav = ({ links, grid = false }) => `<div class="${clsx('utrecht-toptask-nav', {
+  'utrecht-toptask-nav--grid': grid,
+})}">
+  ${links.map(ToptaskLink).join('\n')}
+</div>`;
