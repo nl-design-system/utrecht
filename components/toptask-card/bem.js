dd29ae60babc8a1c8996b15f6ce40f10fec1de4e
Olling <48805670+Ollie-nl@users.noreply.github.com>
2021-11-11 22:58:12 +0100

    Add toptask vars

---
+++ components/toptask-card/bem.js
@@ -0,0 +1,38 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Gemeente Utrecht
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+import clsx from 'clsx';
+
+export const defaultArgs = {
+  active: false,
+  disabled: false,
+  focus: false,
+  focusVisible: false,
+  hover: false,
+  textContent: '',
+};
+
+export const ToptaskCard = ({
+  active = false,
+  disabled = false,
+  focus = false,
+  focusVisible = false,
+  hover = false,
+  textContent = '',
+}) =>
+  `<a href="#" title="link to..." class="${clsx('utrecht-toptask-card', {
+    'utrecht-toptask-card--active': active,
+    'utrecht-toptask-card--hover': hover,
+    'utrecht-toptask-card--focus': focus,
+    'utrecht-toptask-card--focus-visible': focusVisible,
+    'utrecht-toptask-card--disabled': disabled,
+  })}"${disabled ? ' aria-disabled="true"' : ''}>
+  <div class="utrecht-toptask-card-container">
+    <div class="utrecht-toptask-card__icon">
+      <utrecht-icon-loupe></utrecht-icon-loupe>
+    </div>
+ <div class="utrecht-toptask-card__body">
+  ${textContent}</div></div></a>`;
