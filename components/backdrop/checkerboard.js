231df2b8f09bc7bca89e03002039f2684f06048a
Robbert Broersma <mail@robbertbroersma.nl>
2021-11-21 13:03:18 +0100

    feat: backdrop component

---
+++ components/backdrop/checkerboard.js
@@ -0,0 +1,20 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+export const checkerboard = (story) =>
+  `<div style="
+      --checkerboard-color-1: #000000;
+      --checkerboard-color-2: #ffffff;
+      --checkerboard-size: 16px;
+      background-color: var(--checkerboard-color-2);
+      background-image: linear-gradient(45deg, var(--checkerboard-color-1) 25%, transparent 25%, transparent 75%, var(--checkerboard-color-1) 75%,var(--checkerboard-color-1)), linear-gradient(45deg, var(--checkerboard-color-1) 25%, transparent 25%, transparent 75%, var(--checkerboard-color-1) 75%, var(--checkerboard-color-1));
+      background-position: 0 0, var(--checkerboard-size) var(--checkerboard-size);
+      background-size: calc(var(--checkerboard-size) * 2) calc(var(--checkerboard-size) * 2);
+      height: 100%;
+      min-height: calc(40 * var(--checkerboard-size));
+      min-width: calc(40 * var(--checkerboard-size));
+      position: relative;
+      width: 100%;
+">${story()}</div>`;
---
+++ components/backdrop/stencil.tsx
@@ -0,0 +1,23 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+import { Component, h, Prop } from '@stencil/core';
+import clsx from 'clsx';
+
+@Component({
+  tag: 'utrecht-backdrop',
+  styleUrl: 'stencil.scss',
+  shadow: true,
+})
+export class Backdrop {
+  @Prop() viewport: boolean;
+  render() {
+    const { viewport } = this;
+    return (
+      <div class={clsx('utrecht-backdrop', viewport && 'utrecht-backdrop--viewport')}>
+        <slot />
+      </div>
+    );
+  }
+}
