8988061680ea0f21a368afe1748aaa93a432146f
Robbert Broersma <mail@robbertbroersma.nl>
2022-02-10 20:30:39 +0100

    fix: fieldset example with fieldset and div

--- components/form-fieldset/bem.js
+++ components/form-fieldset/bem.js
@@ -3,8 +3,26 @@
  * Copyright (c) 2021 Robbert Broersma
  */
 
-export const FormFieldset = ({ content = '', legend = '' }) =>
-  `<div class="utrecht-form-fieldset">
-  ${legend ? `<div class="utrecht-form-fieldset__legend utrecht-form-fieldset__legend--distanced">${legend}</div>` : ''}
+export const FormFieldset = ({ content = '', legend = '', role = '' }) =>
+  `<fieldset class="utrecht-form-fieldset utrecht-form-fieldset--html-legend"${
+    role && role !== 'group' ? ` role="${role}"` : ''
+  }>
+  ${
+    legend
+      ? `<legend class="utrecht-form-fieldset__legend utrecht-form-fieldset__legend--html-legend utrecht-form-fieldset__legend--distanced">${legend}</legend>`
+      : ''
+  }
+  ${content}
+</fieldset>`;
+
+export const FormFieldsetDiv = ({ content = '', legend = '', role = 'group', id }) =>
+  `<div class="utrecht-form-fieldset" role="${role}"${legend ? ` aria-labelledby="${id}-label"` : ''}>
+  ${
+    legend
+      ? `<div class="utrecht-form-fieldset__legend utrecht-form-fieldset__legend--distanced"${
+          id ? ` id="${id}-label"` : ''
+        }>${legend}</div>`
+      : ''
+  }
   ${content}
 </div>`;
