ac0a06b27a086a53276e21844c8210d91df47064
Yolijn <mail@yolijn.com>
2022-02-11 11:43:57 +0100

    docs: improve docusaurus sidebar and navigation config

--- standalone/docusaurus/docusaurus.config.js
+++ standalone/docusaurus/docusaurus.config.js
@@ -23,7 +23,7 @@ module.exports = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve('./sidebarConfig.js'),
           path: 'docs',
           routeBasePath: 'docs',
           editUrl: 'https://github.com/nl-design-system/utrecht/tree/main/documentation',
@@ -48,48 +48,6 @@ module.exports = {
       prism: {
         theme: require('prism-react-renderer/themes/github'),
       },
-      navbar: {
-        title: 'Utrecht Design System',
-        logo: {
-          alt: 'Utrecht Design System Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'tmp/basis/introductie',
-            position: 'left',
-            label: 'De Basis',
-          },
-          {
-            type: 'doc',
-            docId: 'tmp/huisstijl/colours',
-            position: 'left',
-            label: 'De Stijl',
-          },
-          {
-            type: 'doc',
-            docId: 'componenten/Heading',
-            position: 'left',
-            label: 'Componenten',
-          },
-          {
-            type: 'doc',
-            docId: 'tmp/gebruikersonderzoek/research',
-            position: 'left',
-            label: 'Onderzoek',
-          },
-          {
-            href: 'https://github.com/nl-design-system/utrecht',
-            label: 'GitHub',
-            position: 'right',
-          },
-          {
-            href: 'https://nl-design-system.github.io/utrecht/storybook',
-            label: 'Storybook',
-            position: 'right',
-          },
-        ],
-      },
+      navbar: require('./navigationConfig'),
     },
 };
