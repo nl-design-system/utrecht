9ef17abe8b7d60792681e44eb1cb87df6801c22d
Yolijn <mail@yolijn.com>
2022-02-11 11:43:57 +0100

    docs: improve docusaurus sidebar and navigation config

---
+++ standalone/docusaurus/navigationConfig.js
@@ -0,0 +1,45 @@
+/* eslint-env node */
+
+module.exports = {
+  title: 'Utrecht Design System',
+  logo: {
+    alt: 'Utrecht Design System Logo',
+    src: 'img/logo.svg',
+  },
+  items: [
+    {
+      type: 'doc',
+      docId: 'tmp/basis/introductie',
+      position: 'left',
+      label: 'De Basis',
+    },
+    {
+      type: 'doc',
+      docId: 'tmp/huisstijl/colours',
+      position: 'left',
+      label: 'De Stijl',
+    },
+    {
+      type: 'doc',
+      docId: 'componenten/Heading',
+      position: 'left',
+      label: 'Componenten',
+    },
+    {
+      type: 'doc',
+      docId: 'tmp/gebruikersonderzoek/research',
+      position: 'left',
+      label: 'Onderzoek',
+    },
+    {
+      href: 'https://github.com/nl-design-system/utrecht',
+      label: 'GitHub',
+      position: 'right',
+    },
+    {
+      href: 'https://nl-design-system.github.io/utrecht/storybook',
+      label: 'Storybook',
+      position: 'right',
+    },
+  ],
+};
