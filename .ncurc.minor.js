3a833d475ed79681d54133e22839d7fba9fa4ffe
Yolijn <yolijn@frameless.io>
2022-04-07 17:51:27 +0200

    build: patch and minor updates for npm packages including storybook

--- .ncurc.minor.js
+++ .ncurc.minor.js
@@ -1,18 +1,5 @@
 const patchConfig = require('./.ncurc.patch');
 
 module.exports = {
-  reject: [
-    ...patchConfig.reject,
-    'typescript',
-    // Storybook 6.4 causes issues with the code samples in our stories.
-    // A fix and manual update is needed before upgrading.
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-    '@storybook/addon-viewport',
-    '@storybook/html',
-    '@storybook/theming',
-    '@storybook/builder-webpack5',
-    '@storybook/cli',
-    '@storybook/manager-webpack5',
-  ],
+  reject: [...patchConfig.reject, 'typescript'],
 };
