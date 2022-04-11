02b89bd623b6f9a80880ff29f298c806bb97ee48
Robbert Broersma <mail@robbertbroersma.nl>
2022-04-11 13:25:15 +0200

    feat: introduce `@utrecht/component-library-react` package

---
+++ packages/component-library-react/jest.config.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+const nextJest = require('next/jest');
+
+// Next Jest config is great for Jest 27 + React + TypeScript, so let's use that as basis
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['/dist/'],
+  // transformIgnorePatterns: ['node_modules/(?!@utrecht/web-component-library-react)'],
+};
+
+module.exports = createJestConfig(customJestConfig);
