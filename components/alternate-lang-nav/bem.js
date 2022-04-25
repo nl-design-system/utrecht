01b9c099c88e36613609d169fda823d7680dc1f4
Yolijn <mail@yolijn.com>
2022-04-25 20:26:20 +0200

    refactor: rename link bem to css
    - rename bem.scss to css/index.scss
    - rename bem.js to template
    - split stories
    - include readme
    - separate tokens into a story
    - include alternate lang as a modifier

--- components/alternate-lang-nav/bem.js
+++ components/alternate-lang-nav/bem.js
@@ -4,9 +4,9 @@
  * Copyright (c) 2021 Robbert Broersma
  */
 
-import { AlternateLangLink } from '../alternate-lang-link/bem';
+import { Link } from '../link/css/template';
 
 export const AlternateLangNav = ({ languages }) =>
   `<div class="utrecht-alternate-lang-nav">
-${languages.map(AlternateLangLink).join('\n<span aria-hidden="true"> | </span>\n')}
+${languages.map(Link).join('\n<span aria-hidden="true"> | </span>\n')}
 </div>`;
