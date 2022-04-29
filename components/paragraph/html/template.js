4b863fba7a6359e2e029f7bf047c244a64c5a10e
Yolijn <mail@yolijn.com>
2022-04-29 12:36:52 +0200

    docs(paragraph): split stories

--- components/paragraph/html/template.js
+++ components/paragraph/html/template.js
@@ -1,4 +1,11 @@
 export const Paragraph = ({ textContent = '', lead = false }) =>
   `<section class="utrecht-html">
   <p${lead ? ' class="lead"' : ''}>${textContent}</p>
-</section>`
+</section>`;
+
+export const Paragraphs = ({ textContent = '' }) =>
+  `<section class="utrecht-html">
+  <p class="lead">${textContent}</p>
+  <p>${textContent}</p>
+  <p>${textContent}</p>
+</section>`;
