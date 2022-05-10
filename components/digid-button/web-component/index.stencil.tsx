ced8a6609791d1e51d2d3df4ecf47bb7566af292
Yolijn <yolijn@frameless.io>
2022-05-10 15:37:40 +0200

    refactor: group digid-button implementations and rename bem to css

--- components/digid-button/web-component/index.stencil.tsx
+++ components/digid-button/web-component/index.stencil.tsx
@@ -8,7 +8,7 @@ import { Component, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'utrecht-digid-button',
-  styleUrl: 'stencil.scss',
+  styleUrl: 'index.scss',
   shadow: true,
 })
 export class DigidButton {
