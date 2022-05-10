fa79ed399c85f94994fed333c5062f02d826a3c7
Yolijn <yolijn@frameless.io>
2022-05-10 16:04:40 +0200

    refactor: group digid-logo implementation

--- components/digid-logo/web-component/index.stencil.tsx
+++ components/digid-logo/web-component/index.stencil.tsx
@@ -8,7 +8,7 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'utrecht-digid-logo',
-  styleUrl: 'stencil.scss',
+  styleUrl: 'index.scss',
   shadow: true,
 })
 export class DigidLogo {
