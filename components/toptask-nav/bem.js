e91d4acde67ed1c3181d71ad414b44173c69b8cb
Yolijn <mail@yolijn.com>
2022-05-12 19:41:08 +0200

    refactor: group toptask-link implementation and rename bem to css

--- components/toptask-nav/bem.js
+++ components/toptask-nav/bem.js
@@ -4,16 +4,16 @@
  * Copyright (c) 2021 Robbert Broersma
  */
 
-import clsx from 'clsx';
-import { ToptaskLink } from '../toptask-link/bem';
+import clsx from 'clsx'
+import { ToptaskLink } from '../toptask-link/css/template'
 
 export const defaultArgs = {
   links: [],
   grid: false,
-};
+}
 
 export const ToptaskNav = ({ links, grid = false }) => `<div class="${clsx('utrecht-toptask-nav', {
   'utrecht-toptask-nav--grid': grid,
 })}">
   ${links.map(ToptaskLink).join('\n')}
-</div>`;
+</div>`
