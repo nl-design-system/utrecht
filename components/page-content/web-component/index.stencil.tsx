49dfd7b43cd1434831dc0941fb872b4dd87396d2
Yolijn <mail@yolijn.com>
2022-05-12 16:52:39 +0200

    refactor: group page-content implementations and rename bem to css

--- components/page-content/web-component/index.stencil.tsx
+++ components/page-content/web-component/index.stencil.tsx
@@ -7,7 +7,7 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'utrecht-page-content',
-  styleUrl: 'stencil.scss',
+  styleUrl: 'index.scss',
   shadow: true,
 })
 export class Page {
