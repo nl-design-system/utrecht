50f4d48e920b66123321e838a0e3883983529125
Yolijn <mail@yolijn.com>
2022-05-12 18:23:41 +0200

    refactor: group separator implementations and rename bem to css

--- components/select/css/template.js
+++ components/select/css/template.js
@@ -4,13 +4,13 @@
  * Copyright (c) 2021 Robbert Broersma
  */
 
-import clsx from 'clsx'
+import clsx from 'clsx';
 
 const exampleOptions = [
   { value: '1', label: 'Option #1' },
   { value: '2', label: 'Option #2', selected: true },
   { value: '3', label: 'Option #3' },
-]
+];
 
 export const defaultArgs = {
   disabled: false,
@@ -18,7 +18,7 @@ export const defaultArgs = {
   invalid: false,
   options: exampleOptions,
   required: false,
-}
+};
 
 export const Select = ({ disabled = false, focus = false, invalid = false, options = [], required = false }) =>
   `<select${invalid ? ' aria-invalid="true"' : ''}${disabled ? ' disabled' : ''}${
@@ -36,4 +36,4 @@ export const Select = ({ disabled = false, focus = false, invalid = false, optio
         `<option${selected ? ' selected' : ''}${value ? ` value="${value}"` : ''}>${label}</option>`,
     )
     .join('\n  ')}
-</select>`
+</select>`;
