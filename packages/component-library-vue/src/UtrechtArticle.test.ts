75585d08b7b1c01d0a700c990db839b3919edcf2
Robbert Broersma <mail@robbertbroersma.nl>
2022-05-28 13:34:12 +0200

    test: add more unit tests for Vue components

---
+++ packages/component-library-vue/src/UtrechtArticle.test.ts
@@ -0,0 +1,60 @@
+/* eslint-env jest */
+import { render } from '@testing-library/vue';
+import Article from './UtrechtArticle.vue';
+import '@testing-library/jest-dom';
+
+describe('Article', () => {
+  it('renders a article role element', () => {
+    const { getByRole } = render(Article);
+
+    const article = getByRole('article');
+
+    expect(article).toBeVisible();
+  });
+
+  it('renders an HTML article element', () => {
+    const { container } = render(Article);
+
+    const article = container.querySelector('article:only-child');
+
+    expect(article).toBeInTheDocument();
+  });
+
+  it('renders a design system BEM class name', () => {
+    const { container } = render(Article);
+
+    const article = container.querySelector(':only-child');
+
+    expect(article).toHaveClass('utrecht-article');
+  });
+
+  it('renders rich text content', () => {
+    const { container } = render(Article, {
+      slots: {
+        default: '<strong>Breaking</strong> news',
+      },
+    });
+
+    const article = container.querySelector(':only-child');
+
+    const richText = article?.querySelector('strong');
+
+    expect(richText).toBeInTheDocument();
+  });
+
+  it('can be hidden', () => {
+    const { container } = render(Article, { props: { hidden: true } });
+
+    const article = container.querySelector(':only-child');
+
+    expect(article).not.toBeVisible();
+  });
+
+  it('can have a custom class name', () => {
+    const { container } = render(Article, { props: { class: 'breaking-news' } });
+
+    const article = container.querySelector(':only-child');
+
+    expect(article).toHaveClass('breaking-news');
+  });
+});
