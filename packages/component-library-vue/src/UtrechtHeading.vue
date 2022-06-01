f4c3973cbbe85675bc5647de0794545b480c0fb1
Ali Amori kadhim <ali@Alis-MBP.home>
2022-06-01 17:32:39 +0200

    feat: create heading component for vuejs

---
+++ packages/component-library-vue/src/UtrechtHeading.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+  defineProps<{
+    level: number;
+  }>();
+</script>
+
+<template>
+  <h1 v-if="level == 1" class="utrecht-heading-1">
+    <slot></slot>
+  </h1>
+  <h2 v-else-if="level == 2" class="utrecht-heading-2">
+    <slot></slot>
+  </h2>
+  <h3 v-else-if="level == 3" class="utrecht-heading-3">
+    <slot></slot>
+  </h3>
+  <h4 v-else-if="level == 4" class="utrecht-heading-4">
+    <slot></slot>
+  </h4>
+  <h5 v-else-if="level == 5" class="utrecht-heading-5">
+    <slot></slot>
+  </h5>
+  <h6 v-else class="utrecht-heading-6">
+    <slot></slot>
+  </h6>
+</template>
+
+<style scoped src="@utrecht/components/heading-1/css/index.scss"></style>
+<style scoped src="@utrecht/components/heading-2/css/index.scss"></style>
+<style scoped src="@utrecht/components/heading-3/css/index.scss"></style>
+<style scoped src="@utrecht/components/heading-4/css/index.scss"></style>
+<style scoped src="@utrecht/components/heading-5/css/index.scss"></style>
+<style scoped src="@utrecht/components/heading-6/css/index.scss"></style>
