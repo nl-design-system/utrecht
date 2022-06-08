097a7137880d4ef76fb947d174cdba036352f695
Ali Amori Kadhim <ali@frameless.io>
2022-06-08 17:08:23 +0200

    feat: create custom radio button style & storybook

---
+++ components/custom-radio-button/css/template.js
@@ -0,0 +1,20 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+ import clsx from 'clsx';
+
+ export const defaultArgs = {
+   checked: false,
+   disabled: false,
+ };
+ 
+ export const CustomRadioButton = ({ checked = false, disabled = false }) =>
+   `<input type="radio"${checked ? ' checked' : ''}${disabled ? ' disabled' : ''} class="${clsx(
+     'utrecht-custom-radio-button',
+     'utrecht-custom-radio-button--html-input',
+     checked && 'utrecht-custom-radio-button--checked',
+     disabled && 'utrecht-custom-radio-button--disabled',
+   )}">`;
+ 
---
+++ components/custom-radio-button/design.md
@@ -0,0 +1,7 @@
+<!-- @license CC0-1.0 -->
+
+<!-- markdownlint-disable first-line-h1 -->
+
+## Design
+
+[Figma - Utrecht Design System - Custom RadioButton](https://www.figma.com/file/msb3CfQBefPoruqNQ968Zh/Utrecht-Design-System?node-id=499%3A5622)
