f7d9be6b44d5f1a9ff1d8b1bd3b60b128f817661
Ali Amori Kadhim <ali@frameless.io>
2022-06-09 10:09:08 +0200

    fixup! feat: create custom radio button style & storybook

--- components/custom-radio-button/css/template.js
+++ components/custom-radio-button/css/template.js
@@ -3,18 +3,17 @@
  * Copyright (c) 2021 Robbert Broersma
  */
 
- import clsx from 'clsx';
+import clsx from 'clsx';
 
- export const defaultArgs = {
-   checked: false,
-   disabled: false,
- };
- 
- export const CustomRadioButton = ({ checked = false, disabled = false }) =>
-   `<input type="radio"${checked ? ' checked' : ''}${disabled ? ' disabled' : ''} class="${clsx(
-     'utrecht-custom-radio-button',
-     'utrecht-custom-radio-button--html-input',
-     checked && 'utrecht-custom-radio-button--checked',
-     disabled && 'utrecht-custom-radio-button--disabled',
-   )}">`;
- 
+export const defaultArgs = {
+  checked: false,
+  disabled: false,
+};
+
+export const CustomRadioButton = ({ checked = false, disabled = false }) =>
+  `<input type="radio"${checked ? ' checked' : ''}${disabled ? ' disabled' : ''} class="${clsx(
+    'utrecht-custom-radio-button',
+    'utrecht-custom-radio-button--html-input',
+    checked && 'utrecht-custom-radio-button--checked',
+    disabled && 'utrecht-custom-radio-button--disabled',
+  )}">`;
