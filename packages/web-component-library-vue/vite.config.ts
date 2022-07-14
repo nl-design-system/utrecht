83253056140be55c9956687064b3053ae108c06c
Ali Amori Kadhim <ali@frameless.io>
2022-07-14 13:15:27 +0200

    fix: web component library vue
    **issue:**
    it didn't render component
    
    ***improvements:**
    
    - [x] use vite to build the library instead of tsc
    - [x] remove patch-vue.ts from stencil

---
+++ packages/web-component-library-vue/vite.config.ts
@@ -0,0 +1,24 @@
+/* eslint-env node */
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'WebComponentLibraryVue',
+      fileName: (format) => `web-component-library-vue.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [dts({ copyDtsFiles: false, outputDir: './dist', tsConfigFilePath: './tsconfig.json' }), vue()],
+});
