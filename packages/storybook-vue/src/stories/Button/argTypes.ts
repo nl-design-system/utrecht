fbffd577ea4d5a743ca8d0953e0492c8fa2a1e4e
Ali Amori Kadhim <ali@frameless.io>
2022-07-18 17:26:16 +0200

    docs: add storybook to the vue component library
    note: work in progress
    
    - [] add button stories

---
+++ packages/storybook-vue/src/stories/Button/argTypes.ts
@@ -0,0 +1,10 @@
+export default {
+  appearance: {
+    type: 'select',
+    options: ['primary-action-button', 'secondary-action-button', 'subtle-button'],
+  },
+  type: {
+    type: 'select',
+    options: ['button', 'submit', 'reset'],
+  },
+};
