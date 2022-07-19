3e2b70e47cbc5d4b1bef114c232ec6a99c11a93f
Ali Amouri Kadhim <53454241+AliKdhim87@users.noreply.github.com>
2022-07-19 15:13:24 +0200

    docs: add button states (#1066)
    * docs: add button states
    
    * chore: move packages to dev dependencies

--- packages/storybook-vue/config/main.js
+++ packages/storybook-vue/config/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-pseudo-states'],
   framework: '@storybook/vue3',
   staticDirs: ['../../../proprietary/assets'],
 };
