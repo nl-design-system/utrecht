b16b2ab6683b35f2d91ae4de1898b83bda2ad31b
Ali Amori Kadhim <ali@frameless.io>
2022-07-29 16:46:01 +0200

    docs: add vue textbox component stories

---
+++ packages/storybook-vue/src/stories/Textbox/argTypes.ts
@@ -0,0 +1,33 @@
+export const argTypes = {
+  disabled: {
+    name: 'disabled',
+    type: { name: 'boolean', required: false },
+    defaultValue: false,
+  },
+  invalid: {
+    name: 'invalid',
+    type: { name: 'boolean', required: false },
+    defaultValue: false,
+  },
+  required: {
+    name: 'required',
+    type: { name: 'boolean', required: false },
+    defaultValue: false,
+  },
+  readonly: {
+    name: 'readonly',
+    type: { name: 'boolean', required: false },
+    defaultValue: false,
+  },
+  modelValue: {
+    name: 'modelValue',
+    type: { name: 'string', required: false },
+    defaultValue: 'The Quick Brown Fox Jumps Over The Lazy Dog',
+  },
+  type: {
+    name: 'type',
+    type: { name: 'select', required: false },
+    options: ['text', 'email', 'password', 'number'],
+    defaultValue: 'text',
+  },
+};
