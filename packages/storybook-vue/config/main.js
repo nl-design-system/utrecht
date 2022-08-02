97f2d3821f5626d3e209afb6617c1e119c5dc49f
Robbert Broersma <git@robbertbroersma.nl>
2022-08-02 19:33:11 +0200

    fix: use same build settings for both Storybook configs

--- packages/storybook-vue/config/main.js
+++ packages/storybook-vue/config/main.js
@@ -1,5 +1,11 @@
 /* eslint-disable no-undef */
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
+  features: {
+    postcss: false,
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-pseudo-states'],
   framework: '@storybook/vue3',
