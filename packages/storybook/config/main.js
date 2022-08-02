04cf1c9ab28b2748227f5b5d79ca9d677fca7438
Ali Amori Kadhim <ali@frameless.io>
2022-08-02 09:11:43 +0200

    build: enable storybook composition

--- packages/storybook/config/main.js
+++ packages/storybook/config/main.js
@@ -21,4 +21,20 @@ module.exports = {
     '@etchteam/storybook-addon-status/register',
     '@whitespace/storybook-addon-html',
   ],
+  refs: (config, { configType }) => {
+    if (configType === 'DEVELOPMENT') {
+      return {
+        vue: {
+          title: 'Vue.js Component',
+          url: 'http://localhost:6007',
+        },
+      };
+    }
+    return {
+      vue: {
+        title: 'Vue.js Component',
+        url: 'https://nl-design-system.github.io/utrecht/storybook-vue/?path=/story/vue-js-component-component-introduction--page',
+      },
+    };
+  },
 };
