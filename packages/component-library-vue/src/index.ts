8f9a8f9f74625867153e00e6fe5488172cde8433
Ali Amori Kadhim <ali@frameless.io>
2022-08-08 10:56:38 +0200

    fix: change the way of export the vue component from index

--- packages/component-library-vue/src/index.ts
+++ packages/component-library-vue/src/index.ts
@@ -1,73 +1,35 @@
-import Article from './UtrechtArticle.vue';
-import BadgeStatus from './UtrechtBadgeStatus.vue';
-import Button from './UtrechtButton.vue';
-import ButtonGroup from './UtrechtButtonGroup.vue';
-import Checkbox from './UtrechtCheckbox.vue';
-import Document from './UtrechtDocument.vue';
-import FormField from './UtrechtFormField.vue';
-import FormFieldset from './UtrechtFormFieldset.vue';
-import FormFieldsetLegend from './UtrechtFormFieldsetLegend.vue';
-import FormLabel from './UtrechtFormLabel.vue';
-import Heading from './UtrechtHeading.vue';
-import Heading1 from './UtrechtHeading1.vue';
-import Heading2 from './UtrechtHeading2.vue';
-import Heading3 from './UtrechtHeading3.vue';
-import Heading4 from './UtrechtHeading4.vue';
-import Heading5 from './UtrechtHeading5.vue';
-import Heading6 from './UtrechtHeading6.vue';
-import Link from './UtrechtLink.vue';
-import Page from './UtrechtPage.vue';
-import PageContent from './UtrechtPageContent.vue';
-import PageFooter from './UtrechtPageFooter.vue';
-import PageHeader from './UtrechtPageHeader.vue';
-import Paragraph from './UtrechtParagraph.vue';
-import RadioButton from './UtrechtRadioButton.vue';
-import Select from './UtrechtSelect.vue';
-import Table from './UtrechtTable.vue';
-import TableBody from './UtrechtTableBody.vue';
-import TableCaption from './UtrechtTableCaption.vue';
-import TableCell from './UtrechtTableCell.vue';
-import TableFooter from './UtrechtTableFooter.vue';
-import TableHeader from './UtrechtTableHeader.vue';
-import TableHeaderCell from './UtrechtTableHeaderCell.vue';
-import TableRow from './UtrechtTableRow.vue';
-import Textarea from './UtrechtTextarea.vue';
-import Textbox from './UtrechtTextbox.vue';
-
-export {
-  Article,
-  BadgeStatus,
-  Button,
-  ButtonGroup,
-  Checkbox,
-  Document,
-  FormField,
-  FormFieldset,
-  FormFieldsetLegend,
-  FormLabel,
-  Heading,
-  Heading1,
-  Heading2,
-  Heading3,
-  Heading4,
-  Heading5,
-  Heading6,
-  Link,
-  Paragraph,
-  Page,
-  PageContent,
-  PageFooter,
-  PageHeader,
-  RadioButton,
-  Select,
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHeader,
-  TableHeaderCell,
-  TableRow,
-  Textbox,
-  Textarea,
-};
+export { default as Article } from './UtrechtArticle.vue';
+export { default as BadgeStatus } from './UtrechtBadgeStatus.vue';
+export { default as Button } from './UtrechtButton.vue';
+export { default as ButtonGroup } from './UtrechtButtonGroup.vue';
+export { default as Checkbox } from './UtrechtCheckbox.vue';
+export { default as Document } from './UtrechtDocument.vue';
+export { default as FormField } from './UtrechtFormField.vue';
+export { default as FormFieldset } from './UtrechtFormFieldset.vue';
+export { default as FormFieldsetLegend } from './UtrechtFormFieldsetLegend.vue';
+export { default as FormLabel } from './UtrechtFormLabel.vue';
+export { default as Heading } from './UtrechtHeading.vue';
+export { default as Heading1 } from './UtrechtHeading1.vue';
+export { default as Heading2 } from './UtrechtHeading2.vue';
+export { default as Heading3 } from './UtrechtHeading3.vue';
+export { default as Heading4 } from './UtrechtHeading4.vue';
+export { default as Heading5 } from './UtrechtHeading5.vue';
+export { default as Heading6 } from './UtrechtHeading6.vue';
+export { default as Link } from './UtrechtLink.vue';
+export { default as Page } from './UtrechtPage.vue';
+export { default as PageContent } from './UtrechtPageContent.vue';
+export { default as PageFooter } from './UtrechtPageFooter.vue';
+export { default as PageHeader } from './UtrechtPageHeader.vue';
+export { default as Paragraph } from './UtrechtParagraph.vue';
+export { default as RadioButton } from './UtrechtRadioButton.vue';
+export { default as Select } from './UtrechtSelect.vue';
+export { default as Table } from './UtrechtTable.vue';
+export { default as TableBody } from './UtrechtTableBody.vue';
+export { default as TableCaption } from './UtrechtTableCaption.vue';
+export { default as TableCell } from './UtrechtTableCell.vue';
+export { default as TableFooter } from './UtrechtTableFooter.vue';
+export { default as TableHeader } from './UtrechtTableHeader.vue';
+export { default as TableHeaderCell } from './UtrechtTableHeaderCell.vue';
+export { default as TableRow } from './UtrechtTableRow.vue';
+export { default as Textarea } from './UtrechtTextarea.vue';
+export { default as Textbox } from './UtrechtTextbox.vue';
