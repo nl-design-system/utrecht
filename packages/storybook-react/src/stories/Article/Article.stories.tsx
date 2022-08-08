3f0f34c3549ce5d966011c4b3bab978541414559
Ali Amori Kadhim <ali@frameless.io>
2022-08-08 14:15:26 +0200

    build: add react storybook

---
+++ packages/storybook-react/src/stories/Article/Article.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Article, Heading1, Paragraph } from '@utrecht/component-library-react';
+import React from 'react';
+
+export default {
+  title: 'React.js Component/Article',
+  id: 'react-article',
+  component: Article,
+} as ComponentMeta<typeof Article>;
+
+const Template: ComponentStory<typeof Article> = (args) => <Article {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: (
+    <>
+      <Heading1>Lorem ipsum</Heading1>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </Paragraph>
+    </>
+  ),
+};
