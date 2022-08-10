fe0ab5222a41821e5c680bb9447486c5668ec3a2
Ali Amori Kadhim <ali@frameless.io>
2022-08-10 14:07:55 +0200

    docs: add react custom radio button component stories

---
+++ packages/storybook-react/src/stories/CustomRadioButton/CustomRadioButton.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { CustomRadioButton } from '@utrecht/component-library-react';
+import React from 'react';
+import { argTypes } from './argTypes';
+
+export default {
+  title: 'React.js Component/Custom Radio Button',
+  id: 'react-custom-radio-button',
+  component: CustomRadioButton,
+  argTypes,
+} as ComponentMeta<typeof CustomRadioButton>;
+
+const Template: ComponentStory<typeof CustomRadioButton> = (args) => <CustomRadioButton {...args} />;
+
+export const Default = Template.bind({});
