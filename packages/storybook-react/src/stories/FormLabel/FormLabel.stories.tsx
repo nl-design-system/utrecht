6b9e4f7edae104e44b96bca5465161e53d274ee0
Ali Amori Kadhim <ali@frameless.io>
2022-08-10 14:09:56 +0200

    docs: add react form label component stories

---
+++ packages/storybook-react/src/stories/FormLabel/FormLabel.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { FormLabel } from '@utrecht/component-library-react';
+import React from 'react';
+import { argTypes } from './argTypes';
+
+export default {
+  title: 'React.js Component/Form Label',
+  id: 'react-form-label',
+  component: FormLabel,
+  argTypes,
+} as ComponentMeta<typeof FormLabel>;
+
+const Template: ComponentStory<typeof FormLabel> = (args) => <FormLabel {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: 'Username',
+};
---
+++ packages/storybook-react/src/stories/FormLabel/FormLabelStates.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { FormLabel } from '@utrecht/component-library-react';
+import React from 'react';
+import { argTypes } from './argTypes';
+
+export default {
+  title: 'React.js Component/Form Label/States',
+  id: 'react-form-label-states',
+  component: FormLabel,
+  argTypes,
+} as ComponentMeta<typeof FormLabel>;
+
+const Template: ComponentStory<typeof FormLabel> = (args) => <FormLabel {...args} />;
+
+export const Checked = Template.bind({});
+
+Checked.args = {
+  children: 'Form Label Checked',
+  type: 'checkbox',
+};
+
+export const Disabled = Template.bind({});
+
+Disabled.args = {
+  children: 'Form Label Disabled',
+  type: 'radio',
+};
