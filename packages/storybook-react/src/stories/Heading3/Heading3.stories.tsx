e3cb5d849501b1735cb5a1719752499a3e521ab7
Ali Amori Kadhim <ali@frameless.io>
2022-08-11 13:23:08 +0200

    docs: add react heading component stories

---
+++ packages/storybook-react/src/stories/Heading3/Heading3.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Heading3 } from '@utrecht/component-library-react';
+import React from 'react';
+
+export default {
+  title: 'React.js Component/Heading 3',
+  id: 'react-heading-3',
+  component: Heading3,
+} as ComponentMeta<typeof Heading3>;
+
+const Template: ComponentStory<typeof Heading3> = (args) => <Heading3 {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: 'The Quick Brown Fox Jumps Over The Lazy Dog',
+};
---
+++ packages/storybook-react/src/stories/Heading4/Heading4.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Heading4 } from '@utrecht/component-library-react';
+import React from 'react';
+
+export default {
+  title: 'React.js Component/Heading 4',
+  id: 'react-heading-4',
+  component: Heading4,
+} as ComponentMeta<typeof Heading4>;
+
+const Template: ComponentStory<typeof Heading4> = (args) => <Heading4 {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: 'The Quick Brown Fox Jumps Over The Lazy Dog',
+};
