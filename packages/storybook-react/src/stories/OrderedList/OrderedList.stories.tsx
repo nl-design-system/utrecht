d6fee9cc641944e241100400acf82900d1e99a1d
Ali Amori Kadhim <ali@frameless.io>
2022-08-11 16:49:05 +0200

    docs: add react order list component stories

---
+++ packages/storybook-react/src/stories/OrderedList/OrderedList.stories.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/jsx-key */
+// performance optimizations are not relevant for story rendering, ignore ESLint
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { OrderedList } from '@utrecht/component-library-react';
+import React from 'react';
+import { Default as OrderedListItem } from './OrderedListItem.stories.tsx';
+import { argTypes } from './argTypes';
+
+export default {
+  title: 'React.js Component/Ordered List',
+  id: 'react-ordered-list',
+  component: OrderedList,
+  subcomponents: { OrderedListItem },
+  argTypes,
+} as ComponentMeta<typeof OrderedList>;
+
+export const Default: ComponentStory<typeof OrderedList> = (args) => <OrderedList {...args} />;
+
+Default.args = {
+  children: [
+    <OrderedListItem {...OrderedListItem.args} />,
+    <OrderedListItem {...OrderedListItem.args} />,
+    <OrderedListItem {...OrderedListItem.args} />,
+  ],
+};
