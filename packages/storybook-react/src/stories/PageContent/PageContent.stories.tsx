86d6cbec585f9cc7bdd6ecf0b7bd69c12c9fd3b5
Ali Amori Kadhim <ali@frameless.io>
2022-08-12 14:38:02 +0200

    docs: add react page components stories

---
+++ packages/storybook-react/src/stories/PageContent/PageContent.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { PageContent, Paragraph } from '@utrecht/component-library-react';
+import React from 'react';
+
+export default {
+  title: 'React.js Component/Page Content',
+  id: 'react-page-content',
+  component: PageContent,
+} as ComponentMeta<typeof PageContent>;
+
+const Template: ComponentStory<typeof PageContent> = (args) => <PageContent {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: <Paragraph>Page Content Area</Paragraph>,
+};
+
+(Default as any).displayName = 'PageContent';
---
+++ packages/storybook-react/src/stories/PageFooter/PageFooter.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { PageFooter, Paragraph } from '@utrecht/component-library-react';
+import React from 'react';
+
+export default {
+  title: 'React.js Component/Page Footer',
+  id: 'react-page-footer',
+  component: PageFooter,
+} as ComponentMeta<typeof PageFooter>;
+
+const Template: ComponentStory<typeof PageFooter> = (args) => <PageFooter {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: <Paragraph>Page Footer Area</Paragraph>,
+};
+
+(Default as any).displayName = 'PageFooter';
