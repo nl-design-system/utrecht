e3d1800f7d171be0fafd3d02170bf2dbc9fea955
Ali Amori Kadhim <ali@frameless.io>
2022-08-18 12:06:31 +0200

    feat: enable tree-shaking by using rollupjs to build react libraries

---
+++ packages/web-component-library-react/rollup.config.js
@@ -0,0 +1,59 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import del from 'rollup-plugin-delete';
+import filesize from 'rollup-plugin-filesize';
+import nodeExternal from 'rollup-plugin-node-externals';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
+import packageJson from './package.json';
+
+// rollup.config.js
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+
+export const outputGlobals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+};
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+      globals: outputGlobals,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+      globals: outputGlobals,
+    },
+  ],
+  external: [/@babel\/runtime/, 'react-dom', 'react'],
+  plugins: [
+    peerDepsExternal({ includeDependencies: true }),
+    nodeExternal(),
+    resolve({ browser: true }),
+    commonjs({
+      include: /node_modules/,
+    }),
+    nodePolyfills(),
+    del({ targets: ['dist/*', 'pages/*'] }),
+    typescript({ includeDependencies: false }),
+    babel({
+      presets: ['@babel/preset-react'],
+      babelHelpers: 'runtime',
+      exclude: ['node_modules/**', 'dist/**'],
+      extensions: ['.ts', '.tsx'],
+      inputSourceMap: true,
+      plugins: ['@babel/plugin-transform-runtime'],
+    }),
+    filesize(),
+  ],
+};
