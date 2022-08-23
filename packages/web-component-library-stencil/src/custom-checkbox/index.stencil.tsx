e427e38e55cc34bb0f2c877d397e604443016888
Ali Amori Kadhim <ali@frameless.io>
2022-08-23 10:51:06 +0200

    feat: refactor the custom checkbox stencil
    BREAKING CHANGE: use native html input

--- packages/web-component-library-stencil/src/custom-checkbox/index.stencil.tsx
+++ packages/web-component-library-stencil/src/custom-checkbox/index.stencil.tsx
@@ -3,7 +3,7 @@
  * Copyright (c) 2021 Robbert Broersma
  */
 
-import { Component, Event, EventEmitter, h, Prop } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 import clsx from 'clsx';
 
 @Component({
@@ -12,75 +12,27 @@ import clsx from 'clsx';
   shadow: true,
 })
 export class CustomCheckbox {
-  @Prop({ reflect: true }) disabled: boolean = false;
-  @Prop({ reflect: true }) checked: boolean = false;
-  @Prop({ reflect: true }) indeterminate: boolean = false;
-  @Prop({ reflect: true }) invalid: boolean = false;
-  @Prop({ reflect: true }) required: boolean = false;
-  @Event() utrechtBlur: EventEmitter;
-  @Event() utrechtChange: EventEmitter;
-  @Event() utrechtFocus: EventEmitter;
-  @Event() utrechtInput: EventEmitter;
-
+  @Prop() disabled?: boolean;
+  @Prop() checked?: boolean;
+  @Prop() indeterminate?: boolean;
+  @Prop() invalid?: boolean;
+  @Prop() required?: boolean;
   render() {
-    const { checked, disabled, indeterminate, invalid, required } = this;
-    const active = false;
-
-    const toggleInteraction = () => {
-      if (!this.disabled) {
-        this.checked = !this.checked;
-        this.utrechtInput.emit(this);
-        this.utrechtChange.emit(this);
-      }
-    };
-
-    const handleClick = () => {
-      toggleInteraction();
-    };
-
-    const handleKeyPress = (evt) => {
-      if (evt.code === 'Space' || evt.key === ' ') {
-        // Do not scroll the page using Space when the toggle is focussed
-        evt.preventDefault();
-      }
-    };
-
-    const handleKeyUp = (evt) => {
-      if (evt.code === 'Space' || evt.key === ' ') {
-        toggleInteraction();
-      }
-    };
-
+    const { checked, required, disabled, indeterminate, invalid } = this;
     return (
-      <div
-        class="utrecht-custom-checkbox"
-        role="checkbox"
-        tabIndex={disabled ? null : 0}
-        aria-checked={indeterminate ? 'mixed' : checked ? 'true' : 'false'}
-        aria-disabled={disabled ? 'true' : null}
+      <input
         aria-invalid={invalid ? 'true' : null}
-        aria-required={required ? 'true' : null}
-        onClick={handleClick}
-        onKeyPress={handleKeyPress}
-        onKeyUp={handleKeyUp}
-      >
-        <div
-          class={clsx(
-            'utrecht-custom-checkbox__box',
-            active && 'utrecht-custom-checkbox__box--active',
-            checked && 'utrecht-custom-checkbox__box--checked',
-            !checked && 'utrecht-custom-checkbox__box--not-checked',
-            disabled && 'utrecht-custom-checkbox__box--disabled',
-            invalid && 'utrecht-custom-checkbox__box--invalid',
-            indeterminate && 'utrecht-custom-checkbox__box--indeterminate',
-          )}
-        >
-          {checked && <utrecht-icon-checkmark class="utrecht-custom-checkbox__icon">✔</utrecht-icon-checkmark>}
-          {indeterminate && (
-            <utrecht-icon-indeterminate class="utrecht-custom-checkbox__icon">■</utrecht-icon-indeterminate>
-          )}
-        </div>
-      </div>
+        class={clsx(
+          'utrecht-custom-checkbox',
+          'utrecht-custom-checkbox--html-input',
+          invalid && 'utrecht-custom-checkbox__box--invalid',
+        )}
+        type="checkbox"
+        checked={checked}
+        required={required}
+        indeterminate={indeterminate}
+        disabled={disabled}
+      />
     );
   }
 }
