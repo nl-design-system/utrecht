cfdf7ed7aa5c904f02bf98e8fa3cad670e26de3f
Robbert Broersma <git@robbertbroersma.nl>
2022-08-24 15:23:40 +0200

    feat: add `appearance` to React `ButtonLink`

--- packages/storybook-react/src/stories/ButtonLink/argTypes.ts
+++ packages/storybook-react/src/stories/ButtonLink/argTypes.ts
@@ -1,4 +1,9 @@
 export const argTypes = {
+  appearance: {
+    description: 'Appearance',
+    control: { type: 'select' },
+    options: ['', 'button', 'primary-action-button', 'secondary-action-button', 'subtle-button'],
+  },
   external: {
     type: 'boolean',
     table: {
