4b124862af56d3a986fe5ca6b87bad45c41e9d4e
Robbert Broersma <git@robbertbroersma.nl>
2022-08-30 13:11:11 +0200

    feat: skip link component

---
+++ components/skip-link/css/story-template.jsx
@@ -0,0 +1,70 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2022 Gemeente Utrecht
+ * Copyright (c) 2022 Frameless B.V.
+ */
+import clsx from 'clsx';
+import React from 'react';
+
+export const argTypes = {
+  href: {
+    description: 'Target URL',
+    type: {
+      name: 'string',
+      required: true,
+    },
+  },
+  textContent: {
+    description: 'Link text',
+    type: {
+      name: 'string',
+      required: true,
+    },
+  },
+  visibility: {
+    description: 'Visibilty',
+    type: 'select',
+    options: ['', 'hidden', 'visible'],
+  },
+  visibleOnFocus: {
+    description: 'Visible on focus',
+    control: 'boolean',
+  },
+};
+
+export const defaultArgs = {
+  href: '',
+  focus: false,
+  visibility: '',
+  visibleOnFocus: false,
+  textContent: '',
+};
+
+export const exampleArgs = {
+  ...defaultArgs,
+  href: '#main',
+  focus: true,
+  textContent: 'Skip to main content',
+};
+
+export const SkipLink = ({
+  focus = defaultArgs.focus,
+  href = defaultArgs.href,
+  textContent = defaultArgs.textContent,
+  visibility = defaultArgs.visibility,
+  visibleOnFocus = defaultArgs.visibleOnFocus,
+}) => (
+  <a
+    href={href}
+    class={clsx('utrecht-skip-link', {
+      'utrecht-skip-link--focus': focus,
+      'utrecht-skip-link--visible-on-focus': visibleOnFocus,
+      'utrecht-skip-link--hidden': visibility === 'hidden',
+      'utrecht-skip-link--visible': visibility === 'visible',
+    })}
+  >
+    {textContent}
+  </a>
+);
+
+export default SkipLink;
