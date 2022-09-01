273cdc706cfe1cb091304bef8583f4464aed392d
Yolijn <mail@yolijn.com>
2022-09-01 16:44:38 +0200

    docs: use react template for page

--- components/page/css/story-template.jsx
+++ components/page/css/story-template.jsx
@@ -3,7 +3,7 @@
  * Copyright (c) 2022 Gemeente Utrecht
  * Copyright (c) 2022 Frameless B.V.
  */
-import parser from 'html-react-parser';
+import parse from 'html-react-parser';
 import React from 'react';
 
 export const argTypes = {
@@ -14,11 +14,13 @@ export const argTypes = {
 };
 
 export const defaultArgs = {
+  innerHTML: '',
+};
+
+export const exampleArgs = {
   innerHTML: `<header class="utrecht-page-header">Header area</header><div className="utrecht-page-content"><main className="utrecht-page-content__main">Content area</main></div><footer class="utrecht-page-footer">Footer area</footer>`,
 };
 
-export const Page = ({ innerHTML }) => (
-  <div class="utrecht-page">{innerHTML && typeof innerHTML === 'string' ? parser(innerHTML) : innerHTML}</div>
-);
+export const Page = ({ innerHTML = defaultArgs.innerHTML }) => <div className="utrecht-page">{parse(innerHTML)}</div>;
 
 export default Page;
--- components/page/web-component/story-template.jsx
+++ components/page/web-component/story-template.jsx
@@ -3,7 +3,7 @@
  * Copyright (c) 2022 Gemeente Utrecht
  * Copyright (c) 2022 Frameless B.V.
  */
-import parser from 'html-react-parser';
+import parse from 'html-react-parser';
 import React from 'react';
 
 export const argTypes = {
@@ -14,9 +14,11 @@ export const argTypes = {
 };
 
 export const defaultArgs = {
+  innerHTML: '',
+};
+
+export const exampleArgs = {
   innerHTML: `<utrecht-page-header>Header content</utrecht-page-header><utrecht-page-content>Page content</utrecht-page-content><utrecht-page-footer>Footer content</utrecht-page-footer>`,
 };
 
-export const Page = ({ innerHTML }) => (
-  <utrecht-page>{innerHTML && typeof innerHTML === 'string' ? parser(innerHTML) : innerHTML}</utrecht-page>
-);
+export const Page = ({ innerHTML = defaultArgs.innerHTML }) => <utrecht-page>{parse(innerHTML)}</utrecht-page>;
