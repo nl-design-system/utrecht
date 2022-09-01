873327b66f7e31288cfc8cecec1466b05f87cf16
Yolijn <mail@yolijn.com>
2022-09-01 16:24:25 +0200

    docs: use react template for heading-1

---
+++ components/heading-1/css/story-template.jsx
@@ -0,0 +1,40 @@
+/*
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2022 Gemeente Utrecht
+ * Copyright (c) 2020-2022 Frameless B.V.
+ */
+
+import clsx from 'clsx';
+import React from 'react';
+
+export const argTypes = {
+  textContent: {
+    description: 'De content van de header',
+    control: 'text',
+  },
+  distanced: {
+    description: 'Zet afstand tussen de header en nabije content',
+    control: 'boolean',
+  },
+};
+
+export const defaultArgs = {
+  textContent: '',
+  distanced: false,
+};
+
+export const exampleArgs = {
+  textContent: 'The Quick Brown Fox Jumps Over The Lazy Dog',
+};
+
+export const Heading1 = ({ textContent = defaultArgs.textContent, distanced = defaultArgs.distanced }) => (
+  <h1
+    className={clsx('utrecht-heading-1', {
+      'utrecht-heading-1--distanced': distanced,
+    })}
+  >
+    {textContent}
+  </h1>
+);
+
+export default Heading1;
--- components/heading-1/html/template.js
+++
@@ -1,4 +0,0 @@
-export const Heading1 = ({ textContent = '' }) =>
-  `<section class="utrecht-html">
-  <h1>${textContent}</h1>
-</section>`;
