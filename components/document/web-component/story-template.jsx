b155ede633a75655e148a8dff25a2e21e6397024
Yolijn <mail@yolijn.com>
2022-09-01 15:41:48 +0200

    docs: use react template for document

---
+++ components/document/web-component/story-template.jsx
@@ -0,0 +1,28 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2022 Gemeente Utrecht
+ * Copyright (c) 2020-2022 Frameless B.V.
+ */
+
+import parse from 'html-react-parser';
+import React from 'react';
+
+export const argTypes = {
+  innerHTML: {
+    description: 'Document content',
+    control: 'text',
+  },
+};
+
+export const defaultArgs = {
+  innerHTML: '',
+};
+
+export const exampleArgs = {
+  innerHTML:
+    '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+};
+
+export const Document = ({ innerHTML = defaultArgs.innerHTML }) => (
+  <utrecht-document>{parse(innerHTML)}</utrecht-document>
+);
