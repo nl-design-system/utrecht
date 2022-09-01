d4b24b578ecef50782bcefb2bf5ce8bb4fdb57cf
Yolijn <mail@yolijn.com>
2022-09-01 16:47:38 +0200

    docs: use react template for templates

---
+++ components/templates/contact-card-template/web-component/story-template.jsx
@@ -0,0 +1,10 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2022 Gemeente Utrecht
+ * Copyright (c) 2022 Frameless B.V.
+ */
+import React from 'react';
+
+export const ContactCardTemplate = () => <utrecht-contact-card-template></utrecht-contact-card-template>;
+
+export default ContactCardTemplate;
