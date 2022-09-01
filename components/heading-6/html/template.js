cd645f247a3d44f81a7dc8ce0a721aae660185a9
Yolijn <mail@yolijn.com>
2022-09-01 16:26:06 +0200

    docs: use react template for heading-6

--- components/heading-6/html/template.js
+++
@@ -1,4 +0,0 @@
-export const Heading6 = ({ textContent = '' }) =>
-  `<section class="utrecht-html">
-  <h6>${textContent}</h6>
-</section>`;
--- components/heading-6/web-component/template.js
+++
@@ -1,4 +0,0 @@
-export const Heading6 = ({ textContent = '' }) =>
-  `<section>
-  <utrecht-heading-6>${textContent}</utrecht-heading-6>
-</section>`;
