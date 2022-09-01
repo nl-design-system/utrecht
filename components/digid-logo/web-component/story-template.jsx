b0fcd6e2c5ad96988472d728f6f6903bf41ce02d
Yolijn <mail@yolijn.com>
2022-09-01 15:39:54 +0200

    docs: use react template for digid-logo

---
+++ components/digid-logo/web-component/story-template.jsx
@@ -0,0 +1,9 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2022 Gemeente Utrecht
+ * Copyright (c) 2020-2022 Frameless B.V.
+ */
+
+import React from 'react';
+
+export const DigidLogo = () => <utrecht-digid-logo></utrecht-digid-logo>;
--- components/digid-logo/web-component/template.js
+++
@@ -1,1 +0,0 @@
-export const DigidLogo = () => `<utrecht-digid-logo></utrecht-digid-logo>`;
