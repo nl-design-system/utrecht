76b54b3471857f3317afa497c48674276cfe2343
Yolijn <mail@yolijn.com>
2022-09-01 15:42:58 +0200

    docs: use react template for eidas-logo

---
+++ components/eidas-logo/web-component/story-template.jsx
@@ -0,0 +1,9 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2022 Gemeente Utrecht
+ * Copyright (c) 2020-2022 Frameless B.V.
+ */
+
+import React from 'react';
+
+export const EidasLogo = () => <utrecht-eidas-logo></utrecht-eidas-logo>;
