7c8f31debf2f67e26e2c07e9d927ffe476970c7f
Yolijn <mail@yolijn.com>
2022-09-01 16:27:53 +0200

    docs: use react template for icon

--- components/icon/web-component/template.js
+++
@@ -1,7 +0,0 @@
-export const IconExample = ({
-  color = 'blue',
-  icon = 'utrecht-icon-arrow',
-  size = 12,
-}) => `<div style="--utrecht-icon-color: ${color}; --utrecht-icon-size: ${size}px;">
-  <${icon}></${icon}>
-</div>`;
