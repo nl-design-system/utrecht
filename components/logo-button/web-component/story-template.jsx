8612d4182989579fe5f87bdf7c0f18aa7fd2cf45
Yolijn <mail@yolijn.com>
2022-09-01 16:42:52 +0200

    docs: use react template for logo-button

---
+++ components/logo-button/web-component/story-template.jsx
@@ -0,0 +1,17 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2022 Gemeente Utrecht
+ * Copyright (c) 2020-2022 Frameless B.V.
+ */
+
+import React from 'react';
+
+export const DigidButton = () => (
+  <utrecht-digid-button>
+    <utrecht-button appearance="primary-action-button">
+      Inloggen <utrecht-icon-arrow></utrecht-icon-arrow>
+    </utrecht-button>
+  </utrecht-digid-button>
+);
+
+export default DigidButton;
