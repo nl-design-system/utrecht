897a0203abb38e95109aaa013dda257023a79e12
Yolijn <mail@yolijn.com>
2022-09-01 16:25:34 +0200

    docs: use react template for heading-3

--- components/heading-3/html/template.js
+++
@@ -1,4 +0,0 @@
-export const Heading3 = ({ textContent = '' }) =>
-  `<section class="utrecht-html">
-  <h3>${textContent}</h3>
-</section>`;
--- components/heading-3/web-component/template.js
+++
@@ -1,4 +0,0 @@
-export const Heading3 = ({ textContent = '' }) =>
-  `<section>
-  <utrecht-heading-3>${textContent}</utrecht-heading-3>
-</section>`;
