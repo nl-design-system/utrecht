4dc44a3edd5737a4d440758c6e515728616b6508
Ali Amori Kadhim <ali@frameless.io>
2022-09-05 11:43:26 +0200

    docs: add react heading group stories

---
+++ packages/storybook-react/src/stories/HeadingGroup/HeadingGroup.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Heading1, HeadingGroup, Paragraph } from '@utrecht/component-library-react';
+import React from 'react';
+
+export default {
+  title: 'React.js Component/Heading Group',
+  id: 'react-heading-group',
+  component: HeadingGroup,
+} as ComponentMeta<typeof HeadingGroup>;
+
+const Template: ComponentStory<typeof HeadingGroup> = () => (
+  <HeadingGroup style={{ '--utrecht-space-around': 1 } as any}>
+    <Heading1>The Quick Brown Fox Jumps Over The Lazy Dog</Heading1>
+    <Paragraph>The Quick Brown Fox Jumps Over The Lazy Dog</Paragraph>
+  </HeadingGroup>
+);
+
+export const Default = Template.bind({});
