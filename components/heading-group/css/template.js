2873f71691ce94ce56ed59289d873b130c7d5bcc
Ali Amori Kadhim <ali@frameless.io>
2022-09-05 11:42:34 +0200

    docs: add css heading group stories

---
+++ components/heading-group/css/template.js
@@ -0,0 +1,7 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+export const HeadingGroup = ({ children }) =>
+  `<hgroup class="utrecht-heading-group" style="--utrecht-space-around: 1"> ${children}</hgroup>`;
