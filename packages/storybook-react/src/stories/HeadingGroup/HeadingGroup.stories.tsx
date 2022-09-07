88926a278aa32ebc512a35dc975a5eb362f6497d
Robbert Broersma <git@robbertbroersma.nl>
2022-09-07 17:47:54 +0200

    docs: show HeadingGroup with PreHeading in React

--- packages/storybook-react/src/stories/HeadingGroup/HeadingGroup.stories.tsx
+++ packages/storybook-react/src/stories/HeadingGroup/HeadingGroup.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Heading1, HeadingGroup, Paragraph } from '@utrecht/component-library-react';
+import { Heading1, HeadingGroup, Paragraph, PreHeading } from '@utrecht/component-library-react';
 import React from 'react';
 
 export default {
@@ -16,3 +16,12 @@ const Template: ComponentStory<typeof HeadingGroup> = () => (
 );
 
 export const Default = Template.bind({});
+
+const PreHeadingTemplate: ComponentStory<typeof HeadingGroup> = () => (
+  <HeadingGroup style={{ '--utrecht-space-around': 1 } as any}>
+    <Heading1>The Quick Brown Fox Jumps Over The Lazy Dog</Heading1>
+    <PreHeading>The Quick Brown Fox Jumps Over The Lazy Dog</PreHeading>
+  </HeadingGroup>
+);
+
+export const WithPreHeading = PreHeadingTemplate.bind({});
