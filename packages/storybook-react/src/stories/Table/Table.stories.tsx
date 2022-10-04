1dad282b569dd46ee82c8fb849f3be09f8358c1e
Robbert Broersma <git@robbertbroersma.nl>
2022-10-04 15:33:30 +0200

    docs: include React Table component in Storybook

---
+++ packages/storybook-react/src/stories/Table/Table.stories.tsx
@@ -0,0 +1,47 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+} from '@utrecht/component-library-react';
+import React from 'react';
+
+export default {
+  title: 'React.js Component/Table',
+  id: 'react-table',
+  component: Table,
+} as ComponentMeta<typeof Table>;
+
+const Template: ComponentStory<typeof Table> = () => (
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHeaderCell scope="col">Column A</TableHeaderCell>
+        <TableHeaderCell scope="col">Column B</TableHeaderCell>
+        <TableHeaderCell scope="col">Column C</TableHeaderCell>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableHeaderCell scope="row">Item 1</TableHeaderCell>
+        <TableCell>Data</TableCell>
+        <TableCell>More data</TableCell>
+      </TableRow>
+    </TableBody>
+    <TableFooter>
+      <TableRow>
+        <TableCell>More data</TableCell>
+        <TableCell>More data</TableCell>
+        <TableCell>More data</TableCell>
+      </TableRow>
+    </TableFooter>
+  </Table>
+);
+
+export const Default = Template.bind({});
+
+Default.args = {};
