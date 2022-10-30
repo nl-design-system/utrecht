791a46a679f5a55d9b4295644ef2f0f8755bb5ed
Robbert Broersma <git@robbertbroersma.nl>
2022-10-30 12:21:34 +0100

    feat: add button group language switcher

---
+++ packages/storybook-react/src/stories/AlternateLangNav/AlternateLangButtonGroup.stories.tsx
@@ -0,0 +1,53 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ButtonGroup, Heading, LinkButton } from '@utrecht/component-library-react';
+import React from 'react';
+
+export default {
+  title: 'React.js Component/Alternate language navigation/Button group',
+  id: 'react-alternate-lang-button-group',
+  component: LinkButton,
+} as ComponentMeta<typeof LinkButton>;
+
+const Template: ComponentStory<typeof LinkButton> = () => {
+  const args = {
+    heading: 'Taal kiezen',
+    headingId: '0103ec04-5ef6-41e6-b5f7-2fa7f60b0ed9',
+    headingLevel: 2,
+    items: [
+      {
+        label: 'This page in English',
+        lang: 'en',
+        current: true,
+        textContent: 'EN',
+      },
+      {
+        label: 'Deze pagina in Nederlands',
+        lang: 'nl',
+        current: false,
+        textContent: 'NL',
+      },
+    ],
+  };
+  const { heading, headingId, headingLevel, items } = args;
+
+  return (
+    <section className="utrecht-alternate-lang-nav" aria-labelledby={headingId}>
+      <Heading level={headingLevel} className="utrecht-alternate-lang-nav__heading" id={headingId}>
+        {heading}
+      </Heading>
+      <ButtonGroup>
+        {items.map(({ current, label, lang, textContent }) => (
+          <LinkButton inline pressed={current} lang={lang} aria-label={label} key={lang}>
+            {textContent}
+          </LinkButton>
+        ))}
+      </ButtonGroup>
+    </section>
+  );
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: <></>,
+};
--- packages/storybook-react/src/stories/AlternateLangNav/AlternateLangNav.stories.tsx
+++ packages/storybook-react/src/stories/AlternateLangNav/AlternateLangNav.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Link } from '@utrecht/component-library-react';
+import { Heading, Link } from '@utrecht/component-library-react';
 import React from 'react';
 
 export default {
@@ -8,31 +8,43 @@ export default {
   component: Link,
 } as ComponentMeta<typeof Link>;
 
-const Template: ComponentStory<typeof Link> = () => (
-  <nav>
-    <Link
-      aria-current="page"
-      className="utrecht-link--current-lang"
-      href="https://example.com/en"
-      hrefLang="en"
-      lang="en"
-      title="This page in English"
-    >
-      EN
-    </Link>
-    <span aria-hidden="true">{' | '}</span>
-    <Link
-      className="utrecht-link--alternate-lang"
-      href="https://example.com/nl"
-      hrefLang="nl"
-      lang="nl"
-      rel="alternate"
-      title="Deze pagina in Nederlands"
-    >
-      NL
-    </Link>
-  </nav>
-);
+const Template: ComponentStory<typeof Link> = () => {
+  const args = {
+    heading: 'Taal kiezen',
+    headingId: '0103ec04-5ef6-41e6-b5f7-2fa7f60b0ed9',
+    headingLevel: 2,
+  };
+  const { heading, headingId, headingLevel } = args;
+
+  return (
+    <nav className="utrecht-alternate-lang-nav" aria-labelledby={headingId}>
+      <Heading level={headingLevel} className="utrecht-alternate-lang-nav__heading" id={headingId}>
+        {heading}
+      </Heading>
+      <Link
+        aria-current="page"
+        className="utrecht-link--current-lang"
+        href="https://example.com/en"
+        hrefLang="en"
+        lang="en"
+        title="This page in English"
+      >
+        EN
+      </Link>
+      <span aria-hidden="true">{' | '}</span>
+      <Link
+        className="utrecht-link--alternate-lang"
+        href="https://example.com/nl"
+        hrefLang="nl"
+        lang="nl"
+        rel="alternate"
+        title="Deze pagina in Nederlands"
+      >
+        NL
+      </Link>
+    </nav>
+  );
+};
 
 export const Default = Template.bind({});
 
