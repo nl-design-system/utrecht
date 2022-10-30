f5a4939a196b01eb3583f4cc2021f46e81f90d73
Robbert Broersma <git@robbertbroersma.nl>
2022-10-30 12:21:34 +0100

    feat: add button group language switcher

--- components/alternate-lang-nav/README.md
+++ components/alternate-lang-nav/README.md
@@ -4,11 +4,15 @@
 
 ## HTML
 
+Use the `<nav>` with `<a>` element approach for hyperlinks to alternate languages. Use the button group with toggle button approach (`role="region"` with `<button aria-pressed="true">`) for single page apps where language is replaced using client-side scripting.
+
 - Use `aria-current="page"` to annotate the link to the page the use is on currently.
 
 - Use a word separator between the links to keep the links comprehensible without CSS, so do not write `<a>EN</a><a>NL</a>`.
 
-- The links are best discoverable inside a [navigation region](https://www.w3.org/TR/wai-aria-1.1/#navigation), so inside a `role="navigation"` or `<nav>` element.
+- The links are best discoverable inside a [navigation landmark region](https://www.w3.org/TR/wai-aria-1.1/#navigation), so inside a `role="navigation"` or `<nav>` element.
+
+- The buttons are best discoverable inside a `role="region"` landmark. Provide a label for the landmark, for example by using `aria-label`. <!-- Hmm... isn't <section> + <h2> preferable to using ARIA? -->
 
 - Since it is not possible to provide alternate content for every language, it can help users that only understand other languages to use automatic translation tools such as [Google Translate](https://translate.google.com/). Ensure the language of the [current page is declared](https://www.w3.org/International/questions/qa-html-language-declarations.en) using `<html lang="…">`, so the browser can recognize the page is in a foreign language and suggest to enable automatic translation. Provide server side-rendering to improve the ability of those tools to retrieve the text content for translation.
 
--- components/alternate-lang-nav/css/story-template.jsx
+++ components/alternate-lang-nav/css/story-template.jsx
@@ -5,9 +5,15 @@
  */
 
 import React from 'react';
-import Link from '../../link/css/story-template';
+import { ButtonGroup } from '../../button-group/css/story-template';
+import { LinkButton } from '../../link-button/css/story-template';
+import { Link } from '../../link/css/story-template';
+import './index.scss';
 
 export const argTypes = {
+  headingLevel: {
+    description: 'Heading level',
+  },
   languages: {
     description: 'Languages',
     type: {
@@ -18,19 +24,35 @@ export const argTypes = {
 };
 
 export const defaultArgs = {
+  headingLevel: 1,
   languages: [],
 };
 
 export const exampleArgs = {
+  headingLevel: 2,
   languages: [
     { textContent: 'EN', title: 'This page in English', lang: 'en', current: true, hrefLang: 'en' },
     { textContent: 'NL', title: 'Deze pagina in Nederlands', lang: 'nl', current: false, hrefLang: 'nl' },
   ],
 };
 
-export const AlternateLangNav = ({ languages = [] }) => {
+const Heading = ({ children, level, ...restProps }) => {
+  const HeadingX =
+    level === 2 ? 'h2' : level === 3 ? 'h3' : level === 4 ? 'h4' : level === 5 ? 'h5' : level === 6 ? 'h6' : 'h1';
+
+  return <HeadingX {...restProps}>{children}</HeadingX>;
+};
+
+export const AlternateLangNav = ({ headingLevel = defaultArgs.headingLevel, languages = [] }) => {
   return (
-    <div className="utrecht-alternate-lang-nav">
+    <nav className="utrecht-alternate-lang-nav" aria-labelledby="b9704d3a-6fb7-467b-9c35-55b3cd80c9c7">
+      <Heading
+        level={headingLevel}
+        className="utrecht-alternate-lang-nav__heading"
+        id="b9704d3a-6fb7-467b-9c35-55b3cd80c9c7"
+      >
+        Taal kiezen
+      </Heading>
       {languages.map((langLink, index, { length }) =>
         index < length - 1 && length > 1 ? (
           <React.Fragment key={index}>
@@ -41,7 +63,28 @@ export const AlternateLangNav = ({ languages = [] }) => {
           <Link {...langLink} key={index} />
         ),
       )}
-    </div>
+    </nav>
+  );
+};
+
+export const AlternateLangButtonGroup = ({ headingLevel = defaultArgs.headingLevel, languages = [] }) => {
+  return (
+    <section className="utrecht-alternate-lang-nav" aria-labelledby="1053c964-5c2c-46c7-b514-4d6a1462d7b4">
+      <Heading
+        level={headingLevel}
+        className="utrecht-alternate-lang-nav__heading"
+        id="1053c964-5c2c-46c7-b514-4d6a1462d7b4"
+      >
+        Taal kiezen
+      </Heading>
+      <ButtonGroup>
+        {languages.map(({ current, lang, textContent, title }, index) => (
+          <LinkButton pressed={!!current} aria-label={title} lang={lang} key={index}>
+            {textContent}
+          </LinkButton>
+        ))}
+      </ButtonGroup>
+    </section>
   );
 };
 
