eb484b67328b64dfe559ea674cd5533d29c740bb
Robbert Broersma <git@robbertbroersma.nl>
2022-11-10 19:40:52 +0100

    docs: improve article story (use components, not HTML)

--- components/article/css/story-template.jsx
+++ components/article/css/story-template.jsx
@@ -6,6 +6,8 @@
 
 import parse from 'html-react-parser';
 import React from 'react';
+import { Heading1 } from '../../heading-1/css/story-template';
+import { Paragraph } from '../../paragraph/css/story-template';
 
 export const argTypes = {
   htmlContent: {
@@ -19,14 +21,25 @@ export const defaultArgs = {
 };
 
 export const exampleArgs = {
-  htmlContent: `<h1>Lorem ipsum</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-  aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-  occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`,
+  children: (
+    <>
+      <Heading1>Lorem ipsum</Heading1>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </Paragraph>
+    </>
+  ),
 };
 
-export const Article = ({ htmlContent = defaultArgs.htmlContent }) => (
-  <div className="utrecht-article">{parse(htmlContent)}</div>
+export const Article = ({ children, htmlContent = defaultArgs.htmlContent }) => (
+  <div className="utrecht-article">
+    {parse(htmlContent)}
+    {children}
+  </div>
 );
 
 export default Article;
