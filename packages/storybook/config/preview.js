aa3129f9eacea1bdc043fa2439222db35410f22b
Robbert Broersma <git@robbertbroersma.nl>
2022-12-20 12:25:55 +0100

    docs: toolbar option to disable Storybook CSS or only design tokens

--- packages/storybook/config/preview.js
+++ packages/storybook/config/preview.js
@@ -148,6 +148,8 @@ const addonThemes = {
     default: 'Gemeente Utrecht',
     target: 'root',
     list: [
+      { name: 'No style', class: 'no-style', color: '#000000' },
+      { name: 'No design tokens', class: 'no-theme', color: '#CCCCCC' },
       { name: 'Gemeente Utrecht', class: 'utrecht-theme', color: '#CC0000' },
       { name: 'Gemeente Utrecht (dark mode)', class: 'utrecht-theme--color-scheme-dark', color: '#000000' },
     ],
