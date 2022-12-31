5c936e9ad8a2e8fb859328ceb64b7a738026a188
Robbert Broersma <git@robbertbroersma.nl>
2022-12-31 14:19:50 +0100

    docs: include Jest tests in React Storybook

--- packages/storybook-react/config/preview.js
+++ packages/storybook-react/config/preview.js
@@ -1,8 +1,13 @@
+import { withTests } from '@storybook/addon-jest';
+import results from '@utrecht/component-library-react/dist/.jest-test-results.json';
 import '@utrecht/components/html-content/css/index.scss';
 import '@utrecht/components/index.scss';
 import '@utrecht/design-tokens/dist/index.css';
 
-export const decorators = [(Story) => <div className="utrecht-document utrecht-theme">{Story()}</div>];
+export const decorators = [
+  (Story) => <div className="utrecht-document utrecht-theme">{Story()}</div>,
+  withTests({ results }),
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
