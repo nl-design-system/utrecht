abe71de814e129bd85f6acccf6fb74387e284956
Ali Amori Kadhim <ali@frameless.io>
2023-01-06 15:54:25 +0100

    test: integrate jest into component library angular

---
+++ packages/component-library-angular/jest.config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+};
