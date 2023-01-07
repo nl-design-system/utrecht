8585f5f7d0a2866207a961d5d923240dcb7345d3
Robbert Broersma <git@robbertbroersma.nl>
2023-01-07 21:58:19 +0100

    build: migrate Storybook for Vue.js to Vite

--- packages/storybook-vue/config/preview.js
+++ packages/storybook-vue/config/preview.js
@@ -1,53 +1,7 @@
 import { withTests } from '@storybook/addon-jest';
-import { app } from '@storybook/vue3';
-import {
-  BadgeStatus,
-  Button,
-  Checkbox,
-  FormField,
-  FormFieldset,
-  FormFieldsetLegend,
-  FormLabel,
-  Heading,
-  PageContent,
-  PageFooter,
-  PageHeader,
-  Paragraph,
-  Select,
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHeader,
-  TableHeaderCell,
-  TableRow,
-} from '@utrecht/component-library-vue';
 import results from '@utrecht/component-library-vue/dist/.jest-test-results.json';
 import '@utrecht/component-library-vue/dist/style.css';
 import '@utrecht/design-tokens/dist/index.css';
-
-app.component('BadgeStatus', BadgeStatus);
-app.component('Button', Button);
-app.component('Checkbox', Checkbox);
-app.component('FormFieldset', FormFieldset);
-app.component('FormFieldsetLegend', FormFieldsetLegend);
-app.component('FormField', FormField);
-app.component('FormLabel', FormLabel);
-app.component('Heading', Heading);
-app.component('PageContent', PageContent);
-app.component('PageFooter', PageFooter);
-app.component('PageHeader', PageHeader);
-app.component('Paragraph', Paragraph);
-app.component('Select', Select);
-app.component('Table', Table);
-app.component('TableBody', TableBody);
-app.component('TableCaption', TableCaption);
-app.component('TableCell', TableCell);
-app.component('TableFooter', TableFooter);
-app.component('TableHeader', TableHeader);
-app.component('TableHeaderCell', TableHeaderCell);
-app.component('TableRow', TableRow);
 
 export const decorators = [
   (story) => ({
