47aa651aaec83eff26c37d7e6cfc7e127f0bfea4
Robbert Broersma <git@robbertbroersma.nl>
2023-01-07 21:58:19 +0100

    build: migrate Storybook for Vue.js to Vite

--- packages/storybook-vue/config/main.js
+++ packages/storybook-vue/config/main.js
@@ -1,15 +1,10 @@
 /* eslint-disable no-undef */
 module.exports = {
-  core: {
-    builder: 'webpack5',
-    disableTelemetry: true,
-  },
   features: {
-    babelModeV7: true,
     postcss: false,
     buildStoriesJson: true,
     previewMdx2: true,
-    storyStoreV7: false,
+    storyStoreV7: true,
   },
   stories: ['../src/**/*.stories.@(js|jsx|mdx|ts|tsx)'],
   addons: [
@@ -20,18 +15,11 @@ module.exports = {
     '@storybook/addon-jest',
   ],
   framework: {
-    name: '@storybook/vue3-webpack5',
+    name: '@storybook/vue3-vite',
     options: {},
   },
   staticDirs: ['../../../proprietary/assets'],
-  webpackFinal: async (config) => ({
-    ...config,
-    performance: {
-      // Disable warning for: "asset size exceeds the recommended limit (244 KiB)"
-      hints: false,
-    },
-  }),
   docs: {
-    autodocs: true,
+    autodocs: 'tag',
   },
 };
