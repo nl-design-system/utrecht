dfbbb3354ec3bbd1b960997caa363f2bc0d599f7
Robbert Broersma <git@robbertbroersma.nl>
2023-01-12 20:03:52 +0100

    feat: invalid form field in Vue

--- packages/component-library-vue/src/FormField.vue
+++ packages/component-library-vue/src/FormField.vue
@@ -1,13 +1,26 @@
 <script lang="ts">
-  import { defineComponent } from "vue";
+  import { defineComponent, type PropType } from "vue";
 
+  type FormFieldTypes = "checkbox" | "radio" | "text" | undefined;
   export default defineComponent({
     name: "UtrechtFormField",
+    props: {
+      invalid: { type: Boolean, required: false, default: false },
+      type: { type: String as PropType<FormFieldTypes>, required: false, default: undefined },
+    },
   });
 </script>
 
 <template>
-  <div class="utrecht-form-field">
+  <div
+    :class="[
+      'utrecht-form-field',
+      { 'utrecht-form-field--invalid': invalid },
+      { 'utrecht-form-field--checkbox': type === 'checkbox' },
+      { 'utrecht-form-field--radio': type === 'radio' },
+      { 'utrecht-form-field--text': !type || type === 'text' },
+    ]"
+  >
     <slot />
   </div>
 </template>
