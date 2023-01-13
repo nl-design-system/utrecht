f407182e7c287cf5298ac7467dbff3fc1bab67f7
Ali Amori Kadhim <ali@frameless.io>
2023-01-13 17:09:29 +0100

    feat: change implement angular document component
    BREAKING CHANGE: deprecated `<utrecht-document />` new way is <div utrecht-document />
    
    the reason why?
    that way allow us to have all the html attributes available!

--- packages/component-library-angular/src/document/component.ts
+++ packages/component-library-angular/src/document/component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'utrecht-document',
+  selector: '[utrecht-document]',
   templateUrl: 'index.html',
   styleUrls: ['index.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.utrecht-document]': 'true',
+  },
 })
 export class UtrechtDocument {
   constructor() {}
