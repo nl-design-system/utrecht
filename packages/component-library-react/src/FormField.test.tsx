33f623865aa4a4eb76069b5ab3d1d8e5259bfe94
savitris <11764984+savitris@users.noreply.github.com>
2023-01-31 13:14:20 +0100

    refactor: duplicate deletion

--- packages/component-library-react/src/FormField.test.tsx
+++ packages/component-library-react/src/FormField.test.tsx
@@ -65,14 +65,7 @@ describe('Form field', () => {
     expect(field).toHaveClass('large');
     expect(field).toHaveClass('utrecht-form-field');
   });
-  it('can have a additional class name', () => {
-    const { container } = render(<FormField className="large" />);
 
-    const field = container.querySelector(':only-child');
-
-    expect(field).toHaveClass('large');
-    expect(field).toHaveClass('utrecht-form-field');
-  });
   it('supports ForwardRef in React', () => {
     const ref = createRef<HTMLDivElement>();
 
