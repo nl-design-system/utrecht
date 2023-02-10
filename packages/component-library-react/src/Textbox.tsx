4e4aaae4dce16b390433ed6b379db154d145a961
Ali Amori Kadhim <ali@frameless.io>
2023-02-10 16:51:06 +0100

    feat: add `novalidate` property add textbox react component

--- packages/component-library-react/src/Textbox.tsx
+++ packages/component-library-react/src/Textbox.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import { ForwardedRef, forwardRef, InputHTMLAttributes } from 'react';
+
 export type TextboxTypes =
   | 'date'
   | 'datetime-local'
@@ -16,6 +17,7 @@ export type TextboxTypes =
 
 export interface TextboxProps extends InputHTMLAttributes<HTMLInputElement> {
   invalid?: boolean;
+  novalidate?: boolean;
   type?: TextboxTypes;
 }
 
@@ -28,33 +30,35 @@ export const Textbox = forwardRef(
       required,
       className,
       type = 'text',
-      maxLength,
       inputMode,
+      novalidate = false,
       ...restProps
     }: TextboxProps,
     ref: ForwardedRef<HTMLInputElement>,
-  ) => (
-    <input
-      {...restProps}
-      ref={ref}
-      type={type}
-      className={clsx(
-        'utrecht-textbox',
-        'utrecht-textbox--html-input',
-        disabled && 'utrecht-textbox--disabled',
-        invalid && 'utrecht-textbox--invalid',
-        readOnly && 'utrecht-textbox--readonly',
-        required && 'utrecht-textbox--required',
-        className,
-      )}
-      maxLength={maxLength}
-      disabled={disabled}
-      readOnly={readOnly}
-      required={required}
-      aria-invalid={invalid || undefined}
-      inputMode={inputMode || (type === 'number' ? 'numeric' : undefined)}
-    />
-  ),
+  ) => {
+    return (
+      <input
+        {...restProps}
+        ref={ref}
+        type={type}
+        className={clsx(
+          'utrecht-textbox',
+          'utrecht-textbox--html-input',
+          disabled && 'utrecht-textbox--disabled',
+          invalid && 'utrecht-textbox--invalid',
+          readOnly && 'utrecht-textbox--readonly',
+          required && 'utrecht-textbox--required',
+          className,
+        )}
+        disabled={disabled}
+        readOnly={readOnly}
+        required={required && !novalidate ? required : undefined}
+        aria-required={(required && novalidate) || undefined}
+        aria-invalid={invalid || undefined}
+        inputMode={inputMode || (type === 'number' ? 'numeric' : undefined)}
+      />
+    );
+  },
 );
 
 Textbox.displayName = 'Textbox';
