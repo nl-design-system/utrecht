5648c6722d2f0d93666714d5e0b64d516d60fae1
Ali Amori Kadhim <ali@frameless.io>
2023-02-17 18:01:38 +0100

    feat: update the search-bar component & add css stories

--- components/search-bar/css/story-template.jsx
+++ components/search-bar/css/story-template.jsx
@@ -3,7 +3,74 @@
  * Copyright (c) 2022 Gemeente Utrecht
  * Copyright (c) 2022 Frameless B.V.
  */
+import clsx from 'clsx';
 import React from 'react';
+
+export const SearchBarDropdown = ({ isSelected, isActive }) => (
+  <div className={clsx('utrecht-search-bar__dropdown')}>
+    <section className={clsx('utrecht-search-bar__section')}>
+      <span className={clsx('utrecht-search-bar__section-title')}>Section #1</span>
+      <ul className={clsx('utrecht-unordered-list')} role="listbox">
+        <li
+          role="option"
+          className={clsx('utrecht-search-bar__list-item', 'utrecht-unordered-list__item')}
+          id="item-1-section-1"
+          aria-selected={false}
+        >
+          Option #1
+        </li>
+        <li
+          role="option"
+          className={clsx('utrecht-search-bar__list-item', 'utrecht-unordered-list__item')}
+          id="item-2-section-1"
+          aria-selected={false}
+        >
+          Option #2
+        </li>
+        <li
+          role="option"
+          className={clsx('utrecht-search-bar__list-item', 'utrecht-unordered-list__item')}
+          id="item-3-section-1"
+          aria-selected={false}
+        >
+          Option #3
+        </li>
+      </ul>
+    </section>
+    <section className={clsx('utrecht-search-bar__section')}>
+      <span className={clsx('utrecht-search-bar__section-title')}>Section #2</span>
+      <ul className={clsx('utrecht-unordered-list')} role="listbox">
+        <li
+          role="option"
+          className={clsx('utrecht-search-bar__list-item', 'utrecht-unordered-list__item', {
+            'utrecht-search-bar__list-item--is-active': isActive,
+            'utrecht-search-bar__list-item--is-selected': isSelected,
+          })}
+          id="item-1-section-2"
+          aria-selected={isSelected}
+        >
+          Option #1
+        </li>
+        <li
+          role="option"
+          className={clsx('utrecht-search-bar__list-item', 'utrecht-unordered-list__item')}
+          id="item-2-section-2"
+          aria-selected={false}
+        >
+          Option #2
+        </li>
+        <li
+          role="option"
+          className={clsx('utrecht-search-bar__list-item', 'utrecht-unordered-list__item')}
+          id="item-3-section-2"
+          aria-selected={false}
+        >
+          Option #3
+        </li>
+      </ul>
+    </section>
+  </div>
+);
 
 export const argTypes = {
   buttonLabel: {
@@ -22,6 +89,15 @@ export const argTypes = {
     description: 'Current search term',
     type: 'text',
   },
+  isOpen: {
+    type: 'boolean',
+  },
+  isActive: {
+    type: 'boolean',
+  },
+  isSelected: {
+    type: 'boolean',
+  },
 };
 
 export const defaultArgs = {
@@ -29,13 +105,19 @@ export const defaultArgs = {
   formLabel: '',
   inputLabel: '',
   value: '',
+  isOpen: false,
+  isActive: false,
+  isSelected: false,
 };
 
 export const exampleArgs = {
   buttonLabel: 'Zoeken',
   formLabel: 'zoeken in Utrecht.nl',
   inputLabel: 'zoektermen',
-  value: 'The answer to life, the universe, and everything',
+  value: '',
+  isOpen: false,
+  isActive: false,
+  isSelected: false,
 };
 
 export const SearchBar = ({
@@ -43,24 +125,40 @@ export const SearchBar = ({
   formLabel = defaultArgs.formLabel,
   inputLabel = defaultArgs.inputLabel,
   value = defaultArgs.value,
+  isOpen = defaultArgs.isOpen,
+  isActive = defaultArgs.isActive,
+  isSelected = defaultArgs.isSelected,
 }) => (
-  <form className="utrecht-search-bar" role="search" aria-label={formLabel}>
-    <input
-      type="search"
-      className="utrecht-search-bar__input utrecht-textbox utrecht-textbox utrecht-textbox--html-input"
-      name="q"
-      autocomplete="off"
-      spellcheck="false"
-      value={value}
-      aria-label={inputLabel}
-    />
-    <button
-      type="submit"
-      value="Zoeken"
-      className="utrecht-search-bar__button utrecht-button utrecht-button--primary-action"
-    >
-      {buttonLabel}
-    </button>
+  <form
+    role="search"
+    aria-label={formLabel}
+    aria-haspopup="listbox"
+    aria-expanded={isOpen}
+    aria-owns={isOpen ? 'utrecht-search-bar-1-menu' : undefined}
+  >
+    <div className={clsx('utrecht-search-bar')}>
+      <input
+        type="search"
+        className={clsx('utrecht-search-bar__input', 'utrecht-textbox', 'utrecht-textbox--html-input')}
+        name="q"
+        autoComplete="off"
+        spellCheck="false"
+        aria-controls={isOpen ? 'utrecht-search-bar-1-menu' : undefined}
+        aria-activedescendant={isOpen ? 'item-1-section-2' : undefined} // list item id
+        value={value}
+        aria-label={inputLabel}
+        placeholder="Waar bent u naar op zoek?"
+        aria-autocomplete="list"
+      />
+      <button
+        type="submit"
+        value="Zoeken"
+        className={clsx('utrecht-search-bar__button', 'utrecht-button', 'utrecht-button--primary-action')}
+      >
+        {buttonLabel}
+      </button>
+    </div>
+    {isOpen && <SearchBarDropdown isActive={isActive} isSelected={isSelected} />}
   </form>
 );
 
