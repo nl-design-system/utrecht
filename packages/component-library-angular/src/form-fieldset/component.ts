408b0cd7cb23250ddc141e6c13393085abe4ac12
AndreaBusse29 <andrea.busse@the-experts.nl>
2023-02-20 17:34:20 +0100

    feat: extended angular fieldset component and added unit tests for it

--- packages/component-library-angular/src/form-fieldset/component.ts
+++ packages/component-library-angular/src/form-fieldset/component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: '[utrecht-fieldset]',
@@ -7,9 +7,27 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   host: {
+    '[attr.name]': 'name',
+    '[attr.form]': 'form',
+    '[attr.role]': 'role',
+    '[attr.aria-invalid]': 'invalid || undefined',
+    '[attr.aria-describedby]': 'ariaDescribedby',
+    '[attr.aria-labelledby]': 'ariaLabelledBy',
+    '[attr.aria-label]': 'ariaLabel',
     '[class.utrecht-fieldset]': 'true',
+    '[class.utrecht-form-fieldset--html-fieldset]': 'true',
+    '[class.utrecht-form-fieldset--disabled]': 'disabled || null',
+    '[class.utrecht-form-fieldset--invalid]': 'invalid || null',
   },
 })
 export class UtrechtFieldsetAttr {
+  @Input() disabled?: boolean = false;
+  @Input() invalid?: boolean = false;
+  @Input() ariaDescribedby?: string = '';
+  @Input() ariaLabel?: string = '';
+  @Input() ariaLabelledBy?: string = '';
+  @Input() form?: string = '';
+  @Input() name?: string = '';
+  @Input() role?: string = '';
   constructor() {}
 }
