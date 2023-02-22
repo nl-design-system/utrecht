dfdd9add04017885df04c1411ba75ac1078a161a
Robbert Broersma <git@robbertbroersma.nl>
2023-02-22 12:30:24 +0100

    refactor: simplify implementation of Angular fieldset

--- packages/component-library-angular/src/form-fieldset/component.ts
+++ packages/component-library-angular/src/form-fieldset/component.ts
@@ -7,13 +7,6 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   host: {
-    '[attr.name]': 'name',
-    '[attr.form]': 'form',
-    '[attr.role]': 'role',
-    '[attr.aria-invalid]': 'invalid || undefined',
-    '[attr.aria-describedby]': 'ariaDescribedby',
-    '[attr.aria-labelledby]': 'ariaLabelledBy',
-    '[attr.aria-label]': 'ariaLabel',
     '[class.utrecht-fieldset]': 'true',
     '[class.utrecht-form-fieldset--html-fieldset]': 'true',
     '[class.utrecht-form-fieldset--disabled]': 'disabled || null',
@@ -23,11 +16,5 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
 export class UtrechtFieldsetAttr {
   @Input() disabled?: boolean = false;
   @Input() invalid?: boolean = false;
-  @Input() ariaDescribedby?: string = '';
-  @Input() ariaLabel?: string = '';
-  @Input() ariaLabelledBy?: string = '';
-  @Input() form?: string = '';
-  @Input() name?: string = '';
-  @Input() role?: string = '';
   constructor() {}
 }
