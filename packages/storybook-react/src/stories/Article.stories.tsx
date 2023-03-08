044f2515bd90f0c7a5aaee56756f844f5387e8e5
AndreaBusse29 <andrea.busse@the-experts.nl>
2023-03-08 17:43:09 +0100

    refactor: convert article component react story to storybook 7

--- packages/storybook-react/src/stories/Article.stories.tsx
+++ packages/storybook-react/src/stories/Article.stories.tsx
@@ -1,4 +1,5 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Description } from '@storybook/addon-docs';
+import { Meta } from '@storybook/react';
 import { Article, Heading1, Paragraph } from '@utrecht/component-library-react/dist/css-module/index';
 import readme from '@utrecht/components/article/README.md?raw';
 import tokensDefinition from '@utrecht/components/article/tokens.json';
@@ -6,39 +7,41 @@ import tokens from '@utrecht/design-tokens/dist/index.json';
 import React from 'react';
 import { designTokenStory } from './util';
 
-const meta = {
+const Documentation = () => <Description markdown={readme} />;
+export const Default = {
   title: 'React Component/Article',
   id: 'react-article',
   component: Article,
-  tags: ['autodocs'],
+  argTypes: {
+    children: {
+      name: 'Text content',
+      type: {
+        name: 'array',
+        required: true,
+      },
+    },
+  },
+  args: {
+    children: [
+      <Heading1>Lorem ipsum</Heading1>,
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </Paragraph>,
+    ],
+  },
   parameters: {
     tokensPrefix: 'utrecht-article',
     tokens,
     tokensDefinition,
     docs: {
-      description: {
-        component: readme,
-      },
+      page: Documentation,
     },
   },
-} as Meta<typeof Article>;
+} satisfies Meta<typeof Article>;
 
-export default meta;
-
-const Template: StoryObj<typeof Article> = (args) => <Article {...args} />;
-
-export const Default = Template.bind({});
-
-Default.args = {
-  children: [
-    <Heading1>Lorem ipsum</Heading1>,
-    <Paragraph>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </Paragraph>,
-  ],
-};
-
-export const DesignTokens = designTokenStory(meta);
+export default Default;
+export const DesignTokens = designTokenStory(Default);
