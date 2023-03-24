cd812cb38812b9be9e4888a0924776b76a7c3511
scar055 <48519289+scar055@users.noreply.github.com>
2023-03-24 16:18:20 +0100

    feat: add react badge counter component

---
+++ packages/component-library-react/src/BadgeCounter.tsx
@@ -0,0 +1,32 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2023 Gemeente Utrecht
+ * Copyright (c) 2023 Frameless B.V.
+ */
+
+import clsx from 'clsx';
+import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react';
+
+export interface BadgeCounterProps extends HTMLAttributes<HTMLElement> {
+  value?: string;
+}
+
+export const BadgeCounter = forwardRef(
+  (
+    { children, className, value, ...restProps }: PropsWithChildren<BadgeCounterProps>,
+    ref: ForwardedRef<HTMLElement | HTMLDataElement>,
+  ) => {
+    const props = {
+      children,
+      className: clsx('utrecht-badge-counter', className),
+      ...restProps,
+    };
+    return typeof value !== 'undefined' ? (
+      <data {...props} value={value} ref={ref as ForwardedRef<HTMLDataElement>} />
+    ) : (
+      <span {...props} ref={ref as ForwardedRef<HTMLElement>} />
+    );
+  },
+);
+
+BadgeCounter.displayName = 'BadgeCounter';
