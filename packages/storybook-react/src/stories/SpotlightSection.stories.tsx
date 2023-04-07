d69f311dba2c0246a509f174fafb3ee204a9729c
Robbert Broersma <git@robbertbroersma.nl>
2023-04-07 12:42:06 +0200

    docs: info and warning type spotlight section stories in React

--- packages/storybook-react/src/stories/SpotlightSection.stories.tsx
+++ packages/storybook-react/src/stories/SpotlightSection.stories.tsx
@@ -56,4 +56,16 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 export const Default: Story = {};
 
+export const Info: Story = {
+  args: {
+    type: 'info',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    type: 'warning',
+  },
+};
+
 export const DesignTokens = designTokenStory(meta);
