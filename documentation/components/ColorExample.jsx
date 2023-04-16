1e265326ab3e0ba56e1f64be0427362d03636ba3
Robbert Broersma <git@robbertbroersma.nl>
2023-04-16 22:34:08 +0200

    feat: improve ColorSample

--- documentation/components/ColorExample.jsx
+++ documentation/components/ColorExample.jsx
@@ -2,15 +2,18 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import '../../components/color-sample/css/index.scss';
 
-export const ColorExample = ({ color }) => (
+export const ColorExample = ({ color, children, style, ...restProps }) => (
   <data
     className="utrecht-color-sample"
     style={{
       color,
-      outline: '1px solid silver',
+      ...style,
     }}
     value={color}
-  ></data>
+    {...restProps}
+  >
+    {children}
+  </data>
 );
 
 ColorExample.propTypes = {
