9e15f4841478772d924e183874bc247779dd4d63
scar055 <48519289+scar055@users.noreply.github.com>
2023-04-18 15:43:54 +0200

    feat: react story book status badge added

---
+++ packages/storybook-react/src/stories/StatusBadge.stories.tsx
@@ -0,0 +1,55 @@
+import { ArgsTable, Description, Primary, PRIMARY_STORY, Stories } from '@storybook/addon-docs';
+import { Meta, StoryObj } from '@storybook/react';
+import { Paragraph, StatusBadge } from '@utrecht/component-library-react/dist/css-module/index';
+import readme from '@utrecht/components/badge-data/README.md?raw';
+import tokensDefinition from '@utrecht/components/badge-data/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import React from 'react';
+import { designTokenStory } from './util';
+
+const meta = {
+  title: 'React Component/Status badge',
+  id: 'react-status-badge',
+  component: StatusBadge,
+  args: {
+    children: 'Utrecht',
+  },
+  argTypes: {
+    status: {
+      name: 'value',
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: '' } },
+    },
+  },
+  parameters: {
+    tokensPrefix: 'utrecht-badge-status',
+    tokens,
+    tokensDefinition,
+    docs: {
+      page: () => (
+        <>
+          <Description>{readme}</Description>
+          <Primary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
+    },
+  },
+  decorators: [(Story) => <Paragraph>{Story()}</Paragraph>],
+} as Meta<typeof StatusBadge>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {};
+
+export const Value: Story = {
+  args: {
+    ...Default.args,
+    children: 'neutral',
+    value: 'neutral',
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
