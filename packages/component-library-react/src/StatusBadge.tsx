8b6c4a7e0ae110dcbf7175c1fda6b6819b9e670d
scar055 <48519289+scar055@users.noreply.github.com>
2023-04-18 11:53:46 +0200

    feat: add statusbadge

---
+++ packages/component-library-react/src/StatusBadge.tsx
@@ -0,0 +1,24 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2023 Gemeente Utrecht
+ * Copyright (c) 2023 Frameless B.V.
+ */
+
+import clsx from 'clsx';
+import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react';
+
+export interface StatusBadgeProps extends HTMLAttributes<HTMLElement> {
+  status?: string;
+}
+
+export const StatusBadge = forwardRef(
+  ({ className, status, ...restProps }: PropsWithChildren<StatusBadgeProps>, ref: ForwardedRef<HTMLElement>) => (
+    <span
+      className={clsx('utrecht-badge-status', `utrecht-badge-status--${status ? status : 'neutral'}`, className)}
+      {...restProps}
+      ref={ref as ForwardedRef<HTMLElement>}
+    />
+  ),
+);
+
+StatusBadge.displayName = 'StatusBadge';
