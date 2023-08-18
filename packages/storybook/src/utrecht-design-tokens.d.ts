48ac79470a9627d76ef6d65f7fce59319ae49dd5
Yolijn <yolijn@frameless.io>
2023-08-18 16:08:36 +0200

    chore: improve utrecht design token definition to match stylelintrc

--- packages/storybook/src/utrecht-design-tokens.d.ts
+++ packages/storybook/src/utrecht-design-tokens.d.ts
@@ -2,9 +2,6 @@ import 'react';
 
 declare module 'react' {
   export interface CSSProperties {
-    [key: `--utrecht-${string}`]: string | number | undefined;
-    [key: `--checkerboard-${string}`]: string | number | undefined;
-    [key: `background-${string}`]: string | number | undefined;
-    [key: `min-${string}`]: string | number | undefined;
+    [key: `--${'example' | 'denhaag' | 'utrecht'}-${string}`]: string | number | undefined;
   }
 }
