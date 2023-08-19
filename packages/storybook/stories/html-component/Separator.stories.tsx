ec48f853d46071aad10f60adb3a84ecf98a12448
Robbert Broersma <git@robbertbroersma.nl>
2023-08-19 11:52:48 +0200

    refactor: migrate HTML hr stories from mdx to tsx

---
+++ packages/storybook/stories/html-component/Separator.stories.tsx
@@ -0,0 +1,50 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/separator/README.md?raw';
+import tokensDefinition from '@utrecht/components/separator/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import React from 'react';
+import { htmlContentDecorator } from './decorator';
+import { designTokenStory } from '../components/util';
+
+const Separator = () => <hr />;
+
+const meta = {
+  title: 'HTML Component/Separator',
+  id: 'html-separator',
+  component: Separator,
+  decorators: [htmlContentDecorator],
+  argTypes: {},
+  args: {},
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-separator',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof Separator>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'Markup using the `<hr>` element.',
+      },
+    },
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
