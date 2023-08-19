6834e5027611836700f6b23b6e446da8346283d3
Robbert Broersma <git@robbertbroersma.nl>
2023-08-19 09:21:55 +0200

    refactor: migrate HTML article stories from mdx to tsx

---
+++ packages/storybook/stories/html-component/Article.stories.tsx
@@ -0,0 +1,65 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/article/README.md?raw';
+import tokensDefinition from '@utrecht/components/article/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import React from 'react';
+import { htmlContentDecorator } from './decorator';
+import { designTokenStory } from '../components/util';
+
+const Article = ({ children }) => <article>{children}</article>;
+
+const meta = {
+  title: 'HTML Component/Article',
+  id: 'html-article',
+  component: Article,
+  decorators: [htmlContentDecorator],
+  argTypes: {
+    children: {
+      description: 'Content of the article',
+    },
+  },
+  args: {
+    children: [
+      <h1>Lorem ipsum</h1>,
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </p>,
+    ],
+  },
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-article',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof Article>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'Markup using the `<article>` element.',
+      },
+    },
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
