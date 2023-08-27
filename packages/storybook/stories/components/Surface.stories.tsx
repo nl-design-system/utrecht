4211968a4930a275cf0580f47ca7eed1b2f38429
Robbert Broersma <git@robbertbroersma.nl>
2023-08-27 17:41:44 +0200

    refactor: migrate surface component from mdx to tsx

--- packages/storybook/stories/components/Surface.stories.tsx
+++ packages/storybook/stories/components/Surface.stories.tsx
@@ -1,37 +1,36 @@
 /* @license CC0-1.0 */
 
-import type { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading2, Paragraph, Surface } from '@utrecht/component-library-react/dist/css-module';
 import readme from '@utrecht/components/surface/README.md?raw';
 import themeColorDocs from '@utrecht/components/surface/_surface-theme-color.md?raw';
 import tokensDefinition from '@utrecht/components/surface/tokens.json';
 import tokens from '@utrecht/design-tokens/dist/index.json';
 import { utrechtSurfaceBackgroundColor } from '@utrecht/design-tokens/dist/index.mjs';
 import React from 'react';
-import { designTokenStory } from '../components/util';
-
-const Surface = ({ children }) => <utrecht-surface>{children}</utrecht-surface>;
+import { designTokenStory } from './util';
 
 const MetaThemeColor = ({ color }) => <meta name="theme-color" content={color} />;
 
 const meta = {
-  title: 'Web Component/Surface',
-  id: 'web-component-surface',
+  title: 'CSS Component/Surface',
+  id: 'css-surface',
   component: Surface,
   argTypes: {
     children: {
       description: 'Content of the surface',
+      control: 'text',
     },
   },
   args: {
-    children: [],
+    children: '',
   },
-  tags: ['autodocs'],
   parameters: {
     layout: 'fullscreen',
+    tokensPrefix: 'utrecht-surface',
     status: {
       type: 'WORK IN PROGRESS',
     },
-    tokensPrefix: 'utrecht-surface',
     tokens,
     tokensDefinition,
     docs: {
@@ -48,12 +47,23 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    children: 'The Quick Brown Fox Jumps Over The Lazy Dog',
+    children: [
+      <Heading2>Lorem ipsum</Heading2>,
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </Paragraph>,
+    ],
   },
 };
 
 export const ThemeColor: Story = {
-  args: {},
+  args: {
+    children: '',
+  },
   name: 'Theme color',
   render: () => MetaThemeColor({ color: utrechtSurfaceBackgroundColor }),
   parameters: {
