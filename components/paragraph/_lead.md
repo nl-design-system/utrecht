41922f9986c2d98373b983010fa648200dcb840c
Robbert Broersma <git@robbertbroersma.nl>
2023-08-27 18:32:33 +0200

    refactor: migrate paragraph component from mdx to tsx

--- components/paragraph/_lead.md
+++ components/paragraph/_lead.md
@@ -1,36 +1,9 @@
-{/* @license CC0-1.0 */}
+<!-- @license CC0-1.0 -->
 
-import { Canvas, Meta, Story } from "@storybook/blocks";
-import { exampleArgs, Paragraph } from "../story-template";
-import "../index.scss";
-
-<Meta
-  id="css-paragraph--lead"
-  title="CSS Component/Paragraph/Lead"
-  component={Paragraph}
-  parameters={{
-    status: {
-      type: "ALPHA",
-    },
-  }}
-/>
-
-# Lead paragraph
+<!-- markdownlint-disable first-line-h1 -->
 
 Gebruik in CSS de `.utrecht-paragraph` en `.utrecht-paragraph--lead` class names.
 
 Gebruik het HTML element `<b>` om de tekst visueel herkenbaar te maken als _lead paragraph_. Gebruik niet `<strong>`, die is bedoeld om informatie extra belangrijk te maken. Je wilt bijvoorbeeld niet dat een _lead paragraph_ extra nadrukkelijk wordt uigesproken door een schermvoorlezer.
 
 Als de CSS niet geladen kan worden, dan blijft de tekst visueel herkenbaar als lead paragraph door het `<b>` element. De `utrecht-paragraph__b` class name zorgt er voor dat tekst alleen vetgedrukt wordt als daar een design token voor is.
-
-<Canvas>
-  <Story
-    name="Lead"
-    args={{
-      ...exampleArgs,
-      lead: true,
-    }}
-  >
-    {Paragraph.bind({})}
-  </Story>
-</Canvas>
---
+++ packages/storybook/stories/components/Paragraph.stories.tsx
@@ -0,0 +1,96 @@
+/* @license CC0-1.0 */
+
+import { Meta, StoryObj } from '@storybook/react';
+import { Paragraph } from '@utrecht/component-library-react/dist/css-module';
+import readme from '@utrecht/components/paragraph/README.md?raw';
+import leadParagraphDocs from '@utrecht/components/paragraph/_lead.md?raw';
+import tokensDefinition from '@utrecht/components/paragraph/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import { designTokenStory } from './util';
+
+const meta = {
+  title: 'CSS Component/Paragraph',
+  id: 'css-paragraph',
+  component: Paragraph,
+  argTypes: {
+    children: {
+      description: 'Content of the paragraph.',
+      control: 'text',
+    },
+    lead: {
+      description: 'Lead paragraph',
+      control: 'boolean',
+    },
+    small: {
+      description: 'Paragraph with small print',
+      control: 'boolean',
+    },
+  },
+  args: {
+    children: '',
+    lead: false,
+    small: false,
+  },
+  parameters: {
+    tokensPrefix: 'utrecht-paragraph',
+    status: {
+      type: 'ALPHA',
+    },
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof Paragraph>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Styling via `utrecht-paragraph` class name.',
+      },
+    },
+  },
+};
+
+export const Lead: Story = {
+  args: {
+    ...Default.args,
+    lead: true,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: leadParagraphDocs,
+      },
+    },
+  },
+};
+
+export const SmallPrint: Story = {
+  args: {
+    ...Default.args,
+    small: true,
+  },
+  name: 'Small print',
+  parameters: {
+    docs: {
+      description: {
+        story: 'Styling via the `.utrecht-paragraph` and `.utrecht-paragraph--small` modifier class name',
+      },
+    },
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
