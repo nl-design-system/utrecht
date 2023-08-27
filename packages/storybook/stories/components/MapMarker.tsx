2cfaabc7692e47ba3d22bfc6204c425b9b3afda0
Robbert Broersma <git@robbertbroersma.nl>
2023-08-27 22:31:40 +0200

    refactor: migrate map marker component from mdx to tsx

---
+++ packages/storybook/stories/components/MapMarker.tsx
@@ -0,0 +1,9 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2022 Gemeente Utrecht
+ * Copyright (c) 2020-2022 Frameless B.V.
+ */
+
+import React, { PropsWithChildren } from 'react';
+
+export const MapMarker = ({ children }: PropsWithChildren<{}>) => <div className="utrecht-map-marker">{children}</div>;
