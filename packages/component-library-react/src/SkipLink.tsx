3935c57e54061bf8bdd88d7e92fea057f47c2de1
Robbert Broersma <git@robbertbroersma.nl>
2023-08-27 17:40:41 +0200

    fix: name of `SkipLinkProps` interface

--- packages/component-library-react/src/SkipLink.tsx
+++ packages/component-library-react/src/SkipLink.tsx
@@ -1,10 +1,10 @@
 import clsx from 'clsx';
 import { AnchorHTMLAttributes, ForwardedRef, forwardRef, PropsWithChildren } from 'react';
 
-export type LinkProps = AnchorHTMLAttributes<HTMLAnchorElement>;
+export type SkipLinkProps = AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export const SkipLink = forwardRef(
-  ({ children, className, ...restProps }: PropsWithChildren<LinkProps>, ref: ForwardedRef<HTMLAnchorElement>) => (
+  ({ children, className, ...restProps }: PropsWithChildren<SkipLinkProps>, ref: ForwardedRef<HTMLAnchorElement>) => (
     <p>
       <a
         ref={ref}
