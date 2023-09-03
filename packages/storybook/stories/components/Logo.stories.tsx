878d3aa32a80cb2891e98db93ad9a1dc41992d27
Robbert Broersma <git@robbertbroersma.nl>
2023-09-03 09:59:38 +0200

    refactor: migrate logo story from mdx to tsx

---
+++ packages/storybook/stories/components/Logo.stories.tsx
@@ -0,0 +1,46 @@
+/* @license CC0-1.0 */
+
+import { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/logo/README.md?raw';
+import tokensDefinition from '@utrecht/components/logo/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import { Logo } from './Logo';
+import { designTokenStory } from './util';
+
+const meta = {
+  title: 'CSS Component/Logo',
+  id: 'css-logo',
+  component: Logo,
+  argTypes: {},
+  args: {},
+  parameters: {
+    tokensPrefix: 'utrecht-logo',
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof Logo>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story: 'Styling via `utrecht-logo` class name.',
+      },
+    },
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
