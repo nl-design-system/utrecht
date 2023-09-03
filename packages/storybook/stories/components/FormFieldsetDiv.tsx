6ef22f3dc65154053f109d7f04d622275d6caec0
Robbert Broersma <git@robbertbroersma.nl>
2023-09-03 12:49:31 +0200

    refactor: migrate form fieldset stories from mdx to tsx

---
+++ packages/storybook/stories/components/FormFieldsetDiv.tsx
@@ -0,0 +1,65 @@
+/*
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2022 Gemeente Utrecht
+ * Copyright (c) 2020-2022 Frameless B.V.
+ */
+
+import clsx from 'clsx';
+import React, { HTMLAttributes, PropsWithChildren, ReactNode } from 'react';
+
+export interface FormFieldsetLegendDivProps extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
+  disabled?: boolean;
+}
+
+export const FormFieldsetLegendDiv = ({ className, disabled, ...restProps }: FormFieldsetLegendDivProps) => (
+  <div
+    className={clsx('utrecht-form-fieldset__legend', {
+      'utrecht-form-fieldset__legend--disabled': disabled,
+      className,
+    })}
+    {...restProps}
+  />
+);
+
+export interface FormFieldsetDivProps extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
+  disabled?: boolean;
+  invalid?: boolean;
+  legend?: ReactNode;
+  section?: boolean;
+}
+
+export const FormFieldsetDiv = ({
+  className,
+  children,
+  disabled = false,
+  id,
+  invalid,
+  legend,
+  role,
+  section,
+  ...restProps
+}: FormFieldsetDivProps) => {
+  const labelId = id && `${id}-legend`;
+  const content = (
+    <>
+      {legend && <FormFieldsetLegendDiv id={labelId}>{legend}</FormFieldsetLegendDiv>}
+      {children}
+    </>
+  );
+  return (
+    <div
+      className={clsx('utrecht-form-fieldset', {
+        'utrecht-form-fieldset--invalid': invalid,
+        'utrecht-form-fieldset--section': section,
+        className,
+      })}
+      aria-disabled={disabled ? 'true' : undefined}
+      aria-invalid={invalid ? 'true' : undefined}
+      role={role || 'group'}
+      aria-labelledby={labelId}
+      {...restProps}
+    >
+      {content}
+    </div>
+  );
+};
