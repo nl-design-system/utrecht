d2728a9fb83813cbc7748efa1579882682829120
Robbert Broersma <git@robbertbroersma.nl>
2023-09-03 11:34:31 +0200

    refactor: migrate logo button stories from mdx to tsx

---
+++ packages/storybook/stories/components/LogoButton.tsx
@@ -0,0 +1,23 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2022 Gemeente Utrecht
+ * Copyright (c) 2020-2022 Frameless B.V.
+ */
+
+import { Button } from '@utrecht/component-library-react/dist/css-module';
+import React, { HTMLAttributes, PropsWithChildren, ReactNode } from 'react';
+
+export interface LogoButtonProps extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
+  logo: ReactNode;
+}
+
+export const LogoButton = ({ children, logo, ...restProps }: LogoButtonProps) => (
+  <div className="utrecht-logo-button" {...restProps}>
+    {logo}
+    <Button appearance="primary-action-button" className="utrecht-logo-button__button">
+      {children}
+    </Button>
+  </div>
+);
+
+export default LogoButton;
