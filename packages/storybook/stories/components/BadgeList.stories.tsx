e548a62dd4937f632fc4c478fbb8ac257c4a4204
Robbert Broersma <git@robbertbroersma.nl>
2023-09-03 08:59:50 +0200

    refactor: migrate badge list stories from mdx to tsx

---
+++ packages/storybook/stories/components/BadgeList.stories.tsx
@@ -0,0 +1,69 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { BadgeList, DataBadge } from '@utrecht/component-library-react/dist/css-module';
+import readme from '@utrecht/components/badge-list/README.md?raw';
+import tokensDefinition from '@utrecht/components/badge-list/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import React from 'react';
+import { designTokenStory } from './util';
+
+interface BadgeListStoryProps {
+  items: string | string[];
+}
+
+const BadgeListStory = ({ items }: BadgeListStoryProps) => (
+  <BadgeList>
+    {(typeof items === 'string' ? items.split(/[\s.,()&;]+/g).filter(Boolean) : Array.isArray(items) ? items : []).map(
+      (item, index) => (
+        <DataBadge key={index}>{item}</DataBadge>
+      ),
+    )}
+  </BadgeList>
+);
+
+const meta = {
+  title: 'CSS Component/Badge list',
+  id: 'css-badge-list',
+  component: BadgeListStory,
+  args: {
+    items: '',
+  },
+  argTypes: {
+    items: {
+      description: 'Items',
+      control: 'text',
+    },
+  },
+  parameters: {
+    tokensPrefix: 'utrecht-badge-list',
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof BadgeListStory>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    items:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Styling via the `.utrecht-badge-list` class name.',
+      },
+    },
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
