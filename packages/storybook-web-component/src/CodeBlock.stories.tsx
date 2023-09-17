150690ff3a906f9ab024301af8cde8b07ca63c22
Robbert Broersma <git@robbertbroersma.nl>
2023-09-17 14:22:59 +0200

    docs: add code block web component story

---
+++ packages/storybook-web-component/src/CodeBlock.stories.tsx
@@ -0,0 +1,56 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/code-block/README.md?raw';
+import tokensDefinition from '@utrecht/components/code-block/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import { UtrechtCodeBlock } from '@utrecht/web-component-library-react';
+import { designTokenStory } from './design-token-story';
+
+const meta = {
+  title: 'Web Component/Code block',
+  id: 'web-component-code-block',
+  component: UtrechtCodeBlock,
+  argTypes: {
+    children: {
+      description: 'Content of the code block',
+    },
+  },
+  args: {},
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-code-block',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof UtrechtCodeBlock>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: `<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+  <head>
+    <title>NL Design System</title>
+    <meta charset="utf-8"/>
+  </head>
+  <body>
+    <h1>NL Design System</h1>
+  </body>
+</html>
+`,
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
