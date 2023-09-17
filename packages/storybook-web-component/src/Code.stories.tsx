cb352b630a7e8e4b562a2a04dca05cb2d8f9372e
Robbert Broersma <git@robbertbroersma.nl>
2023-09-17 14:19:46 +0200

    docs: add code web component story

---
+++ packages/storybook-web-component/src/Code.stories.tsx
@@ -0,0 +1,48 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/code/README.md?raw';
+import tokensDefinition from '@utrecht/components/code/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import { UtrechtCode, UtrechtParagraph } from '@utrecht/web-component-library-react';
+import React from 'react';
+import { designTokenStory } from './design-token-story';
+
+const meta = {
+  title: 'Web Component/Code',
+  id: 'web-component-code',
+  component: UtrechtCode,
+  argTypes: {
+    children: {
+      description: 'Content of the code',
+    },
+  },
+  args: {},
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-code',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+  decorators: [(Story) => <UtrechtParagraph>{Story()}</UtrechtParagraph>],
+} satisfies Meta<typeof UtrechtCode>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: '<input type="url" value="https://example.fi/">',
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
