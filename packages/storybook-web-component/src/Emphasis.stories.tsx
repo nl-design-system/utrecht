641d2e2a1136d155b72e6eb321bcb13960b8bcc6
Robbert Broersma <git@robbertbroersma.nl>
2023-09-17 14:29:27 +0200

    docs: add emphasis web component story

---
+++ packages/storybook-web-component/src/Emphasis.stories.tsx
@@ -0,0 +1,48 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/emphasis/README.md?raw';
+import tokensDefinition from '@utrecht/components/emphasis/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import { UtrechtEmphasis, UtrechtParagraph } from '@utrecht/web-component-library-react';
+import React from 'react';
+import { designTokenStory } from './design-token-story';
+
+const meta = {
+  title: 'Web Component/Emphasis',
+  id: 'web-component-emphasis',
+  component: UtrechtEmphasis,
+  argTypes: {
+    children: {
+      description: 'Emphasised text',
+    },
+  },
+  args: {},
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-emphasis',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+  decorators: [(Story) => <UtrechtParagraph>{Story()}</UtrechtParagraph>],
+} satisfies Meta<typeof UtrechtEmphasis>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'The Quick Brown Fox Jumps Over The Lazy Dog',
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
