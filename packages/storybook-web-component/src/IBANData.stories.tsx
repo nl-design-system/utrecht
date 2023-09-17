01e31d361204dd0c93f18d45f3d5db3bb535a9d7
Robbert Broersma <git@robbertbroersma.nl>
2023-09-17 14:28:18 +0200

    docs: add IBAN data web component story

---
+++ packages/storybook-web-component/src/IBANData.stories.tsx
@@ -0,0 +1,48 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/iban-data/README.md?raw';
+import tokensDefinition from '@utrecht/components/iban-data/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import { UtrechtIbanData, UtrechtParagraph } from '@utrecht/web-component-library-react';
+import React from 'react';
+import { designTokenStory } from './design-token-story';
+
+const meta = {
+  title: 'Web Component/IBAN data',
+  id: 'web-component-iban-data',
+  component: UtrechtIbanData,
+  argTypes: {
+    children: {
+      description: 'Content of the code block',
+    },
+  },
+  args: {},
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-iban-data',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+  decorators: [(Story) => <UtrechtParagraph>{Story()}</UtrechtParagraph>],
+} satisfies Meta<typeof UtrechtIbanData>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    value: 'GO00BACK123467899012',
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
