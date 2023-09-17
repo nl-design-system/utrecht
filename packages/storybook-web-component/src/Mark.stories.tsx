565f3ede704b5f79e8d57bcb6617f452d41dcb17
Robbert Broersma <git@robbertbroersma.nl>
2023-09-17 14:40:01 +0200

    docs: add mark web component story

---
+++ packages/storybook-web-component/src/Mark.stories.tsx
@@ -0,0 +1,49 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/mark/README.md?raw';
+import tokensDefinition from '@utrecht/components/mark/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import { UtrechtMark, UtrechtParagraph } from '@utrecht/web-component-library-react';
+import React from 'react';
+import { designTokenStory } from './design-token-story';
+
+const meta = {
+  title: 'Web Component/Mark',
+  id: 'web-component-mark',
+  component: UtrechtMark,
+  argTypes: {
+    children: {
+      description: 'Set the text content',
+      control: 'text',
+    },
+  },
+  args: {},
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-mark',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+  decorators: [(Story) => <UtrechtParagraph>{Story()}</UtrechtParagraph>],
+} satisfies Meta<typeof UtrechtMark>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'The Quick Brown Fox Jumps Over The Lazy Dog',
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
