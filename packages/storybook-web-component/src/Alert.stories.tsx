fa7f5815ae3fc2cda65bebb0b0222d384b13896e
Robbert Broersma <git@robbertbroersma.nl>
2023-09-17 14:17:15 +0200

    docs: add alert web component story

---
+++ packages/storybook-web-component/src/Alert.stories.tsx
@@ -0,0 +1,87 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/alert/README.md?raw';
+import tokensDefinition from '@utrecht/components/alert/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import { UtrechtAlert, UtrechtHeading1, UtrechtParagraph } from '@utrecht/web-component-library-react';
+import React from 'react';
+import { designTokenStory } from './design-token-story';
+
+const meta = {
+  title: 'Web Component/Alert',
+  id: 'web-component-alert',
+  component: UtrechtAlert,
+  argTypes: {
+    children: {
+      description: 'HTML content of the alert',
+    },
+    type: {
+      description: 'Type',
+      control: { type: 'select' },
+      options: ['', 'error', 'info', 'ok', 'warning'],
+    },
+  },
+  args: {
+    children: [
+      <UtrechtHeading1>Lorem ipsum</UtrechtHeading1>,
+      <UtrechtParagraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </UtrechtParagraph>,
+    ],
+  },
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-alert',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof UtrechtAlert>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Info: Story = {
+  args: {
+    ...Default.args,
+    type: 'info',
+  },
+};
+
+export const OK: Story = {
+  args: {
+    ...Default.args,
+    type: 'ok',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    ...Default.args,
+    type: 'warning',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    ...Default.args,
+    type: 'error',
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
