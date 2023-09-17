e9f3146123db8618ca3f54f6bff42c32d84e7618
Robbert Broersma <git@robbertbroersma.nl>
2023-09-17 15:18:20 +0200

    docs: add spotlight section web component story

---
+++ packages/storybook-web-component/src/SpotlightSection.stories.tsx
@@ -0,0 +1,73 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import readme from '@utrecht/components/spotlight-section/README.md?raw';
+import tokensDefinition from '@utrecht/components/spotlight-section/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import { UtrechtHeading1, UtrechtParagraph, UtrechtSpotlightSection } from '@utrecht/web-component-library-react';
+import React from 'react';
+import { designTokenStory } from './design-token-story';
+
+const meta = {
+  title: 'Web Component/Spotlight section',
+  id: 'web-component-spotlight-section',
+  component: UtrechtSpotlightSection,
+  argTypes: {
+    children: {
+      description: 'HTML content of the spotlight-section',
+    },
+    appearance: {
+      description: 'Appearance',
+      control: { type: 'select' },
+      options: ['', 'error', 'info', 'ok', 'warning'],
+    },
+  },
+  args: {
+    children: [
+      <UtrechtHeading1>Lorem ipsum</UtrechtHeading1>,
+      <UtrechtParagraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </UtrechtParagraph>,
+    ],
+  },
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-spotlight-section',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof UtrechtSpotlightSection>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Info: Story = {
+  args: {
+    ...Default.args,
+    appearance: 'info',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    ...Default.args,
+    appearance: 'warning',
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
