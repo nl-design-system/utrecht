289710488a0d5ef28a7fcc03a4d90efa0617d1c6
Robbert Broersma <git@robbertbroersma.nl>
2023-10-02 16:45:44 +0200

    feat: preserve data component

---
+++ packages/storybook-react/src/stories/PreserveData.stories.tsx
@@ -0,0 +1,80 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Paragraph, PreserveData } from '@utrecht/component-library-react/src/css-module/index';
+import readme from '@utrecht/components/preserve-data/README.md?raw';
+import tokensDefinition from '@utrecht/components/preserve-data/tokens.json';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import React from 'react';
+import { designTokenStory } from './util';
+
+const meta = {
+  title: 'React Component/Preserve data',
+  id: 'react-preserve-data',
+  component: PreserveData,
+  args: {
+    children: '',
+  },
+  argTypes: {
+    dateTime: {
+      name: 'dateTime',
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: '' } },
+    },
+    value: {
+      name: 'value',
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: '' } },
+    },
+  },
+  parameters: {
+    tokensPrefix: 'utrecht-preserve-data',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+  decorators: [(Story) => <Paragraph>{Story()}</Paragraph>],
+} satisfies Meta<typeof PreserveData>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: ' (06) 249 0420 120',
+  },
+};
+
+export const RightToLeft: Story = {
+  args: {
+    children: 'علي,',
+  },
+  name: 'Right-to-left',
+};
+
+export const NoTranslate: Story = {
+  args: {
+    children: 'Mees de Vos',
+    lang: 'nl',
+  },
+  name: 'Prevent translation',
+};
+
+export const Value: Story = {
+  args: {
+    children: '50,000',
+    value: '50000',
+  },
+};
+
+export const Time: Story = {
+  args: {
+    children: '19 januari 2038 om 03:14:08',
+    dateTime: '2038-01-19T03:14:08Z',
+  },
+};
+
+export const DesignTokens = designTokenStory(meta);
