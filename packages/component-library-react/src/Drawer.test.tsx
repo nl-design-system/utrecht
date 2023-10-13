c83c8ba0402a73275aca9711d9ce63971467fa56
Robbert Broersma <git@robbertbroersma.nl>
2023-10-13 07:10:26 +0200

    test: add tests for open drawer

--- packages/component-library-react/src/Drawer.test.tsx
+++ packages/component-library-react/src/Drawer.test.tsx
@@ -1,40 +1,40 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { createRef } from 'react';
 import { Drawer } from './Drawer';
 import '@testing-library/jest-dom';
 
 describe('Drawer', () => {
-  it('renders an HTML dialog element', () => {
-    const { container } = render(<Drawer />);
+  it('renders no dialog role element by default', () => {
+    render(<Drawer />);
 
-    const div = container.querySelector('dialog:only-child');
-
-    expect(div).toBeInTheDocument();
+    expect(() => {
+      screen.getByRole('dialog');
+    }).toThrow();
   });
 
   it('renders a design system BEM class name', () => {
     const { container } = render(<Drawer />);
 
-    const doc = container.querySelector(':only-child');
+    const drawer = container.querySelector(':only-child');
 
-    expect(doc).toHaveClass('utrecht-drawer');
+    expect(drawer).toHaveClass('utrecht-drawer');
   });
+
+  it('is not visible by default', () => {
+    const { container } = render(<Drawer />);
+
+    const drawer = container.querySelector(':only-child');
+
+    expect(drawer).not.toBeVisible();
+  });
+
   it('can have a additional class name', () => {
     const { container } = render(<Drawer className="large" />);
 
-    const document = container.querySelector(':only-child');
+    const drawer = container.querySelector(':only-child');
 
-    expect(document).toHaveClass('large');
-
-    expect(document).toHaveClass('utrecht-drawer');
-  });
-  it('displays as none', () => {
-    const { container } = render(<Drawer />);
-
-    const doc = container.querySelector(':only-child');
-
-    expect(doc).toBeEmptyDOMElement();
-    expect(doc).toHaveStyle({ display: 'none' });
+    expect(drawer).toHaveClass('large');
+    expect(drawer).toHaveClass('utrecht-drawer');
   });
 
   it('renders rich text content', () => {
@@ -44,9 +44,9 @@ describe('Drawer', () => {
       </Drawer>,
     );
 
-    const doc = container.querySelector(':only-child');
+    const drawer = container.querySelector(':only-child');
 
-    const richText = doc?.querySelector('h1');
+    const richText = drawer?.querySelector('h1');
 
     expect(richText).toBeInTheDocument();
   });
@@ -54,17 +54,17 @@ describe('Drawer', () => {
   it('can be hidden', () => {
     const { container } = render(<Drawer hidden />);
 
-    const doc = container.querySelector(':only-child');
+    const drawer = container.querySelector(':only-child');
 
-    expect(doc).not.toBeVisible();
+    expect(drawer).not.toBeVisible();
   });
 
   it('can have a custom class name', () => {
     const { container } = render(<Drawer className="large" />);
 
-    const doc = container.querySelector(':only-child');
+    const drawer = container.querySelector(':only-child');
 
-    expect(doc).toHaveClass('large');
+    expect(drawer).toHaveClass('large');
   });
 
   it('supports ForwardRef in React', () => {
@@ -72,15 +72,37 @@ describe('Drawer', () => {
 
     const { container } = render(<Drawer ref={ref} />);
 
-    const doc = container.querySelector(':only-child');
+    const drawer = container.querySelector(':only-child');
 
-    expect(ref.current).toBe(doc);
+    expect(ref.current).toBe(drawer);
   });
-  it('can be opened', () => {
-    const { container } = render(<Drawer open />);
 
-    const doc = container.querySelector(':only-child');
+  describe('open dialog', () => {
+    it('renders an HTML dialog element', () => {
+      const { container } = render(<Drawer open />);
 
-    expect(doc).toBeVisible();
+      const drawer = container.querySelector('dialog:only-child');
+
+      expect(drawer).toBeInTheDocument();
+    });
+
+    it('can be opened with `open` attribute', () => {
+      const { container } = render(<Drawer open />);
+      const drawer = container.querySelector(':only-child');
+
+      expect(drawer).toBeVisible();
+    });
+
+    it.skip('can be opened with the `showModal` method', () => {
+      /* this only applies to the web component */
+    });
+
+    it('can be hidden', () => {
+      const { container } = render(<Drawer open hidden />);
+
+      const drawer = container.querySelector(':only-child');
+
+      expect(drawer).not.toBeVisible();
+    });
   });
 });
