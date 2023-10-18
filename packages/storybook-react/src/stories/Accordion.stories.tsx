6804dfbdcf077e7dc12f2d57afac5252bd69544c
Ali Amori Kadhim <ali@frameless.io>
2023-10-18 14:11:15 +0200

    docs(react-storybook): add multiple accordion story

--- packages/storybook-react/src/stories/Accordion.stories.tsx
+++ packages/storybook-react/src/stories/Accordion.stories.tsx
@@ -80,6 +80,40 @@ export const Default: Story = {
   },
 };
 
+const accordionData = [
+  {
+    label: 'Lorem ipsum 1',
+    body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum.`,
+    expandedAccordion: true,
+  },
+  {
+    label: 'Lorem ipsum 2',
+    body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum.`,
+    expandedAccordion: false,
+  },
+  {
+    label: 'Lorem ipsum 3',
+    body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum.`,
+    expandedAccordion: false,
+  },
+];
+
+export const Multiple: Story = {
+  render: () => <AccordionProvider sections={accordionData} />,
+};
+
 export const RTL: Story = {
   args: {
     label: 'ما هو "لوريم إيبسوم" ؟',
