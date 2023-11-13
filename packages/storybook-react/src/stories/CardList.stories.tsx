d1dcbdc71f2f6490568b856459296d4546651be3
scar055 <rowanvanlaar@gmail.com>
2023-11-13 17:16:42 +0100

    chore: changed examples on how to use card list item

--- packages/storybook-react/src/stories/CardList.stories.tsx
+++ packages/storybook-react/src/stories/CardList.stories.tsx
@@ -1,7 +1,7 @@
 // performance optimizations are not relevant for story rendering, ignore ESLint
 
 import { Meta, StoryObj } from '@storybook/react';
-import { CardList, CardListItem } from '@utrecht/component-library-react/src/css-module/index';
+import { CardList, CardListItem, Paragraph } from '@utrecht/component-library-react/src/css-module/index';
 import readme from '@utrecht/components/unordered-list/README.md?raw';
 import tokensDefinition from '@utrecht/components/unordered-list/tokens.json';
 import tokens from '@utrecht/design-tokens/dist/index.json';
@@ -15,9 +15,31 @@ const meta = {
   argTypes: {},
   args: {
     children: [
-      <CardListItem headingLevel={2}>List item 1</CardListItem>,
-      <CardListItem headingLevel={2}>List item 2</CardListItem>,
-      <CardListItem headingLevel={2}>List item 3</CardListItem>,
+      <CardListItem headingLevel={2} title="item 1" preHeading="list item 1">
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum
+        </Paragraph>
+      </CardListItem>,
+      <CardListItem headingLevel={3} title="item 2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum
+      </CardListItem>,
+      <CardListItem headingLevel={1}>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum
+        </Paragraph>
+      </CardListItem>,
     ],
   },
   parameters: {
