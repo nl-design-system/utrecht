ff370b8371074014623fe29704373ba51b7553e7
scar055 <rowanvanlaar@gmail.com>
2023-11-15 16:49:46 +0100

    refactor: content paragraph made smaller
    Co-authored-by: savitris <savitris@users.noreply.github.com>

--- packages/storybook-react/src/stories/CardList.stories.tsx
+++ packages/storybook-react/src/stories/CardList.stories.tsx
@@ -13,29 +13,13 @@ const meta = {
   args: {
     children: [
       <CardListItem headingLevel={2} title="item 1" preHeading="list item 1">
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum
-        </Paragraph>
+        <Paragraph>item 1</Paragraph>
       </CardListItem>,
       <CardListItem headingLevel={3} title="item 2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum
+        item 2
       </CardListItem>,
       <CardListItem headingLevel={1}>
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum
-        </Paragraph>
+        <Paragraph>item 3</Paragraph>
       </CardListItem>,
     ],
   },
