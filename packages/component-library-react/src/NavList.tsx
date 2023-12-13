1396669f90e8f41f4d761a95d3bbcdc27edff6d0
Robbert Broersma <git@robbertbroersma.nl>
2023-12-13 16:33:20 +0100

    feat: nav bar with icons

--- packages/component-library-react/src/NavList.tsx
+++ packages/component-library-react/src/NavList.tsx
@@ -1,20 +1,25 @@
 import clsx from 'clsx';
-import { PropsWithChildren } from 'react';
+import { HTMLAttributes, PropsWithChildren } from 'react';
 import type { LinkProps } from './Link';
 import { Link } from './Link';
 import type { LinkButtonProps } from './LinkButton';
 import { LinkButton } from './LinkButton';
 
-export interface NavListProps {
+export interface NavListProps extends HTMLAttributes<HTMLUListElement> {
   appearance?: undefined | string | 'center';
 }
 
-export const NavList = ({ appearance, children }: PropsWithChildren<NavListProps>) => (
+export const NavList = ({ appearance, children, className, ...restProps }: PropsWithChildren<NavListProps>) => (
   <ul
     role="list"
-    className={clsx('utrecht-nav-list', {
-      'utrecht-nav-list--center': appearance === 'center',
-    })}
+    className={clsx(
+      'utrecht-nav-list',
+      {
+        'utrecht-nav-list--center': appearance === 'center',
+      },
+      className,
+    )}
+    {...restProps}
   >
     {children}
   </ul>
@@ -26,11 +31,15 @@ export interface NavListLinkProps extends LinkProps {
   center?: boolean;
 }
 
-export const NavListLink = ({ center, children, ...restProps }: PropsWithChildren<NavListLinkProps>) => (
+export const NavListLink = ({ center, children, className, ...restProps }: PropsWithChildren<NavListLinkProps>) => (
   <li
-    className={clsx('utrecht-nav-list__item', {
-      'utrecht-nav-list__item--center': center,
-    })}
+    className={clsx(
+      'utrecht-nav-list__item',
+      {
+        'utrecht-nav-list__item--center': center,
+      },
+      className,
+    )}
   >
     <Link className="utrecht-nav-list__link" {...restProps}>
       {children}
@@ -44,11 +53,20 @@ export interface NavListLinkButtonProps extends LinkButtonProps {
   center?: boolean;
 }
 
-export const NavListLinkButton = ({ center, children, ...restProps }: PropsWithChildren<NavListLinkButtonProps>) => (
+export const NavListLinkButton = ({
+  center,
+  children,
+  className,
+  ...restProps
+}: PropsWithChildren<NavListLinkButtonProps>) => (
   <li
-    className={clsx('utrecht-nav-list__item', {
-      'utrecht-nav-list__item--center': center,
-    })}
+    className={clsx(
+      'utrecht-nav-list__item',
+      {
+        'utrecht-nav-list__item--center': center,
+      },
+      className,
+    )}
   >
     <LinkButton className="utrecht-nav-list__link" {...restProps}>
       {children}
