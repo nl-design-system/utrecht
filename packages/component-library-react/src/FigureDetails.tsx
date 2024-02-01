6661235c29ca36a76847c5be7185aaa2d7f2675d
scar055 <rowanvanlaar@gmail.com>
2024-02-01 11:55:40 +0100

    perf: changed component code to actual component code

--- packages/component-library-react/src/FigureDetails.tsx
+++ packages/component-library-react/src/FigureDetails.tsx
@@ -1,29 +1,35 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2024 Gemeente Utrecht
+ * Copyright (c) 2024 Frameless B.V.
+ */
+
 import { HTMLAttributes, PropsWithChildren, useEffect, useRef, useState } from 'react';
 import { Button } from './Button';
 import { Drawer } from './Drawer';
 import { Figure } from './Figure';
-import { Table } from './Table';
-import { TableBody } from './TableBody';
-import { TableCell } from './TableCell';
-import { TableFooter } from './TableFooter';
-import { TableHeader } from './TableHeader';
-import { TableHeaderCell } from './TableHeaderCell';
-import { TableRow } from './TableRow';
-import { VegaVisualization } from './VegaVisualization';
-import './styles/FigureDetails.css';
 
 interface FigureDetailsProps extends Omit<HTMLAttributes<HTMLLIElement>, 'children'> {
   openButtonText: string;
+  openButtonAppearance?: string;
+  openButtonClassName?: string;
   closeButtonText: string;
+  closeButtonAppearance?: string;
+  closeButtonClassName?: string;
   drawerClassName?: string;
   children?: any;
 }
 
 export const FigureDetails = ({
   openButtonText,
+  openButtonAppearance,
+  openButtonClassName,
   closeButtonText,
+  closeButtonAppearance,
+  closeButtonClassName,
   drawerClassName,
   children,
+  ...restProps
 }: PropsWithChildren<FigureDetailsProps>) => {
   const [open, setOpen] = useState(false);
   const drawer = useRef<HTMLDialogElement>(null);
@@ -51,115 +57,19 @@ export const FigureDetails = ({
     return () => {
       document.removeEventListener('click', handleBackdropClick);
     };
-  }, []);
+  });
 
-  let FigureDetails = (
-    <div>
-      <Button onClick={showModal}>{openButtonText}</Button>
+  return (
+    <Figure {...restProps}>
+      <Button className={openButtonClassName} appearance={openButtonAppearance} onClick={showModal}>
+        {openButtonText}
+      </Button>
       <Drawer className={drawerClassName} ref={drawer} open={open}>
-        <Button onClick={closeModal}>{closeButtonText}</Button>
+        <Button className={closeButtonClassName} appearance={closeButtonAppearance} onClick={closeModal}>
+          {closeButtonText}
+        </Button>
         {children}
       </Drawer>
-    </div>
-  );
-  return FigureDetails;
-};
-export default function Home() {
-  return (
-    <Figure>
-      <VegaVisualization
-        spec={{
-          $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-          data: {
-            values: [
-              {
-                a: 'A',
-                b: 28,
-              },
-              {
-                a: 'B',
-                b: 55,
-              },
-              {
-                a: 'C',
-                b: 43,
-              },
-              {
-                a: 'D',
-                b: 91,
-              },
-              {
-                a: 'E',
-                b: 81,
-              },
-              {
-                a: 'F',
-                b: 53,
-              },
-              {
-                a: 'G',
-                b: 19,
-              },
-              {
-                a: 'H',
-                b: 87,
-              },
-              {
-                a: 'I',
-                b: 52,
-              },
-            ],
-          },
-          description: 'A simple bar chart with embedded data.',
-          encoding: {
-            x: {
-              axis: {
-                labelAngle: 0,
-              },
-              field: 'a',
-              title: 'x-Axis',
-              type: 'nominal',
-            },
-            y: {
-              field: 'b',
-              title: 'y-Axis',
-              type: 'quantitative',
-            },
-          },
-          mark: 'bar',
-          title: 'Staafgrafiek',
-        }}
-        label="Bla bla heel toegankelijk"
-      />
-      <FigureDetails
-        drawerClassName="utrecht-figure-details"
-        openButtonText={'open tabel'}
-        closeButtonText={'close tabel'}
-      >
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHeaderCell scope="col">Column A</TableHeaderCell>
-              <TableHeaderCell scope="col">Column B</TableHeaderCell>
-              <TableHeaderCell scope="col">Column C</TableHeaderCell>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableHeaderCell scope="row">Item 1</TableHeaderCell>
-              <TableCell>Data</TableCell>
-              <TableCell>More data</TableCell>
-            </TableRow>
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell>More data</TableCell>
-              <TableCell>More data</TableCell>
-              <TableCell>More data</TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </FigureDetails>
     </Figure>
   );
-}
+};
