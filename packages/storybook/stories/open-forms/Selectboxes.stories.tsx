46d615efcb1c0b04ef7c7ac94e1b0bd874ff172c
scar055 <rowanvanlaar@gmail.com>
2024-02-21 15:13:25 +0100

    feat: add checked story for selectboxes

--- packages/storybook/stories/open-forms/Selectboxes.stories.tsx
+++ packages/storybook/stories/open-forms/Selectboxes.stories.tsx
@@ -34,3 +34,26 @@ export const Default: Story = {
     },
   },
 };
+
+export const Checked: Story = {
+  args: {
+    extraComponentProperties: {
+      values: [
+        {
+          label: 'Optie A',
+          value: 'selectA',
+        },
+        {
+          label: 'Optie B',
+          value: 'selectB',
+        },
+        {
+          label:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          value: 'selectC',
+        },
+      ],
+      defaultValue: 'selectA',
+    },
+  },
+};
