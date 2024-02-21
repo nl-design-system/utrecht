cb094b2c8454e1c3b92d5bf4851853f16dd54bc4
scar055 <rowanvanlaar@gmail.com>
2024-02-21 15:10:55 +0100

    fix: changed naming from the stories to the correct story

--- packages/storybook/stories/open-forms/Select.stories.tsx
+++ packages/storybook/stories/open-forms/Select.stories.tsx
@@ -2,11 +2,11 @@ import { Meta, StoryObj } from '@storybook/react';
 import { SingleFormioComponent } from './StoryUtil';
 
 const meta = {
-  id: 'open-forms-selectboxes',
-  title: 'Open Formulieren/Selectboxes',
+  id: 'open-forms-select',
+  title: 'Open Formulieren/Select',
   component: SingleFormioComponent,
   args: {
-    type: 'selectboxes',
+    type: 'select',
   },
 } satisfies Meta<typeof SingleFormioComponent>;
 
@@ -16,21 +16,22 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     extraComponentProperties: {
-      values: [
-        {
-          label: 'Optie A',
-          value: 'selectA',
-        },
-        {
-          label: 'Optie B',
-          value: 'selectB',
-        },
-        {
-          label:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          value: 'selectC',
-        },
-      ],
+      data: {
+        values: [
+          {
+            label: 'Optie A',
+            value: 'radioA',
+          },
+          {
+            label: 'Optie B',
+            value: 'radioB',
+          },
+          {
+            label: 'Optie C',
+            value: 'radioC',
+          },
+        ],
+      },
     },
   },
 };
