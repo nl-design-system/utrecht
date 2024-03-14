2cae212e3a7af4ae0d3689efdbe339ef54c05fce
scar055 <rowanvanlaar@gmail.com>
2024-03-14 17:26:15 +0100

    feat: work in progress

---
+++ components/templates/open-forms/story-template.jsx
@@ -0,0 +1,14 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2024 Gemeente Utrecht
+ * Copyright (c) 2024 Frameless B.V.
+ */
+import React from 'react';
+
+export const OpenFormStep1 = () => (
+  <div>
+    <h1>step 1</h1>
+  </div>
+);
+
+export default OpenFormStep1;
