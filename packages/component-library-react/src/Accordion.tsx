d9aab3bf73755fab90548872c9620889111a2446
Ali Amori Kadhim <ali@frameless.io>
2024-03-19 09:38:38 +0100

    fix(react-library): add the missing attribute to the accordion panel

--- packages/component-library-react/src/Accordion.tsx
+++ packages/component-library-react/src/Accordion.tsx
@@ -115,7 +115,9 @@ export const AccordionSection = forwardRef(
             {children}
           </section>
         ) : (
-          <div {...panelAttributes}>{children}</div>
+          <div id={panelId} {...panelAttributes}>
+            {children}
+          </div>
         )}
       </div>
     );
