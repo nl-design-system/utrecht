dd5acfe9d5142589fe224d944fe2f87e098a9bfc
Robbert Broersma <git@robbertbroersma.nl>
2024-05-11 19:49:29 +0200

    feat: add indeterminate property to React Checkbox

--- packages/component-library-react/src/Checkbox.tsx
+++ packages/component-library-react/src/Checkbox.tsx
@@ -3,12 +3,13 @@ import { ForwardedRef, forwardRef, InputHTMLAttributes } from 'react';
 
 export interface CheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type' | 'readOnly'> {
   appearance?: string;
+  indeterminate?: boolean;
   invalid?: boolean;
 }
 
 export const Checkbox = forwardRef(
   (
-    { appearance = 'custom', disabled, invalid, required, className, ...restProps }: CheckboxProps,
+    { appearance = 'custom', disabled, indeterminate, invalid, required, className, ...restProps }: CheckboxProps,
     ref: ForwardedRef<HTMLInputElement>,
   ) => (
     <input
@@ -21,11 +22,13 @@ export const Checkbox = forwardRef(
         {
           'utrecht-checkbox--disabled': disabled,
           'utrecht-checkbox--custom': appearance === 'custom',
+          'utrecht-checkbox--indeterminate': indeterminate,
           'utrecht-checkbox--invalid': invalid,
           'utrecht-checkbox--required': required,
         },
         className,
       )}
+      aria-checked={indeterminate ? 'mixed' : undefined}
       aria-invalid={invalid || undefined}
       disabled={disabled}
       required={required}
