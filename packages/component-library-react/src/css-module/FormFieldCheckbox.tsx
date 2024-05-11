b5546d700479149504a2faa44386ebc91c69f158
Robbert Broersma <git@robbertbroersma.nl>
2024-05-11 17:28:37 +0200

    feat: form field checkbox component for React

---
+++ packages/component-library-react/src/css-module/FormFieldCheckbox.tsx
@@ -0,0 +1,12 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+// Inject CSS from components used in `FormFieldCheckbox`
+import './Checkbox';
+import './FormField';
+import './FormFieldDescription';
+import './FormFieldErrorMessage';
+
+export * from '../FormFieldCheckbox';
