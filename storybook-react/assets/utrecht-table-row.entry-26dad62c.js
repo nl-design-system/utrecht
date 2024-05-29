13f06136b5786fd0cb62c1ba9ef99c124e26587d
nl-design-system-ci <79643794+nl-design-system-ci@users.noreply.github.com>
2024-05-29 15:18:21 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@b96babc0832ee1c4e5603f2de07b309552bccc15 🚀

---
+++ storybook-react/assets/utrecht-table-row.entry-26dad62c.js
@@ -0,0 +1,2 @@
+import{r as o,h as r,H as s}from"./index.esm-b7c0c3a5.js";import"./clsx.m-c1fc4751.js";import"./getPrototypeOf-624f05c9.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./iframe-fbba98a5.js";import"../sb-preview/runtime.js";const e=":host{break-inside:avoid;page-break-inside:avoid;display:table-row}",i=class{constructor(t){o(this,t)}render(){return r(s,{role:"row"},r("slot",null))}};i.style=e;export{i as utrecht_table_row};
+//# sourceMappingURL=utrecht-table-row.entry-26dad62c.js.map
---
+++ storybook-vue/assets/PageFooter.stories-6db8d1f4.js
@@ -0,0 +1,9 @@
+import{t,G as o}from"./component-library-vue-888268d8.js";import{c as a}from"./util-8ec3c4ee.js";import"./vue.esm-bundler-4be3a786.js";import"./chunk-HLWAVYOI-b365d405.js";import"./iframe-9590028e.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-11d98b33.js";import"./index-4fb43917.js";import"./index-356e4a49.js";const i=`<!-- @license CC0-1.0 -->
+
+# Page Footer
+`,p={"page-footer":{color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"background-image":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<url> | *",inherits:!0}}},"padding-inline-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-inline-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-block-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-block-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}}}},c={utrecht:p},g={id:"vue-page-footer",title:"Vue.js Component/Page footer",component:t,tags:["autodocs"],argTypes:{},args:{},render:()=>({components:{PageFooter:t,Paragraph:o},template:'<PageFooter v-bind="$props"><slot></slot></PageFooter>'}),parameters:{status:{type:"ALPHA"},tokens:c,tokenPrefix:"utrecht-page-footer",docs:{component:{description:i}}}},e=a(g,{name:"Page footer",args:{slot:"Contactgegevens kunnen hier"}});var n,s,r;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`createStory(meta, {
+  name: 'Page footer',
+  args: {
+    slot: \`Contactgegevens kunnen hier\`
+  }
+})`,...(r=(s=e.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const $=["Default"];export{e as Default,$ as __namedExportsOrder,g as default};
