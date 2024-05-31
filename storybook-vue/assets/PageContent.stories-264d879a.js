ea2b3f080ae785a90900b46fefdfabe680504b37
Robbert <mail@robbertbroersma.nl>
2024-05-31 05:48:52 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@4e3d58e73a1eae2e30961cdcd51ab34ba2a09332 🚀

---
+++ storybook-vue/assets/PageContent.stories-264d879a.js
@@ -0,0 +1,9 @@
+import{O as e,G as a}from"./component-library-vue-888268d8.js";import{c as s}from"./util-0c3f2906.js";import"./vue.esm-bundler-4be3a786.js";import"./chunk-HLWAVYOI-6f4f0a2c.js";import"./iframe-4191483d.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-11d98b33.js";import"./index-4fb43917.js";import"./index-356e4a49.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page content
+`,c={"page-content":{"padding-block-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-block-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}}}},i={utrecht:c},m={id:"vue-page-content",title:"Vue.js Component/Page content",component:e,tags:["autodocs"],argTypes:{},args:{},render:()=>({components:{PageContent:e,Paragraph:a},template:'<PageContent v-bind="$props"><slot></slot></PageContent>'}),parameters:{status:{type:"ALPHA"},tokens:i,tokenPrefix:"utrecht-page-content",docs:{component:{description:p}}}},t=s(m,{name:"Page content",args:{slot:"Inhoud van de pagina kan hier"}});var n,o,r;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`createStory(meta, {
+  name: 'Page content',
+  args: {
+    slot: \`Inhoud van de pagina kan hier\`
+  }
+})`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const C=["Default"];export{t as Default,C as __namedExportsOrder,m as default};
