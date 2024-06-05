4f359508d432cdcf54441629b640954331baad1b
Ali Amori Kadhim <ali@frameless.io>
2024-06-05 10:33:21 +0200

    build(storybook-vue): support SCSS absolute import

--- packages/storybook-vue/config/main.ts
+++ packages/storybook-vue/config/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
+import path from 'path';
 
 const config: StorybookConfig = {
   features: {
@@ -23,18 +24,22 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   async viteFinal(config) {
-    // Workaround for `@storybook/addon-jest` with Vite
-    // https://github.com/storybookjs/storybook/issues/14856#issuecomment-1262333250
-    if (config.resolve) {
-      config.resolve = {
-        ...config.resolve,
+    const { mergeConfig } = await import('vite');
+    return mergeConfig(config, {
+      resolve: {
         alias: {
-          ...config.resolve.alias,
+          '~@utrecht': path.resolve(__dirname, '../node_modules/@utrecht'),
           path: require.resolve('path-browserify'),
         },
-      };
-    }
-    return config;
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            includePaths: [path.resolve(__dirname, '../node_modules/@utrecht')],
+          },
+        },
+      },
+    });
   },
 };
 
