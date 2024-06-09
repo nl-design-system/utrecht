3bf038d81c25d6f90896b833b1cfab49edb77790
Robbert Broersma <git@robbertbroersma.nl>
2024-06-09 23:15:30 +0200

    fix: restore missing `dist/` directory to @utrecht/components

---
+++ .changeset/light-dodos-rest.md
@@ -0,0 +1,5 @@
+---
+"@utrecht/components": patch
+---
+
+Add missing `dist/` directory with the `.css` files.
--- components/rollup.config.mjs
+++ components/rollup.config.mjs
@@ -1,26 +1,39 @@
 import postcss from 'rollup-plugin-postcss';
 import discardDuplicates from 'postcss-discard-duplicates';
-import copy from 'rollup-plugin-copy';
 import { cwd } from 'node:process';
 import { basename } from 'node:path';
 
-export default {
-  input: 'src/index.scss',
-  output: {
-    dir: './dist',
-    sourcemap: false,
-    format: 'esm',
-    compact: true,
+export default [
+  {
+    input: 'src/index.scss',
+    output: {
+      dir: './dist',
+      sourcemap: false,
+      format: 'esm',
+      compact: true,
+    },
+    plugins: [
+      postcss({
+        extensions: ['.css', '.scss'],
+        plugins: [discardDuplicates()],
+        extract: true,
+      }),
+    ],
   },
-  plugins: [
-    postcss({
-      extensions: ['.css', '.scss'],
-      plugins: [discardDuplicates()],
-      extract: true,
-    }),
-    // Copy the `index.css` file for compatibility with imports from `@utrecht/components/dist/{component}/css/index.css`
-    copy({
-      targets: [{ src: 'dist/index.css', dest: `../dist/${basename(cwd())}/css/` }],
-    }),
-  ],
-};
+  {
+    input: 'src/index.scss',
+    output: {
+      dir: `../dist/${basename(cwd())}/css/`,
+      sourcemap: false,
+      format: 'esm',
+      compact: true,
+    },
+    plugins: [
+      postcss({
+        extensions: ['.css', '.scss'],
+        plugins: [discardDuplicates()],
+        extract: true,
+      }),
+    ],
+  },
+];
