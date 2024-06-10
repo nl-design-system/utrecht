6cbd8d42a054dcf64f2bc2894de97da3454c5a11
scar055 <rowanvanlaar@gmail.com>
2024-06-10 16:36:21 +0200

    refactor: changed css to src map

--- packages/component-library-react/src/css-module/CardList.tsx
+++ packages/component-library-react/src/css-module/CardList.tsx
@@ -3,6 +3,6 @@
  * Copyright (c) 2021 Robbert Broersma
  */
 
-import '../../../../components/card-list/css/index.scss';
+import '../../../../components/card-list/src/index.scss';
 
 export * from '../CardList';
