5584fb88e1ace9a595e581080207717d00bc13f8
scar055 <rowanvanlaar@gmail.com>
2024-06-11 13:01:40 +0200

    refactor: changed link to be the same as others

--- packages/component-library-react/src/css-module/CardListItem.tsx
+++ packages/component-library-react/src/css-module/CardListItem.tsx
@@ -3,6 +3,6 @@
  * Copyright (c) 2021 Robbert Broersma
  */
 
-import '../../../../components/card-list/src/index.scss';
+import '@utrecht/card-list/src/index.scss';
 
 export * from '../CardListItem';
