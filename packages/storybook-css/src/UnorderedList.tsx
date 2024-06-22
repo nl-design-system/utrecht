5414c5e64d736718c249b81da613636d0f4b9bb3
Robbert Broersma <git@robbertbroersma.nl>
2024-06-22 12:10:43 +0200

    refactor: move Unordered list stories to Storybook for CSS

---
+++ packages/storybook-css/src/UnorderedList.tsx
@@ -0,0 +1,55 @@
+import { UnorderedList, UnorderedListItem } from '@utrecht/component-library-react';
+import type { UnorderedListProps } from '@utrecht/component-library-react';
+import clsx from 'clsx';
+import React, { ReactNode } from 'react';
+
+interface UnorderedListItemData {
+  children?: ReactNode;
+  items?: UnorderedListItemData[];
+}
+
+interface UnorderedListStoryProps extends UnorderedListProps {
+  element?: string | 'div';
+  center?: boolean;
+  items?: UnorderedListItemData[];
+  htmlContent?: boolean;
+  nested?: boolean;
+}
+
+const HTMLList = ({ items }: { items: UnorderedListItemData[] }) => (
+  <ul>
+    {items?.map(({ children: subChildren, items: subItems }, index) => (
+      <li key={index}>
+        {subChildren}
+        {subItems && HTMLList({ items: subItems })}
+      </li>
+    ))}
+  </ul>
+);
+
+export const UnorderedListStory = ({ children, center, items, htmlContent, nested }: UnorderedListStoryProps) => {
+  return (
+    <UnorderedList
+      className={clsx({
+        'utrecht-unordered-list--center': center,
+        'utrecht-unordered-list--html-content': htmlContent,
+        'utrecht-unordered-list--nested': nested,
+      })}
+    >
+      {children}
+      {htmlContent
+        ? items?.map(({ children: subChildren, items: subItems }, index) => (
+            <li key={index}>
+              {subChildren}
+              {subItems && HTMLList({ items: subItems })}
+            </li>
+          ))
+        : items?.map(({ children: subChildren, items: subItems }, index) => (
+            <UnorderedListItem key={index}>
+              {subChildren}
+              {subItems && UnorderedListStory({ items: subItems, nested: true })}
+            </UnorderedListItem>
+          ))}
+    </UnorderedList>
+  );
+};
