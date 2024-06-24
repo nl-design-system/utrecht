5a2ed10d8234a7af83592b24c81c64bf50e6007f
Robbert Broersma <git@robbertbroersma.nl>
2024-06-25 00:26:18 +0200

    refactor: stop using `applyPolyfills`, removed in Stencil 4.18.1

--- packages/web-component-library-vue/src/plugin.ts
+++ packages/web-component-library-vue/src/plugin.ts
@@ -1,10 +1,8 @@
-import { applyPolyfills, defineCustomElements } from '@utrecht/web-component-library-stencil/loader';
+import { defineCustomElements } from '@utrecht/web-component-library-stencil/loader';
 import type { Plugin } from 'vue';
 
 export const UtrechtComponentLibrary: Plugin = {
   async install() {
-    applyPolyfills().then(() => {
-      defineCustomElements();
-    });
+    defineCustomElements();
   },
 };
