bd53c4e0bb7a8e560fa61839a0cd25aa3569944d
scar055 <rowanvanlaar@gmail.com>
2024-07-08 17:12:03 +0200

    feat: add stories to rich text component

--- packages/storybook-react/src/stories/RichText.stories.tsx
+++ packages/storybook-react/src/stories/RichText.stories.tsx
@@ -1,9 +1,16 @@
 // performance optimizations are not relevant for story rendering, ignore ESLint
 
 import { Meta, StoryObj } from '@storybook/react';
-import { Paragraph, RichText } from '@utrecht/component-library-react';
-import readme from '@utrecht/components/rich-text/README.md?raw';
+import {
+  Heading1,
+  Heading2,
+  Paragraph,
+  RichText,
+  UnorderedList,
+  UnorderedListItem,
+} from '@utrecht/component-library-react/src/css-module';
 // import tokensDefinition from '@utrecht/components/card-list/src/tokens.json';
+import readme from '@utrecht/components/rich-text/README.md?raw';
 import tokens from '@utrecht/design-tokens/dist/index.json';
 import React from 'react';
 import { designTokenStory } from './util';
@@ -14,7 +21,18 @@ const meta = {
   component: RichText,
   argTypes: {},
   args: {
-    children: [<Paragraph>test</Paragraph>],
+    children: [
+      <Heading1>Componenten collage NL Design System</Heading1>,
+      <Heading2>Componenten collage NL Design System</Heading2>,
+      <Heading1>Componenten collage NL Design System</Heading1>,
+      <Paragraph>Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *</Paragraph>,
+      <Heading1>Componenten collage NL Design System</Heading1>,
+      <UnorderedList>
+        <UnorderedListItem>label</UnorderedListItem>
+        <UnorderedListItem>label</UnorderedListItem>
+        <UnorderedListItem>label</UnorderedListItem>
+      </UnorderedList>,
+    ],
   },
   parameters: {
     tokensPrefix: 'utrecht-card-list',
@@ -33,5 +51,23 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const paragraph: Story = {
+  name: 'Paragraph',
+  args: {
+    children: [
+      <Paragraph>Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *</Paragraph>,
+      <Heading2>Componenten collage NL Design System</Heading2>,
+      <Paragraph>Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *</Paragraph>,
+      <Heading1>Componenten collage NL Design System</Heading1>,
+      <Paragraph>Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *</Paragraph>,
+      <UnorderedList>
+        <UnorderedListItem>label</UnorderedListItem>
+        <UnorderedListItem>label</UnorderedListItem>
+        <UnorderedListItem>label</UnorderedListItem>
+      </UnorderedList>,
+    ],
+  },
+};
 
 export const DesignTokens = designTokenStory(meta);
