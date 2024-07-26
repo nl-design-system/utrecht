8a9251ac5b81875a2e0991a67def0dc0d2ae944f
matijs <matijs@gmail.com>
2024-07-26 15:59:42 +0200

    chore: update npm-package-json-lint config
    - Use new config file name: npmpackagejsonlint.config.cjs
    - Only use the rules we explicitly want to enforce
    - Use overrides to get rid of config files in folders

---
+++ npmpackagejsonlint.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  rules: {
+    'no-caret-version-dependencies': 'error',
+    'no-caret-version-devDependencies': 'error',
+    'no-tilde-version-dependencies': 'error',
+    'require-repository-directory': 'error',
+    'valid-values-license': ['error', ['EUPL-1.2']],
+    'valid-values-name-scope': ['error', ['@utrecht']],
+  },
+  overrides: [
+    {
+      patterns: ['proprietary/**/package.json'],
+      rules: {
+        'valid-values-license': ['error', ['SEE LICENSE IN ../LICENSE.md']],
+      },
+    },
+  ],
+};
