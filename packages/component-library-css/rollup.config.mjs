4cd5faccccf9605d15f39a89fc95a7ec2a2327c6
Robbert Broersma <git@robbertbroersma.nl>
2024-07-27 16:30:48 +0200

    build: remove unnecessary comments from CSS component library build

--- packages/component-library-css/rollup.config.mjs
+++ packages/component-library-css/rollup.config.mjs
@@ -1,4 +1,5 @@
 import postcss from 'rollup-plugin-postcss';
+import { Comment } from 'postcss';
 import discardDuplicates from 'postcss-discard-duplicates';
 import postcssLogical from 'postcss-logical';
 
@@ -10,6 +11,49 @@ const removeCharset = () => {
   return (css) => {
     css.walkAtRules('charset', (atRule) => {
       atRule.remove();
+    });
+  };
+};
+
+const isLicenseComment = (str) => /@license/.test(str);
+
+const removeComments = ({ keepLicense = false }) => {
+  return (css) => {
+    css.walk((node) => {
+      if (node.type === 'comment') {
+        if (keepLicense && isLicenseComment(node.text)) {
+          return;
+        }
+        node.remove();
+      }
+    });
+  };
+};
+
+const mergeLicenseComments = () => {
+  return (css) => {
+    // Add all licenses to a `Map`, to deduplicate the license declarations.
+    const licenses = new Map();
+    css.walkComments((node) => {
+      if (isLicenseComment(node.text)) {
+        node.remove();
+
+        if (!licenses.has(node.text)) {
+          licenses.set(
+            node.text,
+            new Comment({
+              text: node.text,
+              raws: {
+                left: node.raws.left,
+                right: node.raws.right,
+              },
+            }),
+          );
+        }
+      }
+    });
+    licenses.forEach((license) => {
+      css.prepend(license);
     });
   };
 };
@@ -26,7 +70,13 @@ export default [
     plugins: [
       postcss({
         extensions: ['.css', '.scss'],
-        plugins: [discardDuplicates()],
+        plugins: [
+          discardDuplicates(),
+          mergeLicenseComments(),
+          removeComments({
+            keepLicense: true,
+          }),
+        ],
         extract: true,
       }),
     ],
@@ -42,7 +92,13 @@ export default [
     plugins: [
       postcss({
         extensions: ['.css', '.scss'],
-        plugins: [discardDuplicates()],
+        plugins: [
+          discardDuplicates(),
+          mergeLicenseComments(),
+          removeComments({
+            keepLicense: true,
+          }),
+        ],
         extract: true,
       }),
     ],
@@ -65,6 +121,10 @@ export default [
           }),
           discardDuplicates(),
           removeCharset(),
+          mergeLicenseComments(),
+          removeComments({
+            keepLicense: true,
+          }),
         ],
         extract: true,
       }),
