dca1b1ae315045381239ed6cbabcd452daf43879
Robbert <mail@robbertbroersma.nl>
2024-07-29 07:26:59 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@32fd1340d99e5e2ef259adcba55051d9c3ddc11d 🚀

---
+++ storybook-react/assets/PageHeader.stories-45db2467.js
@@ -0,0 +1,25 @@
+import{j as d}from"./jsx-runtime-c9381026.js";import{j as l,l as p}from"./index-92b23782.js";import{t as m,d as c}from"./util-7a7e14bd.js";import"./index-8b3efc3f.js";import"./_commonjsHelpers-de833af9.js";import"./slicedToArray-55743b32.js";import"./defineProperty-e26b7022.js";import"./clsx-aa410b3d.js";import"./objectDestructuringEmpty-f3aad883.js";import"./extends-4c19d496.js";import"./index-9d475cdf.js";const g=`<!-- @license CC0-1.0 -->
+
+# Page Header
+
+De page header bevat vaak de volgende onderdelen:
+
+- Logo (met link naar de homepage)
+- Navigatie
+- Zoekveld
+- Kruimelpad-navigatie
+- Eventueel een melding met een waarschuwing, bijvoorbeeld over een actuele storing.
+- Cookie banner.
+
+Het logo maakt duidelijk wie verantwoordelijk is voor de website en geeft daarmee belangrijke context voor de rest van de pagina.
+
+## HTML
+
+Gebruik het HTML \`<header>\` element, zodat het automatisch een \`banner\` landmark krijgt.
+
+## Relevante WCAG eisen
+
+- [WCAG eis 1.3.1](https://www.w3.org/TR/WCAG21/#info-and-relationships): plaats de header in een \`banner\` landmark, door het \`<header>\` element te gebruiken
+- [WCAG eis 1.3.2](https://www.w3.org/TR/WCAG21/#meaningful-sequence): plaats de header aan het begin van pagina, maar na de _skip links_. Zo is het gelijk duidelijk wie verantwoordelijk is voor de pagina die er op volgt.
+`,h={"page-header":{"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"padding-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-block-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}}}},u={utrecht:h},k={title:"React Component/Page Header",id:"react-page-header",component:l,args:{children:d(p,{children:"Header Area"})},parameters:{tokensPrefix:"utrecht-page-header",tokens:m,tokensDefinition:u,docs:{description:{component:g}}}},e={},n=c(k);var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var o,s,i;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:"designTokenStory(meta)",...(i=(s=n.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const D=["Default","DesignTokens"];export{e as Default,n as DesignTokens,D as __namedExportsOrder,k as default};
+//# sourceMappingURL=PageHeader.stories-45db2467.js.map
---
+++ storybook-vue/assets/WithTooltip-V3YHNWJZ-0d8dd9b0.js
@@ -0,0 +1,1 @@
+import{W as T,W as e,a as h}from"./index-825c42ec.js";import"./iframe-a82f178d.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-11d98b33.js";import"./index-4fb43917.js";import"./index-356e4a49.js";export{T as WithToolTipState,e as WithTooltip,h as WithTooltipPure};
