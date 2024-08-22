198f2b32adecb0b590cef148a62629b6e248ccaf
Bryan de Jong <bryan@levitade.io>
2024-08-22 10:26:59 +0200

    feat: support label and icon in button web component

--- packages/web-component-library-stencil/src/components/button.tsx
+++ packages/web-component-library-stencil/src/components/button.tsx
@@ -122,6 +122,9 @@ export class Button {
       button.value = value || '';
     }
 
+    const hasIconSlot = this.hostElement.querySelector('[slot="icon"]');
+    const hasLabelSlot = this.hostElement.querySelector('[slot="label"]');
+
     return (
       <button
         class={clsx(
@@ -145,6 +148,12 @@ export class Button {
         type={this.type || 'button'}
         onClick={handleClick}
       >
+        {hasIconSlot && <slot name="icon"></slot>}
+        {hasLabelSlot && (
+          <span class="utrecht-button__label">
+            <slot name="label"></slot>
+          </span>
+        )}
         <slot></slot>
       </button>
     );
