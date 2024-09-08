23f9e1886550199abe34f172a8c0a191abb0c635
Robbert Broersma <git@robbertbroersma.nl>
2024-09-08 14:54:50 +0200

    feat: improve React NavList with restProps/forwardRef/className support

---
+++ packages/component-library-react/src/NavList.test.tsx
@@ -0,0 +1,89 @@
+import { render, screen } from '@testing-library/react';
+import { createRef } from 'react';
+import { NavList, NavListLink } from './NavList';
+import '@testing-library/jest-dom';
+
+describe('Navigation bar link list', () => {
+  it('renders a list role element', () => {
+    render(<NavList />);
+
+    const list = screen.getByRole('list');
+
+    expect(list).toBeInTheDocument();
+  });
+
+  it('renders an HTML ul element', () => {
+    const { container } = render(<NavList />);
+
+    const list = container.querySelector('ul:only-child');
+
+    expect(list).toBeInTheDocument();
+  });
+
+  it('renders a design system BEM class name', () => {
+    const { container } = render(<NavList />);
+
+    const list = container.querySelector(':only-child');
+
+    expect(list).toHaveClass('utrecht-nav-list');
+  });
+
+  it('can contain a navigation list link', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#">Home</NavListLink>
+      </NavList>,
+    );
+
+    const list = container.querySelector(':only-child');
+
+    const listItem = list?.querySelector(':only-child');
+
+    expect(listItem).toBe(screen.getByRole('listitem'));
+  });
+
+  it.skip('displays as block element', () => {
+    const { container } = render(<NavList />);
+
+    const list = container.querySelector(':only-child');
+
+    expect(list).toBeVisible();
+    expect(list).toHaveStyle({ display: 'block' });
+  });
+
+  it('can be hidden', () => {
+    const { container } = render(<NavList hidden />);
+
+    const list = container.querySelector(':only-child');
+
+    expect(list).not.toBeVisible();
+  });
+
+  it('can have a custom class name', () => {
+    const { container } = render(<NavList className="align-end" />);
+
+    const list = container.querySelector(':only-child');
+
+    expect(list).toHaveClass('align-end');
+  });
+
+  it('can have a additional class name', () => {
+    const { container } = render(<NavList className="align-end" />);
+
+    const list = container.querySelector(':only-child');
+
+    expect(list).toHaveClass('align-end');
+
+    expect(list).toHaveClass('utrecht-nav-list');
+  });
+
+  it('supports ForwardRef in React', () => {
+    const ref = createRef<HTMLUListElement>();
+
+    const { container } = render(<NavList ref={ref} />);
+
+    const list = container.querySelector(':only-child');
+
+    expect(ref.current).toBe(list);
+  });
+});
---
+++ packages/component-library-react/src/NavListLink.test.tsx
@@ -0,0 +1,127 @@
+import { render, screen } from '@testing-library/react';
+import { NavList, NavListLink } from './NavList';
+import '@testing-library/jest-dom';
+
+describe('Navigation bar link list link', () => {
+  it('renders a listitem role element', () => {
+    render(
+      <NavList>
+        <NavListLink href="#">Home</NavListLink>
+      </NavList>,
+    );
+
+    const list = screen.getByRole('listitem');
+
+    expect(list).toBeInTheDocument();
+  });
+
+  it('renders a link role element', () => {
+    render(
+      <NavList>
+        <NavListLink href="#">Home</NavListLink>
+      </NavList>,
+    );
+
+    const list = screen.getByRole('link', { name: 'Home' });
+
+    expect(list).toBeInTheDocument();
+  });
+
+  it('renders an HTML li element', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#">Home</NavListLink>
+      </NavList>,
+    );
+
+    const list = container.querySelector('li:only-child');
+
+    expect(list).toBeInTheDocument();
+  });
+
+  it('renders an HTML a element', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#">Home</NavListLink>
+      </NavList>,
+    );
+
+    const list = container.querySelector('a:only-child');
+
+    expect(list).toBeInTheDocument();
+  });
+
+  it('renders a design system BEM class name: utrecht-nav-list__item', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#">Home</NavListLink>
+      </NavList>,
+    );
+
+    const list = container.querySelector('li:only-child');
+
+    expect(list).toHaveClass('utrecht-nav-list__item');
+  });
+
+  it('renders a design system BEM class name: utrecht-nav-list__link', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#">Home</NavListLink>
+      </NavList>,
+    );
+
+    const list = container.querySelector('a:only-child');
+
+    expect(list).toHaveClass('utrecht-nav-list__link');
+  });
+
+  it('can contain a navigation list link', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#">Home</NavListLink>
+      </NavList>,
+    );
+
+    const list = container.querySelector(':only-child');
+
+    const listItem = list?.querySelector(':only-child');
+
+    expect(listItem).toBe(screen.getByRole('listitem'));
+  });
+
+  it.todo('displays as block element');
+
+  it.todo('can be hidden');
+
+  it('can have a custom class name', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#" className="home">
+          Home
+        </NavListLink>
+      </NavList>,
+    );
+
+    const list = container.querySelector('li:only-child');
+
+    expect(list).toHaveClass('home');
+  });
+
+  it('can have a additional class name', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#" className="home">
+          Home
+        </NavListLink>
+      </NavList>,
+    );
+
+    const list = container.querySelector('li:only-child');
+
+    expect(list).toHaveClass('home');
+
+    expect(list).toHaveClass('utrecht-nav-list__item');
+  });
+
+  it.todo('supports ForwardRef in React');
+});
