f38616cdd1d6a1437fb6ecd1d97886bb832172ad
Robbert Broersma <git@robbertbroersma.nl>
2024-10-09 23:20:40 +0200

    feat: add Image to component-library-pdf

---
+++ packages/storybook-pdf/src/Image.stories.tsx
@@ -0,0 +1,40 @@
+/* @license CC0-1.0 */
+
+import { Meta, StoryObj } from '@storybook/react';
+import { Image } from '@utrecht/component-library-react/src/Image';
+import readme from '@utrecht/img-css/README.md?raw';
+import pdfDocs from '@utrecht/img-css/docs/technology-pdf.nl.md?raw';
+import { mergeMarkdown } from '@utrecht/storybook-helpers/src/markdown';
+import React from 'react';
+import { PdfPreview } from './PdfPreview';
+
+const meta = {
+  title: 'PDF Component/Image',
+  id: 'pdf-image',
+  component: Image,
+  argTypes: {},
+  args: {},
+  parameters: {
+    bugs: 'https://github.com/nl-design-system/utrecht/issues?q=is%3Aissue+is%3Aopen+label%3Acomponent%2Fimage',
+    nldesignsystem: 'https://nldesignsystem.nl/image',
+    status: {
+      type: 'ALPHA',
+    },
+    docs: {
+      description: {
+        component: mergeMarkdown([readme, pdfDocs]),
+      },
+    },
+  },
+  render: () => <PdfPreview component="Link" src="pdf-image-alt-1.png" download="pdf-image-alt.pdf" />,
+} satisfies Meta<typeof Image>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const ImageNotFound: Story = {
+  render: () => <PdfPreview component="Link" src="pdf-image-actual-text-1.png" download="pdf-image-actual-text.pdf" />,
+};
