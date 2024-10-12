ac959f9eda95812c9169ecd6053e08a3af16c109
Robbert Broersma <git@robbertbroersma.nl>
2024-10-12 14:29:21 +0200

    style: improve looks of PDF preview in Storybook

--- packages/storybook-pdf/src/PdfPreview.tsx
+++ packages/storybook-pdf/src/PdfPreview.tsx
@@ -1,6 +1,7 @@
 /* global process */
 
 import React from 'react';
+import './PdfPreview.css';
 
 export const PdfPreview = ({
   src,
@@ -13,10 +14,16 @@ export const PdfPreview = ({
   baseUrl?: string;
   download?: string;
 }) => {
-  const img = <img src={`${baseUrl}${src}`} alt={`${component} example, 1st page of the PDF is shown`} />;
+  const img = (
+    <img
+      className="utrecht-pdf-preview__image"
+      src={`${baseUrl}${src}`}
+      alt={`${component} example, 1st page of the PDF is shown`}
+    />
+  );
   if (download) {
     return (
-      <a href={`${baseUrl}${download}`} target="pdf" aria-label="Open PDF file">
+      <a className="utrecht-pdf-preview" href={`${baseUrl}${download}`} target="pdf" aria-label="Open PDF file">
         {img}
       </a>
     );
