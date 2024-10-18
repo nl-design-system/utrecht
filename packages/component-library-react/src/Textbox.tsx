82e2f5f7b5919016ffca020a5e84e000892cae8e
Ali Amori Kadhim <ali@frameless.io>
2024-10-18 14:24:47 +0200

    refactor: re-export React Textbox component package

--- packages/component-library-react/src/Textbox.tsx
+++ packages/component-library-react/src/Textbox.tsx
@@ -1,63 +1,8 @@
-import clsx from 'clsx';
-import { ForwardedRef, forwardRef, InputHTMLAttributes } from 'react';
-export type TextboxTypes =
-  | 'date'
-  | 'datetime-local'
-  | 'email'
-  | 'month'
-  | 'number'
-  | 'password'
-  | 'search'
-  | 'tel'
-  | 'text'
-  | 'time'
-  | 'url'
-  | 'week';
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2025 Frameless B.V.
+ * Copyright (c) 2021-2025 Gemeente Utrecht
+ */
 
-export interface TextboxProps extends InputHTMLAttributes<HTMLInputElement> {
-  inputRequired?: boolean;
-  invalid?: boolean;
-  type?: string | TextboxTypes;
-}
-
-export const Textbox = forwardRef(
-  (
-    {
-      dir,
-      disabled,
-      invalid,
-      readOnly,
-      required,
-      inputRequired,
-      className,
-      type = 'text',
-      inputMode,
-      ...restProps
-    }: TextboxProps,
-    ref: ForwardedRef<HTMLInputElement>,
-  ) => (
-    <input
-      {...restProps}
-      ref={ref}
-      type={type}
-      className={clsx(
-        'utrecht-textbox',
-        'utrecht-textbox--html-input',
-        disabled && 'utrecht-textbox--disabled',
-        invalid && 'utrecht-textbox--invalid',
-        readOnly && 'utrecht-textbox--readonly',
-        (required || inputRequired) && 'utrecht-textbox--required',
-        className,
-      )}
-      dir={dir ?? 'auto'}
-      disabled={disabled}
-      readOnly={readOnly}
-      aria-required={required ? required : undefined}
-      required={inputRequired}
-      aria-invalid={invalid || undefined}
-      inputMode={inputMode || (type === 'number' ? 'numeric' : undefined)}
-    />
-  ),
-);
-
-Textbox.displayName = 'Textbox';
+export type { TextboxProps, TextboxTypes } from '@utrecht/textbox-react';
+export { Textbox } from '@utrecht/textbox-react';
