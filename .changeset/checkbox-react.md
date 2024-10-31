6ec21ae43aa5bbe476c770428a76ae8134bdc5cb
Robbert Broersma <git@robbertbroersma.nl>
2024-10-31 17:41:57 +0100

    feat: add Checkbox React npm package

---
+++ .changeset/checkbox-react.md
@@ -0,0 +1,5 @@
+---
+"@utrecht/checkbox-react": major
+---
+
+Create npm package for the Checkbox React component.
---
+++ packages/components-react/checkbox-react/src/css.tsx
@@ -0,0 +1,10 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+import '@utrecht/custom-checkbox-css/src/index.scss';
+import '@utrecht/checkbox-css/src/index.scss';
+
+export type { CheckboxProps } from './index';
+export { Checkbox } from './index';
