6234ec2a21261f7cb0aa14df25b337f90caf003e
Robbert Broersma <git@robbertbroersma.nl>
2024-11-03 21:30:58 +0100

    feat: add Form Field Checkbox React npm package

---
+++ .changeset/form-field-checkbox-react.md
@@ -0,0 +1,5 @@
+---
+"@utrecht/form-field-checkbox-react": major
+---
+
+Create npm package for the Form Field Checkbox React component.
---
+++ packages/components-react/form-field-checkbox-react/src/css.tsx
@@ -0,0 +1,12 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+// Inject CSS from components used in `FormFieldCheckbox`
+import '@utrecht/checkbox-react/dist/css';
+import '@utrecht/form-field-react/dist/css';
+import '@utrecht/form-field-description-react/dist/css';
+import '@utrecht/form-field-error-message-react/dist/css';
+
+export * from './index';
