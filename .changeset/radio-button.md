56b70ced9bcaf9bd2e15aed091537afc18223809
Robbert Broersma <git@robbertbroersma.nl>
2024-11-04 00:09:36 +0100

    feat: add Radio Button React npm package

---
+++ .changeset/radio-button.md
@@ -0,0 +1,5 @@
+---
+"@utrecht/radio-button-react": major
+---
+
+Create npm package for the Radio Button React component.
---
+++ packages/components-react/radio-button-react/src/css.tsx
@@ -0,0 +1,9 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
+import '@utrecht/radio-button-css/src/index.scss';
+
+export type { RadioButtonProps } from './index';
+export { RadioButton } from './index';
