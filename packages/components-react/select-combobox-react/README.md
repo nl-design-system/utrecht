7907a7ddaab32396339d4926df5bc79b99f27967
Robbert Broersma <git@robbertbroersma.nl>
2024-11-05 23:02:36 +0100

    feat: add Select Combobox component

---
+++ packages/components-react/select-combobox-react/README.md
@@ -0,0 +1,5 @@
+# Select Combobox
+
+## WAI-ARIA 1.2
+
+This combobox implementation is based on WAI-ARIA 1.2, where the combobox select pattern is implemented much differently compared to the previous verion, WAI-ARIA 1.1.
---
+++ packages/components-react/select-combobox-react/src/css.tsx
@@ -0,0 +1,11 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2025 Frameless B.V.
+ * Copyright (c) 2021-2025 Gemeente Utrecht
+ */
+
+import '@utrecht/combobox-react/dist/css';
+import '@utrecht/listbox-react/dist/css';
+import '@utrecht/textbox-react/dist/css';
+
+export * from './index';
