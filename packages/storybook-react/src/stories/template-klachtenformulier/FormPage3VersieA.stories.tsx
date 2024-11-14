3fa10857a0e1083674fe17025d985a82a7acc856
Olling <48805670+Ollie-nl@users.noreply.github.com>
2024-11-14 10:13:31 +0100

    fix: textarea conter

--- packages/storybook-react/src/stories/template-klachtenformulier/FormPage3VersieA.stories.tsx
+++ packages/storybook-react/src/stories/template-klachtenformulier/FormPage3VersieA.stories.tsx
@@ -6,20 +6,12 @@ import {
   Button,
   ButtonGroup,
   ButtonLink,
-  DataList,
-  DataListItem,
-  DataListKey,
-  DataListValue,
-  FormField,
   FormFieldTextarea,
   FormFieldTextbox,
   FormLabel,
   Heading1,
   Heading2,
   Heading3,
-  Link,
-  LinkList,
-  LinkListLink,
   Logo,
   LogoImage,
   Page,
@@ -29,26 +21,16 @@ import {
   Paragraph,
   Separator,
   Strong,
-  Textbox,
   UnorderedList,
   UnorderedListItem,
 } from '@utrecht/component-library-react/dist/css-module';
 import {
-  UtrechtBreadcrumbNav,
-  UtrechtDigidButton,
-  UtrechtFormFieldDescription,
   UtrechtIconArrow,
   UtrechtIconChevronLeft,
   UtrechtIconCross,
-  UtrechtIconFacebook,
-  UtrechtIconInstagram,
-  UtrechtIconLinkedin,
-  UtrechtIconNieuwHuis,
-  UtrechtIconNieuwsbrief,
-  UtrechtIconWhatsapp,
   UtrechtLogo,
 } from '@utrecht/web-component-library-react';
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import FooterKlachten from './FooterKlachtenFormulier'; // Importeer het footer-component
 
@@ -63,69 +45,96 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const One: Story = {
-  render: (args) => (
-    <Page {...args}>
-      <PageHeader>
-        <nav>
-          <Logo>
-            <LogoImage />
-          </Logo>
-        </nav>
-      </PageHeader>
-      <PageContent style={{ '--utrecht-space-around': 1 } as any}>
-        <BreadcrumbNav>
-          <BreadcrumbNavLink href="#">
-            <BreadcrumbNavSeparator>
-              <UtrechtIconChevronLeft />
-            </BreadcrumbNavSeparator>
-            Terug
-          </BreadcrumbNavLink>
-        </BreadcrumbNav>
-        <Heading1>Klacht over de gemeente</Heading1>
-        <section>
-          <Heading2>Uw Klacht</Heading2>
-          <Paragraph>Een duidelijke beschrijving helpt ons bij het behandelen van de klacht.</Paragraph>
-          <Paragraph>Vul alle velden in. Als een veld niet verplicht is, staat dit erbij.</Paragraph>
-          <FormFieldTextarea label="Wat is uw klacht?" name="klacht" rows={5} required />
-          <br />
-          <FormFieldTextbox
-            label="Datum (niet verplicht)"
-            name="datum"
-            description="Bijvoorbeeld: 23 januari of 2 dagen geleden"
-          />
-          <br />
-          <FormFieldTextbox label="Tijdstip (niet verplicht)" name="tijdstip" description="Bijvoorbeeld: 13:15" />
-          <br />
-          <FormFieldTextarea label="Locatie (niet verplicht)" name="locatie" rows={5} />
-          <FormLabel>Bestand toevoegen (niet verplicht)</FormLabel>
-          <Paragraph>Bijvoorbeeld: een foto van de situatie die de klacht duidelijker maakt</Paragraph>
-          <UnorderedList>
-            <UnorderedListItem>U kunt meerdere bestanden tegelijk toevoegen.</UnorderedListItem>
-            <UnorderedListItem>U mag maximaal 10 MB aan bestanden toevoegen.</UnorderedListItem>
-            <UnorderedListItem>
-              Toegestane bestandstypen: doc, docx, xslx, pdf, zip, jpg, png, bmp en gif.
-            </UnorderedListItem>
-          </UnorderedList>
-          <br />
-          <Button appearance="secondary-action-button" type="submit">
-            Bestand toevoegen
-          </Button>
-          <br />
-          <br />
-          <ButtonGroup direction="column">
-            <Button appearance="primary-action-button">
-              Volgende stap <UtrechtIconArrow />
+  render: (args) => {
+    const MAX_CHARACTERS = 1000;
+    const [characterCount, setCharacterCount] = useState(0);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setCharacterCount(event.target.value.length);
+    };
+
+    // Statusbericht genereren op basis van karakteraantal
+    const getStatusMessage = () => {
+      if (characterCount > MAX_CHARACTERS) {
+        return `Let op, u heeft ${characterCount - MAX_CHARACTERS} tekens te veel ingevoerd.`;
+      }
+      return `U heeft nog ${MAX_CHARACTERS - characterCount} tekens over.`;
+    };
+
+    return (
+      <Page {...args}>
+        <PageHeader>
+          <nav>
+            <Logo>
+              <LogoImage />
+            </Logo>
+          </nav>
+        </PageHeader>
+        <PageContent style={{ '--utrecht-space-around': 1 } as any}>
+          <BreadcrumbNav>
+            <BreadcrumbNavLink href="#">
+              <BreadcrumbNavSeparator>
+                <UtrechtIconChevronLeft />
+              </BreadcrumbNavSeparator>
+              Terug
+            </BreadcrumbNavLink>
+          </BreadcrumbNav>
+          <Heading1>Klacht over de gemeente</Heading1>
+          <section>
+            <Heading2>Uw Klacht</Heading2>
+            <Paragraph>
+              Een duidelijke beschrijving helpt ons bij het behandelen van de klacht.
+              <br />
+              Vul alle velden in. Als een veld niet verplicht is, staat dit erbij.
+            </Paragraph>
+            {/* Dynamische FormFieldTextarea met aangepaste karaktertelling */}
+            <FormFieldTextarea
+              label="Wat is uw klacht?"
+              name="klacht"
+              rows={5}
+              onChange={handleInputChange}
+              status={getStatusMessage()} // Gebruik de aangepaste statusfunctie
+            />
+            <br />
+            <FormFieldTextbox
+              label="Datum (niet verplicht)"
+              name="datum"
+              description="Bijvoorbeeld: 23 januari of 2 dagen geleden"
+            />
+            <br />
+            <FormFieldTextbox label="Tijdstip (niet verplicht)" name="tijdstip" description="Bijvoorbeeld: 13:15" />
+            <br />
+            <FormFieldTextarea label="Locatie (niet verplicht)" name="locatie" rows={3} />
+            <FormLabel>Bestand toevoegen (niet verplicht)</FormLabel>
+            <Paragraph>Bijvoorbeeld: een foto van de situatie die de klacht duidelijker maakt</Paragraph>
+            <UnorderedList>
+              <UnorderedListItem>U kunt meerdere bestanden tegelijk toevoegen.</UnorderedListItem>
+              <UnorderedListItem>U mag maximaal 10 MB aan bestanden toevoegen.</UnorderedListItem>
+              <UnorderedListItem>
+                Toegestane bestandstypen: doc, docx, xslx, pdf, zip, jpg, png, bmp en gif.
+              </UnorderedListItem>
+            </UnorderedList>
+            <br />
+            <Button appearance="secondary-action-button" type="submit">
+              Bestand toevoegen
             </Button>
-            <Button appearance="subtle-button">
-              <UtrechtIconArrow /> Opslaan en later verder
-            </Button>
-            <Button appearance="subtle-button">
-              <UtrechtIconCross /> Stoppen met formulier
-            </Button>
-          </ButtonGroup>
-        </section>
-      </PageContent>
-      <FooterKlachten />
-    </Page>
-  ),
+            <br />
+            <br />
+            <ButtonGroup direction="column">
+              <Button appearance="primary-action-button">
+                Volgende stap <UtrechtIconArrow />
+              </Button>
+              <Button appearance="subtle-button">
+                <UtrechtIconArrow /> Opslaan en later verder
+              </Button>
+              <Button appearance="subtle-button">
+                <UtrechtIconCross /> Stoppen met formulier
+              </Button>
+            </ButtonGroup>
+          </section>
+        </PageContent>
+        <FooterKlachten />
+      </Page>
+    );
+  },
 };
