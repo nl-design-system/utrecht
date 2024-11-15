cef74ec6f5a920c008e192e74f8c1b7a346a1874
Jeroen du Chatinier <49227194+JeroenduC@users.noreply.github.com>
2024-11-15 13:44:35 +0100

    feat: verwijzingen naar src bestand gemaakt in introductie en scenario

---
+++ packages/storybook-react/src/stories/prototype-src/IntroductieKlachtenFormulier.tsx
@@ -0,0 +1,45 @@
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Link,
+  LinkList,
+  LinkListLink,
+  PageFooter,
+  Paragraph,
+  Strong,
+  UnorderedList,
+  UnorderedListItem,
+} from '@utrecht/component-library-react/dist/css-module';
+import {
+  UtrechtIconArrow,
+  UtrechtIconFacebook,
+  UtrechtIconInstagram,
+  UtrechtIconLinkedin,
+  UtrechtIconNieuwsbrief,
+  UtrechtIconWhatsapp,
+} from '@utrecht/web-component-library-react';
+import React from 'react';
+const IntroductieKlachten: React.FC = () => {
+  return (
+    <section>
+      <Heading1>Test invullen klacht</Heading1>
+      <Paragraph>
+        Dank je wel dat je ons wilt helpen met het testen van het klachtenformulier! In deze test vragen we je een
+        klacht in te voeren. Een paar belangrijke dingen voordat je begint:
+      </Paragraph>
+      <UnorderedList>
+        <UnorderedListItem>Je kunt geen fouten maken. Wij testen het product werkt, niet jou.</UnorderedListItem>
+        <UnorderedListItem>
+          Vertel hardop wat je doet. Zeg wat je ziet, wat je denkt en wat je wilt doen.
+        </UnorderedListItem>
+        <UnorderedListItem>
+          Ik mag je niet helpen tijdens de test. We willen zien hoe jij het zelf doet.
+        </UnorderedListItem>
+      </UnorderedList>
+    </section>
+  );
+};
+
+export default IntroductieKlachten;
