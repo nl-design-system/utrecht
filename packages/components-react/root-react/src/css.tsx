bb7296c5fc69caa8cc4a22815757a4b51d3e98d9
Robbert Broersma <git@robbertbroersma.nl>
2024-11-17 10:56:50 +0100

    feat: new `Root` React component

---
+++ packages/components-react/root-react/src/css.tsx
@@ -0,0 +1,9 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2024 Frameless B.V.
+ * Copyright (c) 2021-2024 Gemeente Utrecht
+ */
+
+import '@utrecht/root-css/src/index.scss';
+
+export * from './index';
