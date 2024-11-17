be4100da5d7170eccea7a90fd6bba251b0c5d78a
Robbert Broersma <git@robbertbroersma.nl>
2024-11-17 20:01:52 +0100

    refactor: switch to shared CSS utils

--- packages/component-library-css/rollup.config.mjs
+++ packages/component-library-css/rollup.config.mjs
@@ -1,96 +1,10 @@
 import postcss from 'rollup-plugin-postcss';
-import { Comment } from 'postcss';
 import discardDuplicates from 'postcss-discard-duplicates';
 import postcssLogical from 'postcss-logical';
-
-/**
- * Remove the `@charset "UTF-8";` declararation.
- * Prince XML does not support such declaration.
- */
-const removeCharset = () => {
-  return (css) => {
-    css.walkAtRules('charset', (atRule) => {
-      atRule.remove();
-    });
-  };
-};
-
-const isLicenseComment = (str) => /@license/.test(str);
-
-const removeComments = ({ keepLicense = false }) => {
-  return (css) => {
-    css.walk((node) => {
-      if (node.type === 'comment') {
-        if (keepLicense && isLicenseComment(node.text)) {
-          return;
-        }
-        node.remove();
-      }
-    });
-  };
-};
-
-const workaroundsPrinceXML = () => {
-  return (css) => {
-    css.walk((node) => {
-      if (node.type === 'rule') {
-        // Rewrite `a:any-link` selector to `a[href]` because Prince XML does not support `:any-link`.
-        // Rewriing to `a:link, a:visited` seemed too difficult.
-        node.selector = node.selector.replaceAll(':any-link', '[href]');
-
-        // Rewrite pseudo class selectors in a way Prince XML doesn't complain about it
-        node.selector = node.selector
-          .replaceAll(':focus-visible', '[prince-xml-ignore-pseudo-class-focus-visible]')
-          .replaceAll(':placeholder-shown', '[prince-xml-ignore-pseudo-class-placeholder-shown]');
-
-        // Rewrite pseudo element selectors in a way Prince XML doesn't complain about it
-        node.selector = node.selector
-          .replaceAll('::placeholder', '[prince-xml-ignore-pseudo-element-placeholder]')
-          .replaceAll('::backdrop', '[prince-xml-ignore-pseudo-element-backdrop]');
-      } else if (
-        node.type === 'atrule' &&
-        /prefers-contrast|prefers-reduced-motion|prefers-reduced-transparency|forced-colors/.test(node.params)
-      ) {
-        // Remove `@media` rules that target non-print media
-        node.remove();
-      } else if (
-        node.type === 'decl' &&
-        ['print-color-adjust', 'text-decoration-thickness', 'white-space-collapse'].includes(node.prop)
-      ) {
-        // Remove non-essential CSS declarations that Prince XML 15.4 does not support.
-        node.remove();
-      }
-    });
-  };
-};
-
-const mergeLicenseComments = () => {
-  return (css) => {
-    // Add all licenses to a `Map`, to deduplicate the license declarations.
-    const licenses = new Map();
-    css.walkComments((node) => {
-      if (isLicenseComment(node.text)) {
-        node.remove();
-
-        if (!licenses.has(node.text)) {
-          licenses.set(
-            node.text,
-            new Comment({
-              text: node.text,
-              raws: {
-                left: node.raws.left,
-                right: node.raws.right,
-              },
-            }),
-          );
-        }
-      }
-    });
-    licenses.forEach((license) => {
-      css.prepend(license);
-    });
-  };
-};
+import { removeCharset } from '@utrecht/build-utils-css/src/remove-charset.mjs';
+import { mergeLicenseComments } from '@utrecht/build-utils-css/src/remove-duplicate-license.mjs';
+import { removeComments } from '@utrecht/build-utils-css/src/remove-comments.mjs';
+import { workaroundsPrinceXML } from '@utrecht/build-utils-css/src/prince-workarounds.mjs';
 
 export default [
   {
