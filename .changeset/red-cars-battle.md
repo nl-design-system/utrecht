436b7a2382baf361c33d2cf91dcb000e5984eb20
Robbert Broersma <git@robbertbroersma.nl>
2024-11-17 11:11:47 +0100

    feat: create npm package for `PageFooter` React component

---
+++ .changeset/red-cars-battle.md
@@ -0,0 +1,5 @@
+---
+"@utrecht/page-footer-react": major
+---
+
+Create new npm package for `PageFooter` React component.
---
+++ packages/components-react/page-footer-react/src/index.tsx
@@ -0,0 +1,20 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2024 Frameless B.V.
+ * Copyright (c) 2021-2024 Gemeente Utrecht
+ */
+
+import clsx from 'clsx';
+import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react';
+
+export type PageFooterProps = HTMLAttributes<HTMLDivElement>;
+
+export const PageFooter = forwardRef(
+  ({ children, className, ...restProps }: PropsWithChildren<PageFooterProps>, ref: ForwardedRef<HTMLDivElement>) => (
+    <footer {...restProps} ref={ref} className={clsx('utrecht-page-footer', className)}>
+      <div className="utrecht-page-footer__content">{children}</div>
+    </footer>
+  ),
+);
+
+PageFooter.displayName = 'PageFooter';
