0e7516140c0e8c1b2b2619d6bff2e7af0a0d1c7a
Robbert Broersma <git@robbertbroersma.nl>
2024-11-24 14:35:56 +0100

    fix: broken import in `radio-group-react`

--- packages/components-react/radio-group-react/src/css.tsx
+++ packages/components-react/radio-group-react/src/css.tsx
@@ -7,7 +7,7 @@
 // Inject CSS from components used in this component
 import '@utrecht/radio-button-react/dist/css';
 import '@utrecht/form-field-react/dist/css';
-import '@utrecht/form-label/dist/css';
+import '@utrecht/form-label-react/dist/css';
 import '@utrecht/form-field-description-react/dist/css';
 import '@utrecht/form-field-error-message-react/dist/css';
 import '@utrecht/fieldset-react/css';
