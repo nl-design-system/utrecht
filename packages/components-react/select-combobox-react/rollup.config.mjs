60c53cb235fa0a2d75ee5713fd8d3eb8e27c141f
Robbert Broersma <git@robbertbroersma.nl>
2024-11-24 16:49:47 +0100

    build: align rollup config for SelectCombobox with `build-react-package`

--- packages/components-react/select-combobox-react/rollup.config.mjs
+++ packages/components-react/select-combobox-react/rollup.config.mjs
@@ -1,16 +1,8 @@
-import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { readFileSync } from 'fs';
 import filesize from 'rollup-plugin-filesize';
 import nodeExternal from 'rollup-plugin-node-externals';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-
-const packagePath = new URL('./package.json', import.meta.url).pathname;
-const packageJson = JSON.parse(readFileSync(packagePath, 'utf8'));
 
 /**
  * @type {import('rollup').RollupOptions}
@@ -20,31 +12,21 @@ export default [
     input: './src/index.tsx',
     output: [
       {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
+        file: './dist/index.mjs',
         format: 'es',
-        sourcemap: true,
+        // sourcemap: true,
       },
     ],
     plugins: [
-      peerDepsExternal(),
+      // Convert CommonJS modules to ES Modules
       commonjs(),
+
+      // Inline imports from `devDependencies`
+      // Leave `dependencies` and `devDependencies` imports as is.
+      // See: https://www.npmjs.com/package/rollup-plugin-node-externals
       nodeExternal(),
-      resolve({ browser: true }),
-      json(),
+      resolve(),
       typescript({ tsconfig: './tsconfig.build.json' }),
-      babel({
-        presets: ['@babel/preset-react'],
-        babelHelpers: 'runtime',
-        exclude: ['node_modules/**', 'dist/**'],
-        extensions: ['.ts', '.tsx'],
-        inputSourceMap: true,
-        plugins: ['@babel/plugin-transform-runtime'],
-      }),
       filesize(),
     ],
   },
@@ -52,35 +34,21 @@ export default [
     input: './src/css.tsx',
     output: [
       {
-        file: './dist/css.cjs',
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
         file: './dist/css.mjs',
         format: 'es',
-        sourcemap: true,
+        // sourcemap: true,
       },
     ],
     plugins: [
-      peerDepsExternal(),
+      // Convert CommonJS modules to ES Modules
       commonjs(),
+
+      // Inline imports from `devDependencies`
+      // Leave `dependencies` and `devDependencies` imports as is.
+      // See: https://www.npmjs.com/package/rollup-plugin-node-externals
       nodeExternal(),
-      resolve({ browser: true }),
-      json(),
-      postcss({
-        extensions: ['.css', '.scss'],
-        minimize: true,
-      }),
+      resolve(),
       typescript({ tsconfig: './tsconfig.build.json' }),
-      babel({
-        presets: ['@babel/preset-react'],
-        babelHelpers: 'runtime',
-        exclude: ['node_modules/**', 'dist/**'],
-        extensions: ['.ts', '.tsx'],
-        inputSourceMap: true,
-        plugins: ['@babel/plugin-transform-runtime'],
-      }),
       filesize(),
     ],
   },
