6756fb60c62526f3dd058bbca7be9130cdf8efc9
Robbert Broersma <git@robbertbroersma.nl>
2024-11-26 00:11:38 +0100

    fix: correct `stroke-width` syntax in SVG icon

--- packages/component-library-react/src/Accordion.tsx
+++ packages/component-library-react/src/Accordion.tsx
@@ -25,7 +25,7 @@ const IconChevronDown = () => (
     <g style={{ clipPath: 'url(#clippath)' }}>
       <path
         d="m7,8c-.26,0-.51-.1-.71-.29L.29,1.71C-.1,1.32-.1.68.29.29S1.32-.1,1.71.29l5.29,5.29L12.29.29c.39-.39,1.02-.39,1.41,0s.39,1.02,0,1.41l-6,6c-.2.2-.45.29-.71.29Z"
-        style={{ strokeWidth: '0px;' }}
+        style={{ strokeWidth: '0px' }}
       />
     </g>
   </svg>
