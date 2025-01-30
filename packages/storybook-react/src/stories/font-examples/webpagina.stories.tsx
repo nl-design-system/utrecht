4169bf51aa947c1e3689f5207d3c3d411a22bb8c
Olling <48805670+Ollie-nl@users.noreply.github.com>
2025-01-30 09:17:24 +0100

    feat: prototype fontTester
    feat: prototype fontTester

---
+++ packages/storybook-react/src/stories/font-examples/webpagina.stories.tsx
@@ -0,0 +1,94 @@
+import { Meta, StoryObj } from '@storybook/react';
+import {
+  BreadcrumbNav,
+  BreadcrumbNavLink,
+  BreadcrumbNavSeparator,
+  Button,
+  ButtonLink,
+  DataList,
+  DataListItem,
+  DataListKey,
+  DataListValue,
+  FormField,
+  FormLabel,
+  Heading1,
+  Heading2,
+  Heading3,
+  Link,
+  LinkList,
+  LinkListLink,
+  Logo,
+  LogoImage,
+  Page,
+  PageContent,
+  PageFooter,
+  PageHeader,
+  Paragraph,
+  Separator,
+  SpotlightSection,
+  Strong,
+  Surface,
+  Textbox,
+  UnorderedList,
+  UnorderedListItem,
+} from '@utrecht/component-library-react/dist/css-module';
+import {
+  UtrechtBreadcrumbNav,
+  UtrechtDigidButton,
+  UtrechtFormFieldDescription,
+  UtrechtIconArrow,
+  UtrechtIconChevronLeft,
+  UtrechtIconFacebook,
+  UtrechtIconInstagram,
+  UtrechtIconLinkedin,
+  UtrechtIconNieuwHuis,
+  UtrechtIconNieuwsbrief,
+  UtrechtIconWhatsapp,
+  UtrechtLogo,
+} from '@utrecht/web-component-library-react';
+import React from 'react';
+import '../prototype-src/index.css'; // Importeer stylesheet
+import ContentWebpage from '../prototype-src/ContentWebpage'; // Importeer het footer-component
+import FontTester from '../prototype-src/FontTester'; // ✅ Importeer de herbruikbare font-tester
+import FooterKlachten from '../prototype-src/FooterKlachtenFormulier.js'; // Importeer het footer-component
+import urls from '../prototype-src/variables.js';
+
+const meta = {
+  title: 'Prototypes/Font voorbeeld/webpagina',
+  id: 'webpagina-font1',
+  component: Page,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Page>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const One: Story = {
+  render: (args) => (
+    <Surface className="utrecht-html">
+      <Page {...args}>
+        <PageHeader>
+          <Logo>
+            <LogoImage />
+          </Logo>
+        </PageHeader>
+        <PageContent style={{ '--utrecht-space-around': 1 } as any}>
+          <main>
+            <section>
+              <ContentWebpage />
+              <ButtonLink appearance="primary-action-button" href={urls.scenarioAscenario}>
+                Starten
+                <UtrechtIconArrow />
+              </ButtonLink>
+            </section>
+          </main>
+        </PageContent>
+        <FooterKlachten />
+      </Page>
+      <FontTester></FontTester>
+    </Surface>
+  ),
+};
---
+++ packages/storybook-react/src/stories/prototype-src/ContentWebpage.tsx
@@ -0,0 +1,58 @@
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Link,
+  LinkList,
+  LinkListLink,
+  PageFooter,
+  Paragraph,
+  Strong,
+  UnorderedList,
+  UnorderedListItem,
+} from '@utrecht/component-library-react/dist/css-module';
+import {
+  UtrechtIconArrow,
+  UtrechtIconFacebook,
+  UtrechtIconInstagram,
+  UtrechtIconLinkedin,
+  UtrechtIconNieuwsbrief,
+  UtrechtIconWhatsapp,
+} from '@utrecht/web-component-library-react';
+import React from 'react';
+const IntroductieKlachten: React.FC = () => {
+  return (
+    <>
+      <Heading1>Forcem Ipsum</Heading1>
+      <Paragraph>
+        Echo Three to Echo Seven. Han, old buddy, do you read me? Loud and clear, kid. What&apos;s up? Well, I finished
+        my circle. I don&apos;t pick up any life readings. There isn&apos;t enough life on this ice cube to fill a space
+        cruiser. The sensors are placed. I&apos;m going back.
+      </Paragraph>
+      <Paragraph>
+        I&apos;ll get my people to work on it. Good. You know, that ship saved my life quite a few times. She&apos;s the
+        fastest hunk of junk in the galaxy. Detach! You do have your moments. Not many, but you have them.
+        Concentrate...feel the Force flow. Yes.
+      </Paragraph>
+      <Heading2>Forcem Ipsum</Heading2>
+      <Paragraph>
+        But you didn&apos;t see us alone in the south passage. She expressed her true feelings for me. My...! Why, you
+        stuck up...half-witted...scruffy-looking...nerf-herder! Who&apos;s scruffy-looking? I must have hit her pretty
+        close to the mark to get her all riled up like that, huh, kid? Why, I guess you don&apos;t know everything about
+        women yet? Headquarters personnel, report to command center. Take it easy. Excuse us, please. Princess...we have
+        a visitor. We&apos;ve picked up something outside the base in zone twelve, moving east.
+      </Paragraph>
+      <Paragraph>
+        Hello? How interesting. Oh, my. Who are you? Oh, I&apos;m terribly sorry. I, I didn&apos;t mean to intrude.
+      </Paragraph>
+      <UnorderedList>
+        <UnorderedListItem>Did your men deactivate the hyperdrive </UnorderedListItem>
+        <UnorderedListItem>The Empire has takes control of the city.</UnorderedListItem>
+        <UnorderedListItem>I don&apos;t think it&apos;s wise to</UnorderedListItem>
+      </UnorderedList>
+    </>
+  );
+};
+
+export default IntroductieKlachten;
