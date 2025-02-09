490a2e8bceb2edb33c7114cb9c2b33ce81fe143e
Robbert Broersma <git@robbertbroersma.nl>
2025-02-09 12:41:09 +0100

    docs: add stories for Body, Root and PageBody for React and Web Components

---
+++ packages/storybook-react/src/stories/Root.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react';
+import tokens from '@utrecht/design-tokens/dist/list.mjs';
+import readme from '@utrecht/root-css/README.md?raw';
+import tokensDefinition from '@utrecht/root-css/dist/tokens.mjs';
+import { Root } from '@utrecht/root-react/dist/css';
+import React from 'react';
+import { designTokenStory } from './util.js';
+
+const meta = {
+  title: 'React Component/Root',
+  id: 'react-root',
+  component: Root,
+  args: {
+    children: <Root>Root component</Root>,
+  },
+  parameters: {
+    tokensPrefix: 'utrecht-root',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof Root>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const DesignTokens = designTokenStory(meta);
---
+++ packages/storybook-web-component/src/PageBody.stories.tsx
@@ -0,0 +1,44 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import tokens from '@utrecht/design-tokens/dist/index.json';
+import readme from '@utrecht/page-body-css/README.md?raw';
+import tokensDefinition from '@utrecht/page-body-css/src/tokens.json';
+import { UtrechtPageBody } from '@utrecht/web-component-library-react';
+import { designTokenStory } from './design-token-story';
+
+const meta = {
+  title: 'Web Component/PageBody',
+  id: 'web-component-page-body',
+  component: UtrechtPageBody,
+  argTypes: {
+    children: {
+      description: 'Content of the page body',
+    },
+  },
+  args: {
+    children: ['Page body content'],
+  },
+  tags: ['autodocs'],
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    tokensPrefix: 'utrecht-page-body',
+    tokens,
+    tokensDefinition,
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof UtrechtPageBody>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const DesignTokens = designTokenStory(meta);
