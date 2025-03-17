a7439ffa6b108e821e53ac36b8f290c344afb850
Menno van Slooten <menno.vanslooten@gmail.com>
2025-03-17 15:56:43 +0100

    fix: add checkbox test to accordion

--- packages/component-library-react/src/Accordion.test.tsx
+++ packages/component-library-react/src/Accordion.test.tsx
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
-import { AccordionProvider } from './Accordion';
 import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import React, { act } from 'react';
+import { AccordionProvider } from './Accordion';
 
-describe('AccordionProvider', () => {
+describe('with reactive components in body', () => {
+  it('does something', () => {
+    let setStateCheckboxOuter, stateCheckboxOuter;
+    const Checkbox = () => {
+      const [stateCheckbox, setStateCheckbox] = React.useState(false);
+      stateCheckboxOuter = stateCheckbox;
+      setStateCheckboxOuter = setStateCheckbox;
+      return (
+        <input
+          type="checkbox"
+          defaultChecked={stateCheckbox}
+          id="state-checkbox"
+          role="checkbox"
+          onChange={() => setStateCheckbox((checked) => !checked)}
+        />
+      );
+    };
+    render(
+      <AccordionProvider
+        sections={[
+          {
+            label: 'Accordion section',
+            expanded: true,
+            body: <Checkbox />,
+          },
+        ]}
+      />,
+    );
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument();
+
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
+
+    act(() => {
+      fireEvent.click(screen.getByRole('checkbox'));
+    });
+
+    expect(screen.getByRole('checkbox')).toBeChecked();
+
+    console.log('should be false', setStateCheckboxOuter);
+    if (setStateCheckboxOuter) {
+      act(() => {
+        setStateCheckboxOuter(false);
+      });
+    }
+
+    expect(stateCheckboxOuter).toBe(false);
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
+  });
+});
+
+xdescribe('AccordionProvider', () => {
   const accordionHeading = 'Frequently Asked Questions';
   const sectionContent = 'Roughly 39 kilometers per hour.';
   const sectionHeading = 'What Is the Airspeed Velocity of an Unladen Swallow?';
