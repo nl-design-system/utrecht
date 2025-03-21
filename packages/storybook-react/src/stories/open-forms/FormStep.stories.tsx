929f39af2f014112d24381646ee21b57426d63f5
Ali Amori Kadhim <ali@frameless.io>
2025-03-21 10:20:30 +0100

    docs(storybook-react): add FormStep to storybook

---
+++ packages/storybook-react/src/stories/open-forms/FormStep.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { SingleFormioComponent } from './StoryUtil.js';
+
+const meta = {
+  id: 'open-forms-formstep',
+  title: 'Open Formulieren/FormStep',
+  component: SingleFormioComponent,
+  args: {
+    type: 'formio',
+  },
+} satisfies Meta<typeof SingleFormioComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    key: 'formStep1',
+    label: '',
+    formButtons: true,
+    extraComponentProperties: {
+      components: [
+        {
+          type: 'textfield',
+          key: 'text1',
+          label: 'Simple text field',
+          description: 'A help text for the text field',
+        },
+        {
+          type: 'radio',
+          key: 'radio1',
+          label: 'Radio choices',
+          values: [
+            { value: 'option1', label: 'Option1' },
+            { value: 'option2', label: 'Option2' },
+          ],
+        },
+      ],
+    },
+  },
+};
--- packages/storybook-react/src/stories/open-forms/StoryUtil.tsx
+++ packages/storybook-react/src/stories/open-forms/StoryUtil.tsx
@@ -1,11 +1,60 @@
 import { OFLibrary, OpenFormsModule } from '@open-formulieren/sdk';
-import { OpenFormsContainer } from '@utrecht/component-library-react/dist/css-module';
+import { Button, ButtonGroup, Icon, Link, OpenFormsContainer } from '@utrecht/component-library-react/dist/css-module';
 import merge from 'lodash.merge';
 import React, { PropsWithChildren, useEffect, useState } from 'react';
 import { Form, Formio, Templates } from 'react-formio';
 import '@open-formulieren/sdk/styles.css';
 export type FormConfiguration = { type: string; components: { type: string; key: string; label: string }[] };
+const FormButton = () => (
+  <ButtonGroup
+    direction="column"
+    style={{ '--utrecht-icon-inset-block-start': '-4px', '--utrecht-button-icon-gap': '8px' }}
+  >
+    <Button
+      className="openforms-button-with-icon"
+      appearance="primary-action-button"
+      type="submit"
+      aria-disabled="false"
+      name="next"
+      icon={
+        <Icon>
+          <i className="fa fas fa-icon fa-arrow-right-long" aria-hidden="true"></i>
+        </Icon>
+      }
+      label="Volgende"
+    />
+    <Link href="/" className="openforms-previous-link openforms-previous-link--end">
+      Vorige stap
+      <Icon>
+        <i className="fa fas fa-icon fa-arrow-left-long openforms-previous-link__icon" aria-hidden="true"></i>
+      </Icon>
+    </Link>
+    <Button
+      className="utrecht-link-button utrecht-link-button--html-button utrecht-link-button--openforms"
+      appearance="secondary-action-button"
+      type="button"
+      name="save"
+      icon={
+        <Icon>
+          <i className="fa fas fa-icon fa-circle-pause" aria-hidden="true"></i>
+        </Icon>
+      }
+      label="Tussentijds opslaan"
+    />
 
+    <Button
+      icon={
+        <Icon>
+          <i className="fa fas fa-icon fa-xmark" aria-hidden="true"></i>
+        </Icon>
+      }
+      className="utrecht-link-button utrecht-link-button--html-button utrecht-link-button--openforms"
+      type="button"
+      name="abort"
+      label="Annuleren"
+    />
+  </ButtonGroup>
+);
 const useOpenFormsConfiguration = () => {
   const [isConfigured, setIsConfigured] = useState(Templates.current === OFLibrary);
 
@@ -20,29 +69,35 @@ const useOpenFormsConfiguration = () => {
   return isConfigured;
 };
 
-const RenderFormioForm = ({ form }: PropsWithChildren<{ form: FormConfiguration }>) => {
+const RenderFormioForm = ({
+  form,
+  formButtons,
+}: PropsWithChildren<{ form: FormConfiguration; formButtons?: boolean }>) => {
   const isConfigured = useOpenFormsConfiguration();
 
   if (!isConfigured) {
     return null;
   }
   return (
-    <Form
-      form={form}
-      submission={{}}
-      options={{
-        noAlerts: true,
-        language: 'nl',
-        baseUrl: '',
-        evalContext: {
-          ofPrefix: 'openforms-',
-          requiredFieldsWithAsterisk: false,
-        },
-        ofContext: {
-          submissionUuid: '426c8d33-6dcb-4578-8208-f17071a4aebe',
-        },
-      }}
-    />
+    <div>
+      <Form
+        form={form}
+        submission={{}}
+        options={{
+          noAlerts: true,
+          language: 'nl',
+          baseUrl: '',
+          evalContext: {
+            ofPrefix: 'openforms-',
+            requiredFieldsWithAsterisk: false,
+          },
+          ofContext: {
+            submissionUuid: '426c8d33-6dcb-4578-8208-f17071a4aebe',
+          },
+        }}
+      />
+      {formButtons && <FormButton />}
+    </div>
   );
 };
 
@@ -51,6 +106,7 @@ type SingleFormioComponentProps = {
   key: string;
   label: string;
   extraComponentProperties?: { [index: string]: any };
+  formButtons?: boolean;
 };
 
 type MultipleFormioComponentsProps = {
@@ -60,6 +116,7 @@ type MultipleFormioComponentsProps = {
     label: string;
     [index: string]: any;
   }[];
+  formButtons?: boolean;
 };
 
 export const SingleFormioComponent = ({
@@ -67,19 +124,20 @@ export const SingleFormioComponent = ({
   key,
   label,
   extraComponentProperties = {},
+  formButtons = false,
 }: SingleFormioComponentProps) => {
   const component = merge({ type, key, label }, extraComponentProperties);
   return (
     <OpenFormsContainer>
-      <RenderFormioForm form={{ type: 'form', components: [component] }} />
+      <RenderFormioForm formButtons={formButtons} form={{ type: 'form', components: [component] }} />
     </OpenFormsContainer>
   );
 };
 
-export const MultipleFormioComponents = ({ components }: MultipleFormioComponentsProps) => {
+export const MultipleFormioComponents = ({ components, formButtons }: MultipleFormioComponentsProps) => {
   return (
     <OpenFormsContainer>
-      <RenderFormioForm form={{ type: 'form', components: components }} />
+      <RenderFormioForm formButtons={formButtons} form={{ type: 'form', components: components }} />
     </OpenFormsContainer>
   );
 };
