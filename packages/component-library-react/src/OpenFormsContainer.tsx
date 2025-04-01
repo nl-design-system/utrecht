f7f1470e60ef316e2a44f6a1de8094529d518567
Ali Amori Kadhim <ali@frameless.io>
2025-04-01 14:49:10 +0200

    feat(component-library-react): add `OpenFormsContainer` component

---
+++ packages/component-library-react/src/OpenFormsContainer.tsx
@@ -0,0 +1,8 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2025 Frameless B.V.
+ * Copyright (c) 2021-2025 Gemeente Utrecht
+ */
+
+export { type OpenFormsContainerProps } from '@utrecht/open-forms-container-react';
+export { OpenFormsContainer } from '@utrecht/open-forms-container-react';
