86c432ed5a82ff9289e56d7ea65da8ef73d1aab5
Ali Amori Kadhim <ali@frameless.io>
2025-05-20 12:30:07 +0200

    feat(component-library-react): add React Card component

---
+++ packages/component-library-react/src/css-module/Card.tsx
@@ -0,0 +1,11 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2020-2025 Frameless B.V.
+ * Copyright (c) 2021-2025 Gemeente Utrecht
+ */
+
+import css from '@utrecht/card-css/dist/index.mjs';
+import { insertStyle } from './insert-style';
+insertStyle(css);
+
+export * from '../Card';
