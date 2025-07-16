6c34afdb84531354ce7a11591ca71e15ba080ac7
Jeroen du Chatinier <49227194+JeroenduC@users.noreply.github.com>
2025-07-16 08:33:41 +0200

    feat: contactformulier eerste commit

---
+++ packages/storybook-react/src/stories/prototype-contactformulier/FormPage2.stories.tsx
@@ -0,0 +1,80 @@
+import { Meta, StoryObj } from '@storybook/react';
+import {
+  BreadcrumbNav,
+  BreadcrumbNavLink,
+  BreadcrumbNavSeparator,
+  ButtonLink,
+  Heading1,
+  Logo,
+  LogoImage,
+  Page,
+  PageContent,
+  PageHeader,
+  Surface,
+  UnorderedList,
+  UnorderedListItem,
+} from '@utrecht/component-library-react/dist/css-module';
+import { UtrechtIconArrow, UtrechtIconChevronLeft } from '@utrecht/web-component-library-react';
+import React from 'react';
+import '../prototype-src/index.css'; // Importeer stylesheet
+import FooterKlachten from '../prototype-src/FooterKlachtenFormulier.js'; // Importeer het footer-component
+import urls from '../prototype-src/variables.js';
+import '../styles.css';
+
+const meta = {
+  title: 'Prototypes/Contactformulier/Stap 2',
+  id: 'contactformulier-stap-2',
+  component: Page,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Page>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const One: Story = {
+  render: (args: any) => (
+    <Surface className="utrecht-custom-theme">
+      <Page {...args}>
+        <PageHeader>
+          <Logo>
+            <LogoImage />
+          </Logo>
+        </PageHeader>
+        <PageContent>
+          <BreadcrumbNav>
+            <BreadcrumbNavLink href={urls.scenarioBPage1}>
+              <BreadcrumbNavSeparator>
+                <UtrechtIconChevronLeft />
+              </BreadcrumbNavSeparator>
+              Terug
+            </BreadcrumbNavLink>
+          </BreadcrumbNav>
+          <main>
+            <section>
+              <Heading1>Klacht over de gemeente</Heading1>
+              <UnorderedList>
+                <UnorderedListItem>
+                  In dit formulier doorloopt u de volgende stappen: uw klacht, voorstel oplossing, uw gegevens en het
+                  controleren van de ingevulde informatie.
+                </UnorderedListItem>
+                <UnorderedListItem>Vul alles in. Als iets niet verplicht is, staat dat erbij.</UnorderedListItem>
+                <UnorderedListItem>U kunt het formulier tussentijds opslaan en later verder gaan.</UnorderedListItem>
+                <UnorderedListItem>
+                  Na het versturen ontvangt u een bevestigingsmail. Ook kunt u uw klacht downloaden of printen.
+                </UnorderedListItem>
+              </UnorderedList>
+              <ButtonLink appearance="primary-action-button" href={urls.scenarioBPage3}>
+                Doorgaan
+                <UtrechtIconArrow />
+              </ButtonLink>
+            </section>
+          </main>
+        </PageContent>
+        <FooterKlachten />
+      </Page>
+    </Surface>
+  ),
+};
---
+++ packages/storybook-react/src/stories/prototype-contactformulier/introductie.stories.tsx
@@ -0,0 +1,56 @@
+import { Meta, StoryObj } from '@storybook/react';
+import {
+  ButtonLink,
+  Logo,
+  LogoImage,
+  Page,
+  PageContent,
+  PageHeader,
+  Surface,
+} from '@utrecht/component-library-react/dist/css-module';
+import { UtrechtIconArrow } from '@utrecht/web-component-library-react';
+import React from 'react';
+import '../prototype-src/index.css'; // Importeer stylesheet
+import FooterKlachten from '../prototype-src/FooterKlachtenFormulier.js'; // Importeer het footer-component
+import IntroductieKlachten from '../prototype-src/IntroductieKlachtenFormulier.js'; // Importeer het footer-component
+import urls from '../prototype-src/variables.js';
+import '../styles.css';
+
+const meta = {
+  title: 'Prototypes/Contactformulier/Introductie ',
+  id: 'contactformulier-introductie',
+  component: Page,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Page>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const One: Story = {
+  render: (args: any) => (
+    <Surface className="utrecht-custom-theme">
+      <Page {...args}>
+        <PageHeader>
+          <Logo>
+            <LogoImage />
+          </Logo>
+        </PageHeader>
+        <PageContent>
+          <main>
+            <section>
+              <IntroductieKlachten />
+              <ButtonLink appearance="primary-action-button" href={urls.scenarioBscenario}>
+                Starten
+                <UtrechtIconArrow />
+              </ButtonLink>
+            </section>
+          </main>
+        </PageContent>
+        <FooterKlachten />
+      </Page>
+    </Surface>
+  ),
+};
