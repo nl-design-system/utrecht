6da84da531d7693df9a6615cf462c706c019c37b
Jeroen du Chatinier <49227194+JeroenduC@users.noreply.github.com>
2025-07-16 08:33:41 +0200

    feat: contactformulier eerste commit

---
+++ packages/storybook-react/src/stories/prototype-contactformulier/FormPage1.stories.tsx
@@ -0,0 +1,88 @@
+import { Meta, StoryObj } from '@storybook/react';
+import {
+  BreadcrumbNav,
+  BreadcrumbNavLink,
+  BreadcrumbNavSeparator,
+  Button,
+  ButtonLink,
+  Heading1,
+  Heading2,
+  Logo,
+  LogoImage,
+  Page,
+  PageContent,
+  PageHeader,
+  Paragraph,
+  Surface,
+} from '@utrecht/component-library-react/dist/css-module';
+import { UtrechtIconArrow, UtrechtIconChevronLeft } from '@utrecht/web-component-library-react';
+import React from 'react';
+import '../prototype-src/index.css'; // Importeer stylesheet
+import FooterKlachten from '../prototype-src/FooterKlachtenFormulier.js'; // Importeer het footer-component
+import urls from '../prototype-src/variables.js';
+import '../styles.css';
+
+const meta = {
+  title: 'Prototypes/Contactformulier/Stap 1',
+  id: 'contactformulier-stap-1',
+  component: Page,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Page>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const One: Story = {
+  render: (args: any) => (
+    <Surface className="utrecht-custom-theme">
+      <Page {...args}>
+        <PageHeader>
+          <Logo>
+            <LogoImage />
+          </Logo>
+        </PageHeader>
+        <PageContent>
+          <BreadcrumbNav>
+            <BreadcrumbNavLink href={urls.scenarioBscenario}>
+              <BreadcrumbNavSeparator>
+                <UtrechtIconChevronLeft />
+              </BreadcrumbNavSeparator>
+              Terug
+            </BreadcrumbNavLink>
+          </BreadcrumbNav>
+          <main>
+            <section>
+              <Heading1>Klacht</Heading1>
+              <Paragraph appearance="lead">
+                Bent u ontevreden over hoe de gemeente haar taken uitvoert? Bent u bijvoorbeeld niet goed geholpen? Of
+                bent u niet netjes behandeld? De gemeente vindt dat vervelend en zoekt graag samen naar een oplossing.
+              </Paragraph>
+              <Paragraph>
+                U kunt uw klacht op 3 manieren indienen: met het klachtenformulier, bellen of een brief sturen.
+              </Paragraph>
+
+              <ButtonLink appearance="primary-action-button" href={urls.scenarioBPage2}>
+                Ga naar klachtenformulier
+                <UtrechtIconArrow />
+              </ButtonLink>
+              <Heading2>Bellen</Heading2>
+              <Paragraph>
+                U kunt bellen naar telefoonnummer 14030 (of vanuit het buitenland: +31 30 286 00 00). Dit kan van
+                maandag tot en met vrijdag tussen 9.00 en 17.00 uur.
+              </Paragraph>
+              <Heading2>Een brief sturen</Heading2>
+              <Paragraph>
+                U kunt een brief schrijven met uw klacht. Stuur uw brief naar: Gemeente Utrecht Klachtenbehandeling
+                Antwoordnummer 51066 3501 VC Utrecht (geen postzegel nodig)
+              </Paragraph>
+            </section>
+          </main>
+        </PageContent>
+        <FooterKlachten />
+      </Page>
+    </Surface>
+  ),
+};
---
+++ packages/storybook-react/src/stories/prototype-contactformulier/scenario.stories.tsx
@@ -0,0 +1,56 @@
+import { Meta, StoryObj } from '@storybook/react';
+import {
+  ButtonLink,
+  Logo,
+  LogoImage,
+  Page,
+  PageContent,
+  PageHeader,
+  Surface,
+} from '@utrecht/component-library-react/dist/css-module';
+import { UtrechtIconArrow } from '@utrecht/web-component-library-react';
+import React from 'react';
+import '../prototype-src/index.css'; // Importeer stylesheet
+import FooterKlachten from '../prototype-src/FooterKlachtenFormulier.js'; // Importeer het footer-component
+import ScenarioKlachten from '../prototype-src/ScenarioKlachtenFormulier.js'; // Importeer het scenario-component
+import urls from '../prototype-src/variables.js';
+import '../styles.css';
+
+const meta = {
+  title: 'Prototypes/Contactformulier/Scenario',
+  id: 'contactformulie-scenario-B',
+  component: Page,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Page>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const One: Story = {
+  render: (args: any) => (
+    <Surface className="utrecht-custom-theme">
+      <Page {...args}>
+        <PageHeader>
+          <Logo>
+            <LogoImage />
+          </Logo>
+        </PageHeader>
+        <PageContent>
+          <main>
+            <section>
+              <ScenarioKlachten />
+              <ButtonLink appearance="primary-action-button" href={urls.scenarioBPage1}>
+                Naar de klachtenpagina
+                <UtrechtIconArrow />
+              </ButtonLink>
+            </section>
+          </main>
+        </PageContent>
+        <FooterKlachten />
+      </Page>
+    </Surface>
+  ),
+};
