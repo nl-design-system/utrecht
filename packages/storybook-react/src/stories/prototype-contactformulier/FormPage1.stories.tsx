e75bd6fb249d572e678a4899aa0c5efd41e54e61
Jeroen du Chatinier <49227194+JeroenduC@users.noreply.github.com>
2025-07-22 15:24:58 +0200

    fix: form pages

--- packages/storybook-react/src/stories/prototype-contactformulier/FormPage1.stories.tsx
+++ packages/storybook-react/src/stories/prototype-contactformulier/FormPage1.stories.tsx
@@ -40,6 +40,8 @@ import React from 'react';
 import '../prototype-src/index.css'; // Importeer stylesheet
 import FooterKlachten from '../prototype-src/FooterKlachtenFormulier.js'; // Importeer het footer-component
 import urls from '../prototype-src/variables.js';
+import HulpEnContact from '../prototype-src/webpaginablokken/HulpEnContact.js';
+import KTO from '../prototype-src/webpaginablokken/KTO.js'; // Importeer het scenario-component
 import '../styles.css';
 
 const meta = {
@@ -234,37 +236,8 @@ export const One: Story = {
               </iframe>
               {/* TODO: Hulp en contact blok toevoegen 
               TODO: Contactformulier toevoegen */}
-              <Separator />
-              <Fieldset id="df861ef1-844a-42df-8365-b54f59474fb8" role="radiogroup">
-                <FieldsetLegend>Hebt u gevonden wat u zocht op deze pagina?</FieldsetLegend>
-                <FormField type="radio">
-                  <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-                    <FormLabel htmlFor="800b7f3f-5048-4dcc-8f23-d04fa6ca4199" type="radio">
-                      <RadioButton
-                        className="utrecht-form-field__input"
-                        id="800b7f3f-5048-4dcc-8f23-d04fa6ca4199"
-                        name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
-                        value="1"
-                      />
-                      Ja
-                    </FormLabel>
-                  </Paragraph>
-                </FormField>
-                <FormField type="radio">
-                  <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-                    <FormLabel htmlFor="fbc03e63-42c2-46e3-9acc-183fa64c6dcd" type="radio">
-                      <RadioButton
-                        className="utrecht-form-field__input"
-                        id="fbc03e63-42c2-46e3-9acc-183fa64c6dcd"
-                        name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
-                        value="2"
-                      />
-                      Nee
-                    </FormLabel>
-                  </Paragraph>
-                </FormField>
-              </Fieldset>
-              <Separator />
+              <HulpEnContact />
+              <KTO />
             </section>
           </main>
         </PageContent>
