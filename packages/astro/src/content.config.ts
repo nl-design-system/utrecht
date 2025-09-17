540f35c82c2fd8b676c19e33dfc293e2ad8884c2
Bryan de Jong <bryan@levitade.io>
2025-09-17 09:56:17 +0200

    feat: custom page loket

---
+++ packages/astro/src/content.config.ts
@@ -0,0 +1,12 @@
+import { glob } from 'astro/loaders'; // Not available with legacy API
+import { defineCollection, reference, z } from 'astro:content';
+
+const basis = defineCollection({
+  loader: glob({ base: '../../../documentation/website/basis', pattern: '**/!(_)(*).md' }),
+  schema: z.object({
+    description: z.string(),
+    title: z.string(),
+  }),
+});
+
+export const collections = { basis };
