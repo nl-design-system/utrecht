02c55371b23701dfa774b3b8f3f94952d841f4cc
Bryan de Jong <bryan@levitade.io>
2025-09-22 15:26:05 +0200

    fix: astro base url

--- packages/astro/astro.config.ts
+++ packages/astro/astro.config.ts
@@ -1,11 +1,16 @@
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import path from 'node:path';
+import process from 'node:process';
 import { nldsComponentsPlugin } from './plugins/rehype-nlds-components/index.js';
+
+const deploymentBaseUrl = process.env.BASE_URL ?? '/';
+const baseUrl = path.posix.join(deploymentBaseUrl, 'astro');
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/astro',
+  base: baseUrl,
   trailingSlash: 'always',
   build: {
     inlineStylesheets: 'never',
