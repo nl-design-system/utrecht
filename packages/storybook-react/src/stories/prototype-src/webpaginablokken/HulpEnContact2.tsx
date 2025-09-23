ca71523647addfb142b2391b69c684f6ad1ef4fb
JeroenduC <j.du.chatinier@utrecht.nl>
2025-09-23 15:54:52 +0200

    fix: font-size en spotlight

--- packages/storybook-react/src/stories/prototype-src/webpaginablokken/HulpEnContact2.tsx
+++ packages/storybook-react/src/stories/prototype-src/webpaginablokken/HulpEnContact2.tsx
@@ -1,8 +1,11 @@
 import {
+  AlertStory,
   Heading2,
   Heading3,
+  Heading4,
   Paragraph,
   Separator,
+  SpotlightSection,
   Strong,
   UnorderedList,
   UnorderedListItem,
@@ -24,7 +27,7 @@ const HulpEnContact2: React.FC = () => {
 
       <div className="utrecht-help-contact-content">
         <div className="utrecht-help-contact-left">
-          <Heading3>Telefoon</Heading3>
+          <Heading3 style={{ fontWeight: 'normal' }}>Telefoon</Heading3>
           <br />
           <a href="tel:14030" className="utrecht-help-contact-phone">
             14 030
@@ -32,7 +35,7 @@ const HulpEnContact2: React.FC = () => {
         </div>
 
         <div className="utrecht-help-contact-right">
-          <Heading3>Volg ons</Heading3>
+          <Heading3 style={{ fontWeight: 'normal' }}>Volg ons</Heading3>
           <div className="utrecht-social-media-icons">
             <a href="#" className="utrecht-social-media-icon" aria-label="Facebook">
               <UtrechtIconFacebook />
@@ -49,17 +52,20 @@ const HulpEnContact2: React.FC = () => {
 
       <Separator style={{ borderColor: '#ADCBEB', opacity: '0.4' }} />
       <Heading2>Uw vraag of opmerking</Heading2>
-      <Paragraph>
-        <Strong>Uw vraag of opmerking is met succes verstuurd</Strong>
-      </Paragraph>
-      <Heading3>Wat gaat er nu gebeuren?</Heading3>
-      <UnorderedList>
-        <UnorderedListItem>Als u een e-mail adres heeft ingevuld ontvangt u een bevestigingsmail</UnorderedListItem>
-        <UnorderedListItem>De afdeling Projecten gaat met uw verstuurde bericht aan de slag</UnorderedListItem>
-        <UnorderedListItem>
-          Als u een e-mail adres heeft ingevuld wordt u per email op de hoogte gehouden
-        </UnorderedListItem>
-      </UnorderedList>
+      <SpotlightSection style={{ backgroundColor: '#fff' }}>
+        <Heading3> Uw vraag of opmerking is met succes verstuurd</Heading3>
+        <Heading4>
+          <utrecht-icon-checkmark></utrecht-icon-checkmark>
+          Wat gaat er nu gebeuren?
+        </Heading4>
+        <UnorderedList>
+          <UnorderedListItem>Als u een e-mail adres heeft ingevuld ontvangt u een bevestigingsmail</UnorderedListItem>
+          <UnorderedListItem>De afdeling Projecten gaat met uw verstuurde bericht aan de slag</UnorderedListItem>
+          <UnorderedListItem>
+            Als u een e-mail adres heeft ingevuld wordt u per email op de hoogte gehouden
+          </UnorderedListItem>
+        </UnorderedList>
+      </SpotlightSection>
     </div>
   );
 };
