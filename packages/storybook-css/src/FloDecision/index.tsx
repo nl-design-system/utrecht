1ceefad673352943973e302048c144ad146bc06c
Ali Amori Kadhim <ali@frameless.io>
2025-09-23 15:31:21 +0200

    docs(storybook-css): add flo-decision storybook stories

---
+++ packages/storybook-css/src/FloDecision/index.tsx
@@ -0,0 +1,54 @@
+import { loadFloClientScript } from '@utrecht/flo-legal-decision-tree-client';
+import React, { useEffect, useState } from 'react';
+import afvalBeslisboomRegelhulpData from './afval-beslisboom-regelhulp.json';
+import { encodeHtmlEntities } from './encodeHtmlEntities';
+import voorbeeldToelichtingenData from './voorbeeld-toelichtingen-data.json';
+import '@utrecht/flolegal-decision-tree-css/dist/index.css';
+import '@utrecht/flo-legal-decision-tree-client/dist/assets/flo-client-styles.css';
+
+export const dataSources = {
+  'Afval beslisboom Regelhulp': afvalBeslisboomRegelhulpData,
+  'Voorbeeld Toelichting': voorbeeldToelichtingenData,
+};
+
+export type DataSourceType = keyof typeof dataSources | object;
+export type DataSourceContent = (typeof dataSources)[keyof typeof dataSources]['content'] | undefined;
+interface FloDecisionProps {
+  outcomesHeader?: string;
+  checkData?: DataSourceContent;
+  outcomesHeaderLevel?: number;
+  showOutcomes?: boolean;
+  loadingText?: string;
+  nonce?: string;
+}
+
+export const FloDecision = ({
+  checkData,
+  outcomesHeader,
+  outcomesHeaderLevel,
+  showOutcomes = false,
+  loadingText,
+}: FloDecisionProps) => {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    loadFloClientScript().then(() => setReady(true));
+  }, []);
+
+  if (!ready) return loadingText && <div>{loadingText}</div>;
+
+  const encodedData = checkData ? encodeHtmlEntities(JSON.stringify(checkData)) : undefined;
+  const floDecisionContent = `<flo-decision 
+  data-check-data="${encodedData}"
+    data-show-outcomes="${showOutcomes}"
+    data-outcomes-header="${outcomesHeader}"
+    data-outcomes-header-level="${outcomesHeaderLevel ?? 2}"
+  ></flo-decision>`;
+
+  return (
+    <div
+      className="utrecht-html utrecht-flolegal-decision-tree-container"
+      dangerouslySetInnerHTML={{ __html: floDecisionContent }}
+    />
+  );
+};
