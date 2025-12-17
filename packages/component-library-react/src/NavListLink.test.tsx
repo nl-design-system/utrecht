bf053c7853c7274ff61933e5540b1deac468b02c
Michelle <michelleermen@gmail.com>
2025-12-17 16:13:04 +0100

    chore: add tests

--- packages/component-library-react/src/NavListLink.test.tsx
+++ packages/component-library-react/src/NavListLink.test.tsx
@@ -124,4 +124,62 @@ describe('Navigation bar link list link', () => {
   });
 
   it.todo('supports ForwardRef in React');
+
+  it('renders center modifier class when center prop is true', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#" center>
+          Home
+        </NavListLink>
+      </NavList>,
+    );
+
+    const listItem = container.querySelector('li:only-child');
+
+    expect(listItem).toHaveClass('utrecht-nav-list__item--center');
+  });
+
+  it('renders current modifier class and aria-current when current prop is set', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#" current="page">
+          Home
+        </NavListLink>
+      </NavList>,
+    );
+
+    const link = container.querySelector('a:only-child');
+
+    expect(link).toHaveClass('utrecht-nav-list__link--current');
+    expect(link).toHaveAttribute('aria-current', 'page');
+  });
+
+  it('renders current modifier class and aria-current when aria-current prop is set', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#" aria-current="page">
+          Home
+        </NavListLink>
+      </NavList>,
+    );
+
+    const link = container.querySelector('a:only-child');
+
+    expect(link).toHaveClass('utrecht-nav-list__link--current');
+    expect(link).toHaveAttribute('aria-current', 'page');
+  });
+
+  it('prefers current prop over aria-current prop', () => {
+    const { container } = render(
+      <NavList>
+        <NavListLink href="#" current="true" aria-current="page">
+          Home
+        </NavListLink>
+      </NavList>,
+    );
+
+    const link = container.querySelector('a:only-child');
+
+    expect(link).toHaveAttribute('aria-current', 'true');
+  });
 });
