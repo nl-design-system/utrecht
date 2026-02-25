ae75b25306d77c74bae332b1863b0d771a48e481
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2026-02-25 13:33:20 +0100

    refactor: inline the Numberish type
    eslint will complain about this, presumably because it treats script and template separately

--- packages/component-library-vue/src/Image.vue
+++ packages/component-library-vue/src/Image.vue
@@ -1,8 +1,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-type Numberish = string | number;
-
 export default defineComponent({
   name: 'UtrechtImage',
   props: {
@@ -22,8 +20,8 @@ export default defineComponent({
     :class="{ 'utrecht-img--photo': photo }"
     :alt="$attrs?.alt as string"
     :src="$attrs.src as string"
-    :height="$attrs?.height as Numberish"
-    :width="$attrs?.width as Numberish"
+    :height="$attrs?.height as (string | number)"
+    :width="$attrs?.width as (string | number)"
   />
 </template>
 
