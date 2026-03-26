f2f928a8ac2d90ed2567b8d23eea245fec0dd71a
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2026-03-26 11:15:47 +0100

    test: test Root component in a different way
    Since React 19 forbids rendering `<html>` as a child of anything, testing it is impossible using `render()`.

--- packages/components-react/root-react/src/index.test.tsx
+++ packages/components-react/root-react/src/index.test.tsx
@@ -3,11 +3,21 @@ import { Root } from './index';
 import '@testing-library/jest-dom';
 
 describe('Root', () => {
-  it('renders an HTML html element', () => {
+  it.skip('renders an HTML html element', () => {
+    // This test is skipped because since React 19, it is forbidden to render
+    // <html> as a child of anything.
     const { container } = render(<Root />);
 
     const html = container.querySelector('html:only-child');
 
     expect(html).toBeInTheDocument();
+  });
+
+  it('should allow a custom tag name', () => {
+    const { container } = render(<Root Component="table" />);
+
+    const table = container.querySelector('table:only-child');
+
+    expect(table).toBeInTheDocument();
   });
 });
