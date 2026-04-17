33f9bd98d95704ca40a98ff33dd5e8f4d44ee2d4
Bram Smulders <bram@supple.works>
2026-04-17 16:55:05 +0200

    feat: wip - using container queries for the sizing of the field

--- packages/components-react/textbox-react/src/index.tsx
+++ packages/components-react/textbox-react/src/index.tsx
@@ -88,7 +88,7 @@ export const TextboxContainer = forwardRef<HTMLSpanElement, TextboxContainerProp
   ({ children, className, leading, trailing, ...restProps }: TextboxContainerProps, ref) => {
     const leadingRef = useRef<HTMLSpanElement>(null);
     const trailingRef = useRef<HTMLSpanElement>(null);
-    const containerRef = useRef<HTMLSpanElement>(null);
+    const containerInnerRef = useRef<HTMLSpanElement>(null);
     const resizeTimeoutRef = useRef<number | null>(null);
     const [leadingInlineSize, setLeadingInlineSize] = useState<string>('0px');
     const [trailingInlineSize, setTrailingInlineSize] = useState<string>('0px');
@@ -116,8 +116,8 @@ export const TextboxContainer = forwardRef<HTMLSpanElement, TextboxContainerProp
 
       const resizeObserver = new ResizeObserver(deBouncedOnResize);
 
-      if (containerRef.current) {
-        resizeObserver.observe(containerRef.current);
+      if (containerInnerRef.current) {
+        resizeObserver.observe(containerInnerRef.current);
       }
 
       // Cleanup
@@ -132,31 +132,26 @@ export const TextboxContainer = forwardRef<HTMLSpanElement, TextboxContainerProp
     return (
       <span
         {...restProps}
-        ref={(node) => {
-          if (typeof ref === 'function') {
-            ref(node);
-          } else if (ref) {
-            ref.current = node;
-          }
-          containerRef.current = node;
-        }}
         className={clsx('utrecht-textbox-container', className)}
         style={{
           ['--_utrecht-textbox-leading-inline-size-calculated' as keyof CSSProperties]: leadingInlineSize,
           ['--_utrecht-textbox-trailing-inline-size-calculated' as keyof CSSProperties]: trailingInlineSize,
         }}
+        ref={ref}
       >
-        {leading ? (
-          <span ref={leadingRef} className="utrecht-textbox-container__leading">
-            {leading}
-          </span>
-        ) : null}
-        {children}
-        {trailing ? (
-          <span ref={trailingRef} className="utrecht-textbox-container__trailing">
-            {trailing}
-          </span>
-        ) : null}
+        <span className="utrecht-textbox-container__inner" ref={containerInnerRef}>
+          {leading ? (
+            <span ref={leadingRef} className="utrecht-textbox-container__leading">
+              {leading}
+            </span>
+          ) : null}
+          {children}
+          {trailing ? (
+            <span ref={trailingRef} className="utrecht-textbox-container__trailing">
+              {trailing}
+            </span>
+          ) : null}
+        </span>
       </span>
     );
   },
@@ -174,71 +169,58 @@ export interface Textbox2Props extends InputHTMLAttributes<HTMLInputElement>, Te
 }
 
 export const Textbox2 = forwardRef<HTMLInputElement, Textbox2Props>(
-  (
-    {
-      dir,
-      disabled,
-      invalid,
-      readOnly,
-      required,
-      inputRequired,
-      className,
-      type = 'text',
-      inputMode,
-      leading,
-      trailing,
-      ...restProps
-    }: Textbox2Props,
-    ref,
-  ) => {
+  ({ className, leading, trailing, ...restProps }: Textbox2Props, ref) => {
     const inputRef = useRef<HTMLInputElement>(null);
-    const containerRef = useRef<HTMLSpanElement>(null);
+    const containerInnerRef = useRef<HTMLSpanElement>(null);
 
     // Default ref behavior: point to the input element
     useImperativeHandle(ref, () => inputRef.current!, []);
 
     const renderContainer = leading || trailing;
 
-    const inputClasses = clsx(
-      'utrecht-textbox',
-      'utrecht-textbox--html-input',
-      disabled && 'utrecht-textbox--disabled',
-      invalid && 'utrecht-textbox--invalid',
-      readOnly && 'utrecht-textbox--readonly',
-      (required || inputRequired) && 'utrecht-textbox--required',
-      className,
-    );
+    // const inputClasses = clsx(
+    //   'utrecht-textbox',
+    //   'utrecht-textbox--html-input',
+    //   disabled && 'utrecht-textbox--disabled',
+    //   invalid && 'utrecht-textbox--invalid',
+    //   readOnly && 'utrecht-textbox--readonly',
+    //   (required || inputRequired) && 'utrecht-textbox--required',
+    //   className,
+    // );
 
-    const inputClassesInContainer = clsx(
-      'utrecht-textbox-container__input',
-      (required || inputRequired) && 'utrecht-textbox-container__input--required',
-    );
+    // const inputClassesInContainer = clsx(
+    //   'utrecht-textbox-container__input',
+    //   (required || inputRequired) && 'utrecht-textbox-container__input--required',
+    // );
 
-    const Input = (
-      <input
-        {...restProps}
-        ref={inputRef}
-        type={type}
-        className={renderContainer ? inputClassesInContainer : inputClasses}
-        dir={dir ?? 'auto'}
-        disabled={disabled}
-        readOnly={readOnly}
-        aria-required={required ? required : undefined}
-        required={inputRequired}
-        aria-invalid={invalid || undefined}
-        inputMode={inputMode || (type === 'number' ? 'numeric' : undefined)}
-      />
-    );
+    const inputClasses = clsx('utrecht-textbox-sizing', className);
+    const inputClassesInContainer = clsx('utrecht-textbox-container__input', className);
+
+    // const Input = (
+    //   <input
+    //     {...restProps}
+    //     ref={inputRef}
+    //     type={type}
+    //     className={renderContainer ? inputClassesInContainer : inputClasses}
+    //     dir={dir ?? 'auto'}
+    //     disabled={disabled}
+    //     readOnly={readOnly}
+    //     aria-required={required ? required : undefined}
+    //     required={inputRequired}
+    //     aria-invalid={invalid || undefined}
+    //     inputMode={inputMode || (type === 'number' ? 'numeric' : undefined)}
+    //   />
+    // );
 
     if (renderContainer) {
       return (
-        <TextboxContainer className={className} ref={containerRef} leading={leading} trailing={trailing}>
-          {Input}
+        <TextboxContainer ref={containerInnerRef} leading={leading} trailing={trailing}>
+          <Textbox {...restProps} className={inputClassesInContainer} ref={inputRef} />
         </TextboxContainer>
       );
     }
 
-    return Input;
+    return <Textbox {...restProps} className={inputClasses} ref={inputRef} />;
   },
 );
 
