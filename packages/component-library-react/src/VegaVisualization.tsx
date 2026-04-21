84150663ac44815d6181953235220192b75f1a67
Robbert Broersma <git@robbertbroersma.nl>
2026-04-21 08:32:30 +0200

    chore: remove VegaVisualisation from component library bundle

--- packages/component-library-react/src/VegaVisualization.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * @license EUPL-1.2
- * Copyright (c) 2020-2024 Frameless B.V.
- * Copyright (c) 2021-2024 Gemeente Utrecht
- */
-
-export { type VegaVisualizationProps } from '@utrecht/vega-visualization-react';
-export { VegaVisualization } from '@utrecht/vega-visualization-react';
