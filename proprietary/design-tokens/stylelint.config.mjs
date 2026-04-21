e8d5e7cc5a6c561e79cb7b69f1dc8f22fd6ec517
Robbert Broersma <git@robbertbroersma.nl>
2026-04-21 12:05:31 +0200

    refactor: use `.mjs` for stylelint config

---
+++ proprietary/design-tokens/stylelint.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  extends: '../../stylelint.config.mjs',
+  rules: {
+    'comment-empty-line-before': null,
+    'order/properties-alphabetical-order': null,
+    'custom-property-pattern': null,
+    'no-duplicate-selectors': null,
+    'color-hex-length': 'long',
+  },
+};
