3135a18969fa368b2d166f4b427815485055a690
Bram Smulders <bram@supple.works>
2026-04-22 16:16:12 +0200

    feat: start with the structure

--- packages/components-react/textbox-addon-react/src/index.tsx
+++ packages/components-react/textbox-addon-react/src/index.tsx
@@ -1,9 +1,45 @@
-import { ForwardedRef, forwardRef, HTMLAttributes } from 'react';
+import clsx from 'clsx';
+import { forwardRef, HTMLAttributes, ReactNode } from 'react';
 
-export interface TextboxAddonProps extends HTMLAttributes<HTMLSpanElement> {}
+export interface TextboxAddonOwnProps {
+  start?: ReactNode;
+  end?: ReactNode;
+  inputId?: string;
+}
 
-export const TextboxAddon = forwardRef(({ ...restProps }: TextboxAddonProps, ref: ForwardedRef<HTMLSpanElement>) => (
-  <span {...restProps} ref={ref} />
-));
+export interface TextboxAddonProps extends HTMLAttributes<HTMLSpanElement>, TextboxAddonOwnProps {}
+
+export const TextboxAddon = forwardRef<HTMLSpanElement, TextboxAddonProps>(
+  ({ children, className, start, end, ...restProps }: TextboxAddonProps, ref) => {
+    return (
+      <span {...restProps} className={clsx('utrecht-textbox-addon', className)} ref={ref}>
+        {start ? <SlotWrapper className="utrecht-textbox-addon__slot--start">{start}</SlotWrapper> : null}
+        {children}
+        {end ? <SlotWrapper className="utrecht-textbox-addon__slot--end">{end}</SlotWrapper> : null}
+      </span>
+    );
+  },
+);
 
 TextboxAddon.displayName = 'TextboxAddon';
+
+export interface SlotWrapperOwnProps {
+  hiddenFromAT?: boolean;
+  inputId?: string;
+}
+
+export interface SlotWrapperProps extends HTMLAttributes<HTMLElement>, SlotWrapperOwnProps {}
+
+const SlotWrapper = ({ children, className, inputId, ...restProps }: SlotWrapperProps) => {
+  return inputId ? (
+    <label {...restProps} className={clsx('utrecht-textbox-addon__slot', className)}>
+      {children}
+    </label>
+  ) : (
+    <span {...restProps} className={clsx('utrecht-textbox-addon__slot', className)}>
+      {children}
+    </span>
+  );
+};
+
+SlotWrapper.displayName = 'SlotWrapper';
