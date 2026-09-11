2ddd6cf416fed09ba2e183f77c3543b22bf2e43b
Ali Amori Kadhim <ali@frameless.io>
2026-09-11 14:40:04 +0200

    feat: add normalize-none-preprocessor for design tokens

--- proprietary/design-tokens/style-dictionary-config.mjs
+++ proprietary/design-tokens/style-dictionary-config.mjs
@@ -1,11 +1,15 @@
 import { propertyFormatterHooksConfig } from './src/css-property-formatter.mjs';
+import { normalizeFigmaExportPreprocessorHooksConfig } from './src/normalize-none-preprocessor.mjs';
 const stringSort = (a, b) => (a === b ? 0 : a > b ? 1 : -1);
 
 const sortByName = (a, b) => stringSort(a.name, b.name);
 
 export const createStyleDictionaryConfig = ({ themeName, source = ['src/**/*.tokens.json'] }) => ({
-  preprocessors: ['tokens-studio'],
+  preprocessors: ['utrecht/normalize-figma-export', 'tokens-studio'],
   hooks: {
+    preprocessors: {
+      ...normalizeFigmaExportPreprocessorHooksConfig,
+    },
     formats: {
       ...propertyFormatterHooksConfig,
       'json/list': function ({ dictionary }) {
